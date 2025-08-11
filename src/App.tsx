2c5ec81390ef066a7f052c8bf798f31ad94de264
Delta6626 <hassan911ee@gmail.com>
2025-08-12 00:42:22 +0400

    create home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/home";
+
 function App() {
-  return <div className="">Hello, World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
