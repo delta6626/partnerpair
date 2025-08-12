bd2cfe9e94f2267cebe4927232423365d5c1ab03
Delta6626 <hassan911ee@gmail.com>
2025-08-12 22:37:28 +0400

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,24 @@
 export const Navbar = () => {
   return (
-    <header className="font-raleway">
-      <div className="font-bold">PartnerPair</div>
+    <header className="font-raleway flex align-center items-center justify-between">
+      <div className="font-bold text-xl">PartnerPair</div>
+      <div className="hidden md:flex">
+        <a href="" className="btn">
+          Product
+        </a>
+        <a href="" className="btn">
+          Pricing
+        </a>
+        <a href="" className="btn">
+          Blogs
+        </a>
+        <a href="" className="btn">
+          Sign Up
+        </a>
+        <a href="" className="btn">
+          Log In
+        </a>
+      </div>
     </header>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,10 @@ import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
   return (
-    <div className="">
-      <Navbar></Navbar>
+    <div className="w-full h-[100vh] bg-base-300 px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 2xl:px-24">
+      <div className="py-4">
+        <Navbar></Navbar>
+      </div>
     </div>
   );
 };
