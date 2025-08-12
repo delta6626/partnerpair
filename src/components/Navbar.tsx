b811604f2b059cbec672a61c1c50cfa3f6e99979
Delta6626 <hassan911ee@gmail.com>
2025-08-12 22:49:23 +0400

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,22 +2,25 @@ export const Navbar = () => {
   return (
     <header className="font-raleway flex align-center items-center justify-between">
       <div className="font-bold text-xl">PartnerPair</div>
-      <div className="hidden md:flex">
-        <a href="" className="btn">
+      <div className="hidden sm:flex">
+        <a href="" className="btn bg-transparent border-none">
           Product
         </a>
-        <a href="" className="btn">
+        <a href="" className="btn bg-transparent border-none">
           Pricing
         </a>
-        <a href="" className="btn">
+        <a href="" className="btn bg-transparent border-none">
           Blogs
         </a>
-        <a href="" className="btn">
-          Sign Up
-        </a>
-        <a href="" className="btn">
+        <a href="" className="btn bg-transparent border-none">
           Log In
         </a>
+        <a href="" className="btn bg-primary border-none">
+          Sign Up
+        </a>
+      </div>
+      <div className="sm:hidden">
+        <a href="" className="btn"></a>
       </div>
     </header>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
   return (
-    <div className="w-full h-[100vh] bg-base-300 px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20 2xl:px-24">
+    <div className="w-full h-[100vh] bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
       <div className="py-4">
         <Navbar></Navbar>
       </div>
