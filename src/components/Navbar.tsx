fbb85e610007d5be530a3575b13528acc3232c3a
Delta6626 <hassan911ee@gmail.com>
2025-08-12 23:14:11 +0400

    navbar adjustments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,21 +3,33 @@ import { Menu } from "lucide-react";
 export const Navbar = () => {
   return (
     <header className="font-raleway flex align-center items-center justify-between">
-      <div className="font-bold text-xl">PartnerPair</div>
+      <div className="font-bold text-2xl">PartnerPair</div>
       <div className="hidden sm:flex">
-        <a href="" className="btn bg-transparent border-none">
+        <a
+          href=""
+          className="btn bg-transparent border-none hover:text-primary"
+        >
           Product
         </a>
-        <a href="" className="btn bg-transparent border-none">
+        <a
+          href=""
+          className="btn bg-transparent border-none hover:text-primary"
+        >
           Pricing
         </a>
-        <a href="" className="btn bg-transparent border-none">
+        <a
+          href=""
+          className="btn bg-transparent border-none hover:text-primary"
+        >
           Blogs
         </a>
-        <a href="" className="btn bg-transparent border-none">
+        <a
+          href=""
+          className="btn bg-transparent border-none hover:text-primary"
+        >
           Log In
         </a>
-        <a href="" className="btn bg-primary border-none">
+        <a href="" className="btn btn-primary border-none">
           Sign Up
         </a>
       </div>
