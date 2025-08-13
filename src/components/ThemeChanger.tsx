4030e3a79267a80da7861cb11223dac8e4e3b292
Delta6626 <hassan911ee@gmail.com>
2025-08-14 00:49:49 +0400

    theme bs

--- src/components/ThemeChanger.tsx
+++ src/components/ThemeChanger.tsx
@@ -1,41 +1,34 @@
 import { Sun, Moon, MonitorCog } from "lucide-react";
 import type { AppTheme } from "../types/AppTheme";
-import { useState, useEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 
 export const ThemeChanger = () => {
-  const [prefersLight, setPrefersLight] = useState<Boolean>(false);
+  const [theme, setTheme] = useState<AppTheme>("system");
 
-  function handleThemeChange(theme: AppTheme) {
+  useLayoutEffect(() => {
+    const root = document.documentElement;
     if (theme === "system") {
-      document.documentElement.setAttribute(
-        "data-theme",
-        prefersLight ? "light" : "dark"
-      );
+      const prefersDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      root.setAttribute("data-theme", prefersDark ? "dark" : "light");
     } else {
-      document.documentElement.setAttribute("data-theme", theme);
+      root.setAttribute("data-theme", theme);
     }
-  }
-
-  useEffect(() => {
-    const mq = window.matchMedia("(prefers-color-scheme: light)");
-    setPrefersLight(mq.matches);
-
-    const listener = (e: MediaQueryListEvent) => {
-      setPrefersLight(e.matches);
-    };
-
-    mq.addEventListener("change", listener);
-    return () => mq.removeEventListener("change", listener);
-  }, []);
-
-  useEffect(() => {
-    handleThemeChange("system");
-  }, [prefersLight]);
+  }, [theme]);
 
   return (
     <div className="">
       <div className="dropdown dropdown-bottom dropdown-end">
-        <div tabIndex={0} role="button" className="btn btn-square"></div>
+        <div tabIndex={0} role="button" className="btn btn-square">
+          {theme === "light" ? (
+            <Sun size={20} />
+          ) : theme === "dark" ? (
+            <Moon size={20} />
+          ) : (
+            <MonitorCog size={20} />
+          )}
+        </div>
         <ul
           tabIndex={0}
           className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2 shadow-sm"
@@ -43,7 +36,7 @@ export const ThemeChanger = () => {
           <button
             className="btn flex items-center justify-start"
             onClick={() => {
-              handleThemeChange("light");
+              setTheme("light");
             }}
           >
             <Sun size={20} />
@@ -52,7 +45,7 @@ export const ThemeChanger = () => {
           <button
             className="btn flex items-center justify-start"
             onClick={() => {
-              handleThemeChange("dark");
+              setTheme("dark");
             }}
           >
             <Moon size={20} />
@@ -61,7 +54,7 @@ export const ThemeChanger = () => {
           <button
             className="btn flex items-center justify-start"
             onClick={() => {
-              handleThemeChange("system");
+              setTheme("system");
             }}
           >
             <MonitorCog size={20} />
