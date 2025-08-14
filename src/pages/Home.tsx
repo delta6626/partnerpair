2d393d8204fceae11c5517e54db0efe353912421
Delta6626 <hassan911ee@gmail.com>
2025-08-14 13:30:21 +0400

    remove useEffect

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,6 @@
-import { useEffect } from "react";
 import { Navbar } from "../components/navigation/Navbar";
 
 export const Home = () => {
-  useEffect(() => {
-    const prefersLight: Boolean = window.matchMedia(
-      "(prefers-color-scheme: light)"
-    ).matches;
-
-    console.log(prefersLight);
-  }, []);
-
   return (
     <div className="w-full h-[100vh] bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
       <div className="py-4">
