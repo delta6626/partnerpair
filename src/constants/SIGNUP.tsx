088ef4507f83a4de1c58ed7e1c3ce83b78ab9183
Delta6626 <hassan911ee@gmail.com>
2025-08-14 16:24:54 +0400

    constants

--- src/constants/SIGNUP.tsx
+++ src/constants/SIGNUP.tsx
@@ -2,4 +2,6 @@ export const SIGNUP = {
   greetingText: "Hi, let's get you started.",
   signupButtonText: "Continue",
   signupWithGoogleButtonText: "Continue with Google",
+  tosText:
+    "By signing up, you agree to our Terms of Service and Privacy Policy.",
 };
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -8,57 +8,64 @@ export const Signup = () => {
         <Navbar />
       </div>
 
-      <div className="w-full flex flex-col items-center">
-        <h1 className="font-bold text-4xl">{SIGNUP.greetingText}</h1>
-        <form className="mt-16 w-2/8 fieldset">
-          <div className="flex flex-col gap-2">
-            <div className="flex flex-col">
-              <div className="flex gap-2">
-                <input type="text" className="input" placeholder="First name" />
-                <input type="text" className="input" placeholder="Last name" />
+      <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <div className="w-full flex flex-col items-center">
+          <h1 className="font-bold text-4xl">{SIGNUP.greetingText}</h1>
+          <form className="mt-16 fieldset">
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-col">
+                <div className="flex gap-2">
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder="First name"
+                  />
+                  <input
+                    type="text"
+                    className="input"
+                    placeholder="Last name"
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col">
+                <input
+                  type="email"
+                  className="input w-full"
+                  placeholder="Email"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <input
+                  type="password"
+                  className="input w-full"
+                  placeholder="Password"
+                />
+              </div>
+
+              <div className="flex flex-col">
+                <input
+                  type="password"
+                  className="input w-full"
+                  placeholder="Confirm password"
+                />
+              </div>
+
+              <div className="mt-4">
+                <button type="submit" className="btn btn-primary w-full">
+                  {SIGNUP.signupButtonText}
+                </button>
+                <button className="btn mt-2 w-full">
+                  {SIGNUP.signupWithGoogleButtonText}
+                </button>
+              </div>
+              <div className="mt-4">
+                <p>{SIGNUP.tosText}</p>
               </div>
             </div>
-
-            <div className="flex flex-col">
-              <input
-                type="email"
-                className="input w-full"
-                placeholder="Email"
-              />
-            </div>
-
-            <div className="flex flex-col">
-              <input
-                type="password"
-                className="input w-full"
-                placeholder="Password"
-              />
-            </div>
-
-            <div className="flex flex-col">
-              <input
-                type="password"
-                className="input w-full"
-                placeholder="Confirm password"
-              />
-            </div>
-
-            <div className="mt-4">
-              <button className="btn btn-primary w-full">
-                {SIGNUP.signupButtonText}
-              </button>
-              <button className="btn mt-2 w-full">
-                {SIGNUP.signupWithGoogleButtonText}
-              </button>
-            </div>
-            <div className="mt-4">
-              <p>
-                By signing up, you agree to our Terms of Service and Privacy
-                Policy.
-              </p>
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   );
