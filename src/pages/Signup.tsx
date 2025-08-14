dad9091a8e019034f23eeb68295c0cc176b5eea3
Delta6626 <hassan911ee@gmail.com>
2025-08-14 15:55:44 +0400

    sign up form

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -10,6 +10,49 @@ export const Signup = () => {
 
       <div className="pt-16 w-full flex flex-col items-center">
         <h1 className="font-bold text-4xl">{SIGNUP.greeting}</h1>
+        <form className="mt-16 w-2/8 fieldset">
+          <div className="flex flex-col gap-2">
+            <div className="flex flex-col">
+              <div className="flex gap-2">
+                <input type="text" className="input" placeholder="First name" />
+                <input type="text" className="input" placeholder="Last name" />
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <input
+                type="email"
+                className="input w-full"
+                placeholder="Email"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <input
+                type="password"
+                className="input w-full"
+                placeholder="Password"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <input
+                type="password"
+                className="input w-full"
+                placeholder="Confirm password"
+              />
+            </div>
+            <div className="mt-2">
+              <button className="btn btn-primary w-full">Continue</button>
+            </div>
+            <div className="mt-2">
+              <p>
+                By signing up, you agree to our Terms of Service and Privacy
+                Policy.
+              </p>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
