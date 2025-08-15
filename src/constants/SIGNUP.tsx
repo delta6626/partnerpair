78fa56ff256ebbdcd44c3d431a481a4c3fab9d74
Delta6626 <hassan911ee@gmail.com>
2025-08-15 14:45:45 +0400

    add length limits

--- src/constants/SIGNUP.tsx
+++ src/constants/SIGNUP.tsx
@@ -4,4 +4,8 @@ export const SIGNUP = {
   SIGNUP_WITH_GOOGLE_BUTTON_TEXT: "Continue with Google",
   TOS_TEXT:
     "By signing up, you agree to our Terms of Service and Privacy Policy.",
+  MAXIMUM_FIRST_NAME_LENGTH: 50,
+  MAXIMUM_LAST_NAME_LENGTH: 50,
+  MAXIMUM_EMAIL_LENGTH: 254,
+  MAXIMUM_PASSWORD_LENGTH: 128,
 };
