674fd4421fb53e89140c972f8c59d513c3e7447f
Delta6626 <hassan911ee@gmail.com>
2025-08-15 14:32:43 +0400

    separate form logic

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,3 +1,4 @@
+import { SignupForm } from "../components/forms/SignupForm";
 import { Navbar } from "../components/navigation/Navbar";
 import { SIGNUP } from "../constants/SIGNUP";
 
@@ -13,60 +14,7 @@ export const Signup = () => {
           <h1 className="font-bold text-4xl text-center">
             {SIGNUP.greetingText}
           </h1>
-          <form className="mt-16 fieldset">
-            <div className="flex flex-col gap-2">
-              <div className="flex flex-col">
-                <div className="flex gap-2">
-                  <input
-                    type="text"
-                    className="input"
-                    placeholder="First name"
-                  />
-                  <input
-                    type="text"
-                    className="input"
-                    placeholder="Last name"
-                  />
-                </div>
-              </div>
-
-              <div className="flex flex-col">
-                <input
-                  type="email"
-                  className="input w-full"
-                  placeholder="Email"
-                />
-              </div>
-
-              <div className="flex flex-col">
-                <input
-                  type="password"
-                  className="input w-full"
-                  placeholder="Password"
-                />
-              </div>
-
-              <div className="flex flex-col">
-                <input
-                  type="password"
-                  className="input w-full"
-                  placeholder="Confirm password"
-                />
-              </div>
-
-              <div className="mt-4">
-                <button type="submit" className="btn btn-primary w-full">
-                  {SIGNUP.signupButtonText}
-                </button>
-                <button className="btn mt-2 w-full">
-                  {SIGNUP.signupWithGoogleButtonText}
-                </button>
-              </div>
-              <div className="mt-4">
-                <p>{SIGNUP.tosText}</p>
-              </div>
-            </div>
-          </form>
+          <SignupForm />
         </div>
       </div>
     </div>
