69eaca45ce4eb490b75db1891d90bcbdee86c8fc
Delta6626 <hassan911ee@gmail.com>
2025-08-17 01:28:17 +0400

    boilerplate

---
+++ src/sevices/authentication/authServices.ts
@@ -0,0 +1,4 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { firestore, auth, analytics } from "../firebaseConfig";
+
+const createUserByEmail = async (email: string, password: string) => {};
