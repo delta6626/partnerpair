2a260f1f5db0fc91c5652f3047a9ce18fe181274
Delta6626 <hassan911ee@gmail.com>
2025-08-19 00:28:23 +0400

    redirect to sign up if not authenticated

--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -1,3 +1,23 @@
+import { useEffect } from "react";
+import { getAuthenticatedUser } from "../sevices/authentication/authServices";
+import { SIGNUP } from "../constants/SIGNUP";
+import { useNavigate } from "react-router-dom";
+
 export const Verify = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const user = await getAuthenticatedUser();
+      if (user != SIGNUP.UNAUTHENTICATED) {
+        return;
+      } else {
+        navigate("/signup");
+      }
+    };
+
+    checkUser();
+  }, []);
+
   return <div className=""></div>;
 };
