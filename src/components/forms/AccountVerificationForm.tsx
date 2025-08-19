8a608c945a88e79c1b9603ede8f55ecbf2ce98b1
Delta6626 <hassan911ee@gmail.com>
2025-08-19 12:42:48 +0400

    add constants

--- src/components/forms/AccountVerificationForm.tsx
+++ src/components/forms/AccountVerificationForm.tsx
@@ -1,3 +1,4 @@
+import { VERIFY } from "../../constants/VERIFY";
 import { useInitializeUser } from "../../hooks/useInitializeUser";
 
 export const AccountVerificationForm = () => {
@@ -7,15 +8,13 @@ export const AccountVerificationForm = () => {
     <div className="w-3xl absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28 text-center">
       <div className="">
         <h1 className="font-bold text-4xl">
-          Nice to meet you, {user?.basicInfo.firstName + "."}
+          {VERIFY.WELCOME_MESSAGE + ", " + user?.basicInfo.firstName + "."}
         </h1>
         <h4 className="text-lg text-left mt-16">
-          We just need to verify your account. Click the Send Verification Email
-          button and check your inbox for a link. Once you click the link, your
-          account will be verified and this page will update automatically.
+          {VERIFY.VERIFICATON_INSTRUCTIONS}
         </h4>
         <button className="btn btn-primary w-full mt-4">
-          Send verification mail
+          {VERIFY.SEND_VERIFICATION_MAIL}
         </button>
       </div>
     </div>
---
+++ src/constants/VERIFY.tsx
@@ -0,0 +1,7 @@
+export const VERIFY = {
+  WELCOME_MESSAGE: "Nice to meet you",
+  VERIFICATON_INSTRUCTIONS:
+    "We just need to verify your account. Click the Send Verification Email button and check your inbox for a link. Once you click the link, your account will be verified and this page will update automatically.",
+  SEND_VERIFICATION_MAIL: "Send verification mail",
+  VERIFICAITON_MAIL_SENT: "Sent! Check your inbox.",
+};
