90fbf21810c5e7faf5c20d66bd78b32461e3bc82
Delta6626 <hassan911ee@gmail.com>
2025-08-19 12:39:09 +0400

    ui for verification form

--- src/components/forms/AccountVerificationForm.tsx
+++ src/components/forms/AccountVerificationForm.tsx
@@ -1,3 +1,23 @@
+import { useInitializeUser } from "../../hooks/useInitializeUser";
+
 export const AccountVerificationForm = () => {
-  return <div className=""></div>;
+  const { user } = useInitializeUser();
+
+  return (
+    <div className="w-3xl absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28 text-center">
+      <div className="">
+        <h1 className="font-bold text-4xl">
+          Nice to meet you, {user?.basicInfo.firstName + "."}
+        </h1>
+        <h4 className="text-lg text-left mt-16">
+          We just need to verify your account. Click the Send Verification Email
+          button and check your inbox for a link. Once you click the link, your
+          account will be verified and this page will update automatically.
+        </h4>
+        <button className="btn btn-primary w-full mt-4">
+          Send verification mail
+        </button>
+      </div>
+    </div>
+  );
 };
--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -1,21 +1,18 @@
+import { AccountVerificationForm } from "../components/forms/AccountVerificationForm";
 import { useInitializeUser } from "../hooks/useInitializeUser";
 import { useTheme } from "../hooks/useTheme";
 
 export const Verify = () => {
   useTheme();
 
-  const { user, loading } = useInitializeUser();
+  const { loading } = useInitializeUser();
 
   return (
     <div className="font-inter w-full h-[100vh] bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
       {loading ? (
         <p className="loading loading-spinner"></p>
       ) : (
-        <div className="w-full absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28 text-center">
-          <h1 className="font-bold text-4xl">
-            Nice to meet you, {user?.basicInfo.firstName}
-          </h1>
-        </div>
+        <AccountVerificationForm />
       )}
     </div>
   );
