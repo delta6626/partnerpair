a4e15b6c5f9f32cb9c480cd37629ad9138151692
Delta6626 <hassan911ee@gmail.com>
2025-08-20 21:04:26 +0400

    send verification email function

--- src/components/forms/AccountVerificationForm.tsx
+++ src/components/forms/AccountVerificationForm.tsx
@@ -4,6 +4,8 @@ import { useInitializeUser } from "../../hooks/useInitializeUser";
 export const AccountVerificationForm = () => {
   const { user } = useInitializeUser();
 
+  function handleAccountVerifcation() {}
+
   return (
     <div className="flex flex-grow items-center justify-center">
       <div className="w-full max-w-md">
@@ -11,7 +13,10 @@ export const AccountVerificationForm = () => {
           {VERIFY.WELCOME_MESSAGE + ", " + user?.basicInfo.firstName + "."}
         </h1>
         <h4 className="text-lg mt-8">{VERIFY.VERIFICATON_INSTRUCTIONS}</h4>
-        <button className="btn btn-primary w-full mt-6">
+        <button
+          className="btn btn-primary w-full mt-6"
+          onClick={handleAccountVerifcation}
+        >
           {VERIFY.SEND_VERIFICATION_MAIL}
         </button>
       </div>
