e8ad0171ce5ec4d6be09204307c04f6852818017
Delta6626 <hassan911ee@gmail.com>
2025-08-20 22:15:13 +0400

    add initiate flag

--- src/hooks/useEmailVerified.ts
+++ src/hooks/useEmailVerified.ts
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { getVerificationStatus } from "../sevices/authentication/authServices";
 import { VERIFY } from "../constants/VERIFY";
 
-export const useEmailVerified = () => {
+export const useEmailVerified = ({ initiate }: { initiate: boolean }) => {
   const [emailVerified, setEmailVerified] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
+    if (!initiate) return;
+
     const checkVerification = async () => {
       const status = await getVerificationStatus();
       if (typeof status === "string") {
@@ -23,7 +25,7 @@ export const useEmailVerified = () => {
     const interval = setInterval(checkVerification, VERIFY.POLL_INTERVAL);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [initiate]);
 
   return { emailVerified, error };
 };
