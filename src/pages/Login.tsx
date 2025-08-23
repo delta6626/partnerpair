e6d87847ed192e210b380d104607d98e10b780d7
Delta6626 <hassan911ee@gmail.com>
2025-08-23 19:01:26 +0400

    add navbar to login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,11 @@
+import { Navbar } from "../components/navigation/Navbar";
+
 export const Login = () => {
-  return <div className="">login</div>;
+  return (
+    <div className="font-inter w-full h-[100vh] bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28 flex flex-col">
+      <div className="py-4">
+        <Navbar />
+      </div>
+    </div>
+  );
 };
