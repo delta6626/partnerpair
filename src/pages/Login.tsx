d2c734e312a1e8797165a909ef0551bc6510e10d
Delta6626 <hassan911ee@gmail.com>
2025-08-25 21:35:37 +0400

    redirect to dasboard if logged in

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import { LoginForm } from "../components/forms/Loginform";
 import { Navbar } from "../components/navigation/Navbar";
 import { LOGIN } from "../constants/LOGIN";
+import { useEffect } from "react";
+import { getAuthenticatedUser } from "../sevices/authentication/authServices";
 
 export const Login = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const user = await getAuthenticatedUser();
+      if (typeof user === "string") return;
+
+      if (user.emailVerified) {
+        navigate("/dashboard");
+      } else {
+        navigate("/verify");
+      }
+    };
+
+    checkUser();
+  }, []);
+
   return (
     <div className="font-inter w-full h-[100vh] bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28 flex flex-col">
       <div className="py-4">
