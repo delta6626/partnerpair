3135af8b302d0715c37f600d677787e58992628f
Delta6626 <hassan911ee@gmail.com>
2025-08-26 22:08:37 +0400

    fix loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,85 +1,109 @@
 export const Loader = () => {
   return (
-    <div className="relative w-[30px] h-[30px] rounded-lg">
+    <div className="relative w-[25px] h-[25px] rounded-lg">
       <div
-        className="absolute left-1/2 top-[30%] w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 
+                   shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
         style={{
           transform: "rotate(0deg) translate(0, -130%)",
           animationDelay: "0s",
         }}
       ></div>
       <div
-        className="absolute left-1/2 top-[30%] w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 
+                   shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
         style={{
           transform: "rotate(30deg) translate(0, -130%)",
           animationDelay: "-1.1s",
         }}
       ></div>
       <div
-        className="absolute left-1/2 top-[30%] w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 
+                   shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
         style={{
           transform: "rotate(60deg) translate(0, -130%)",
           animationDelay: "-1s",
         }}
       ></div>
       <div
-        className="absolute left-1/2 top-[30%] w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 
+                   shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
         style={{
           transform: "rotate(90deg) translate(0, -130%)",
           animationDelay: "-0.9s",
         }}
       ></div>
       <div
-        className="absolute left-1/2 top-[30%] w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 
+                   shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
         style={{
           transform: "rotate(120deg) translate(0, -130%)",
           animationDelay: "-0.8s",
         }}
       ></div>
       <div
-        className="absolute left-1/2 top-[30%] w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 
+                   shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
         style={{
           transform: "rotate(150deg) translate(0, -130%)",
           animationDelay: "-0.7s",
         }}
       ></div>
       <div
-        className="absolute left-1/2 top-[30%] w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 
+                   shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
         style={{
           transform: "rotate(180deg) translate(0, -130%)",
           animationDelay: "-0.6s",
         }}
       ></div>
       <div
-        className="absolute left-1/2 top-[30%] w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 
+                   shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
         style={{
           transform: "rotate(210deg) translate(0, -130%)",
           animationDelay: "-0.5s",
         }}
       ></div>
       <div
-        className="absolute left-1/2 top-[30%] w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 
+                   shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
         style={{
           transform: "rotate(240deg) translate(0, -130%)",
           animationDelay: "-0.4s",
         }}
       ></div>
       <div
-        className="absolute left-1/2 top-[30%] w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 
+                   shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
         style={{
           transform: "rotate(270deg) translate(0, -130%)",
           animationDelay: "-0.3s",
         }}
       ></div>
       <div
-        className="absolute left-1/2 top-[30%] w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 
+                   shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
         style={{
           transform: "rotate(300deg) translate(0, -130%)",
           animationDelay: "-0.2s",
         }}
       ></div>
       <div
-        className="absolute left-1/2 top-[30%] w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
+        className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                   w-[8%] h-[24%] bg-gray-500 rounded-full opacity-0 
+                   shadow-[0_0_3px_rgba(0,0,0,0.2)] animate-[fade458_1s_linear_infinite]"
         style={{
           transform: "rotate(330deg) translate(0, -130%)",
           animationDelay: "-0.1s",
