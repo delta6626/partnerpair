9f7962b2e0c1756983f63398f4ab359a6df4fcef
Delta6626 <hassan911ee@gmail.com>
2025-08-29 02:07:44 +0400

    add profile photo section

--- src/components/user/ProfileManager.tsx
+++ src/components/user/ProfileManager.tsx
@@ -1,6 +1,6 @@
 import { UserCircle2 } from "lucide-react";
 import { useInitializeUser } from "../../hooks/useInitializeUser";
-import { useState } from "react";
+import { useState, type ChangeEvent } from "react";
 
 export const ProfileManager = () => {
   const { user } = useInitializeUser();
@@ -8,6 +8,18 @@ export const ProfileManager = () => {
   const [firstName, setFirstName] = useState(user?.basicInfo.firstName);
   const [lastName, setLastName] = useState(user?.basicInfo.lastName);
   const [email, setEmail] = useState(user?.basicInfo.email);
+
+  const handleFirstNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFirstName(e.target.value);
+  };
+
+  const handleLastNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setLastName(e.target.value);
+  };
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
 
   return (
     <div className="">
@@ -19,11 +31,23 @@ export const ProfileManager = () => {
       <div className="flex flex-row gap-2">
         <div className="">
           <p className="mt-4">First name</p>
-          <input type="text" className="mt-2 input w-96" placeholder="First name" value={firstName} />
+          <input
+            type="text"
+            className="mt-2 input w-96"
+            placeholder="First name"
+            value={firstName}
+            onChange={handleFirstNameChange}
+          />
         </div>
         <div className="">
           <p className="mt-4">Last name</p>
-          <input type="text" className="mt-2 input w-96" placeholder="Last name" value={lastName} />
+          <input
+            type="text"
+            className="mt-2 input w-96"
+            placeholder="Last name"
+            value={lastName}
+            onChange={handleLastNameChange}
+          />
         </div>
       </div>
 
@@ -35,7 +59,12 @@ export const ProfileManager = () => {
           placeholder="Email"
           value={email}
           disabled={user?.basicInfo.authenticationMethod === "Google"}
+          onChange={handleEmailChange}
         />
+      </div>
+
+      <div className="">
+        <p className="mt-4">Profile photo</p>
       </div>
     </div>
   );
