fb224ce1dbfcf893117c357c020238b4e4ab83d8
Delta6626 <hassan911ee@gmail.com>
2025-08-29 02:37:23 +0400

    change auth service

--- src/constants/SETTINGS.ts
+++ src/constants/SETTINGS.ts
@@ -1,6 +1,7 @@
 export const SETTINGS = {
   SUBTITLE_TEXT: "Manage your account preferences and profile settings.",
   PROFILE_PHOTOS: {
-    DEFAULT_PROFILE_PHOTO: "https://api.dicebear.com/9.x/micah/svg?seed=Adrian&mouth=laughing,pucker,smile,smirk",
+    DEFAULT_PROFILE_PHOTO:
+      "https://api.dicebear.com/9.x/micah/svg?seed=Adrian&backgroundType=solid,gradientLinear&mouth=laughing,pucker,smile,smirk&backgroundColor=b6e3f4,c0aede,d1d4f9,ffd5dc,ffdfbf",
   },
 };
