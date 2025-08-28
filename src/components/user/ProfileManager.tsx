dd1334b11fabc8c06a9382f4ac227988c7c7c5f6
Delta6626 <hassan911ee@gmail.com>
2025-08-28 19:07:17 +0400

    profile manager stuff

--- src/components/user/ProfileManager.tsx
+++ src/components/user/ProfileManager.tsx
@@ -1,6 +1,14 @@
 import { UserCircle2 } from "lucide-react";
+import { useInitializeUser } from "../../hooks/useInitializeUser";
+import { useState } from "react";
 
 export const ProfileManager = () => {
+  const { user } = useInitializeUser();
+
+  const [firstName, setFirstName] = useState(user?.basicInfo.firstName);
+  const [lastName, setLastName] = useState(user?.basicInfo.lastName);
+  const [email, setEmail] = useState(user?.basicInfo.email);
+
   return (
     <div className="">
       <div className="flex items-center gap-2">
@@ -11,17 +19,23 @@ export const ProfileManager = () => {
       <div className="flex flex-row gap-2">
         <div className="">
           <p className="mt-4">First name</p>
-          <input type="text" className="mt-2 input w-96" placeholder="First name" />
+          <input type="text" className="mt-2 input w-96" placeholder="First name" value={firstName} />
         </div>
         <div className="">
           <p className="mt-4">Last name</p>
-          <input type="text" className="mt-2 input w-96" placeholder="Last name" />
+          <input type="text" className="mt-2 input w-96" placeholder="Last name" value={lastName} />
         </div>
       </div>
 
       <div className="">
         <p className="mt-4">Email</p>
-        <input type="email" className="mt-2 input w-194" placeholder="Email" />
+        <input
+          type="email"
+          className="mt-2 input w-194"
+          placeholder="Email"
+          value={email}
+          disabled={user?.basicInfo.authenticationMethod === "Google"}
+        />
       </div>
     </div>
   );
