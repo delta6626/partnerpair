a68898a90e6c3729a5b7ea72944e462abff27758
Delta6626 <hassan911ee@gmail.com>
2025-08-29 02:15:50 +0400

    add default profile pic url

--- src/constants/SETTINGS.ts
+++ src/constants/SETTINGS.ts
@@ -1,3 +1,6 @@
 export const SETTINGS = {
   SUBTITLE_TEXT: "Manage your account preferences and profile settings.",
+  PROFILE_PICTURES: {
+    DEFAULT_PROFILE_PICTURE: "https://api.dicebear.com/9.x/thumbs/svg?seed=Adrian&mouthColor=ffffff&shapeColor=0a5b83",
+  },
 };
