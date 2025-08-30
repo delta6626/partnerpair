aa5aefef774189652de1ee2d7e5822ef72cd50dd
Delta6626 <hassan911ee@gmail.com>
2025-08-31 02:58:52 +0400

    create professional info manager component

---
+++ src/components/user/ProfessionalInformationManager.tsx
@@ -0,0 +1,12 @@
+import { IdCard } from "lucide-react";
+
+export const ProfessionalInformationManager = () => {
+  return (
+    <div className="">
+      <div className="flex items-center gap-2">
+        <IdCard />
+        <h1 className="text-lg">Professional Information</h1>
+      </div>
+    </div>
+  );
+};
