fdef1d06c74347d4331611f7635ec7d01d511642
Delta6626 <hassan911ee@gmail.com>
2025-09-01 23:15:59 +0400

    style + function bp

--- src/components/user/SkillHolder.tsx
+++ src/components/user/SkillHolder.tsx
@@ -1,5 +1,17 @@
+import { XIcon } from "lucide-react";
+
 export const SkillHolder = ({ skillName }: { skillName: string }) => {
+  const handleSkillDeletion = () => {};
+
   return (
-    <div className="inline-flex items-center px-4 py-2 rounded-full bg-base-200 font-medium text-sm">{skillName}</div>
+    <div className="inline-flex items-center px-4 py-3 rounded-full bg-base-200 font-medium text-sm gap-2">
+      {skillName}
+      <button
+        className="text-accent hover:text-error focus:text-error ease-in-out duration-200"
+        onClick={handleSkillDeletion}
+      >
+        <XIcon className="" size={20} />
+      </button>
+    </div>
   );
 };
