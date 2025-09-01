57d342f901e58f1c71fdeab7fc399d89802cb96c
Delta6626 <hassan911ee@gmail.com>
2025-09-01 23:02:29 +0400

    add style

--- src/components/user/SkillHolder.tsx
+++ src/components/user/SkillHolder.tsx
@@ -1,7 +1,5 @@
 export const SkillHolder = ({ skillName }: { skillName: string }) => {
   return (
-    <div className="inline-flex items-center justify-center gap-2 py-2 px-4 rounded-lg bg-base-200 font-semibold text-sm">
-      {skillName}
-    </div>
+    <div className="inline-flex items-center px-4 py-2 rounded-full bg-base-200 font-medium text-sm">{skillName}</div>
   );
 };
