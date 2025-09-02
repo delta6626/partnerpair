214128712d0fb7b2a28c6f0093d86dec3651370b
Delta6626 <hassan911ee@gmail.com>
2025-09-02 16:52:01 +0400

    REFACTOR

--- src/components/modals/AddSkillModal.tsx
+++ src/components/modals/AddSkillModal.tsx
@@ -3,7 +3,10 @@ import { MODALS } from "../../constants/MODALS";
 export const AddSkillModal = () => {
   return (
     <dialog id={MODALS.ADD_SKILL_MODAL.ID} className="modal">
-      <div className="modal-box"></div>
+      <div className="modal-box">
+        <h1 className="text-xl font-medium">{MODALS.ADD_SKILL_MODAL.TITLE}</h1>
+        <p>{MODALS.ADD_SKILL_MODAL.DESCRIPTION}</p>
+      </div>
     </dialog>
   );
 };
--- src/constants/MODALS.ts
+++ src/constants/MODALS.ts
@@ -2,5 +2,6 @@ export const MODALS = {
   ADD_SKILL_MODAL: {
     ID: "addSkillModal",
     TITLE: "Add a new Skill",
+    DESCRIPTION: "Add a new skill to your profile to showcase your expertise.",
   },
 };
