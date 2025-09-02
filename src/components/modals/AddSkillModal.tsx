e3028cbde2d3517f133d31db744c5d1c36a5c3b1
Delta6626 <hassan911ee@gmail.com>
2025-09-02 17:06:27 +0400

    change style

--- src/components/modals/AddSkillModal.tsx
+++ src/components/modals/AddSkillModal.tsx
@@ -4,8 +4,8 @@ export const AddSkillModal = () => {
   return (
     <dialog id={MODALS.ADD_SKILL_MODAL.ID} className="modal">
       <div className="modal-box">
-        <h1 className="text-xl font-medium">{MODALS.ADD_SKILL_MODAL.TITLE}</h1>
-        <p>{MODALS.ADD_SKILL_MODAL.DESCRIPTION}</p>
+        <h1 className="text-lg font-medium">{MODALS.ADD_SKILL_MODAL.TITLE}</h1>
+        <p className="text-accent">{MODALS.ADD_SKILL_MODAL.DESCRIPTION}</p>
       </div>
     </dialog>
   );
