265381ef69c072d681f0dfa5d0fa96253bbd0683
Delta6626 <hassan911ee@gmail.com>
2025-09-04 15:11:49 +0400

    change ui

--- src/components/navigation/MainNavbar.tsx
+++ src/components/navigation/MainNavbar.tsx
@@ -3,8 +3,11 @@ import { Logo } from "../branding/Logo";
 import { NotificationOverview } from "../notifications/NotificationOverview";
 import { MainNavbarNavigationItems } from "./MainNavbarNavigationItems";
 import { Link } from "react-router-dom";
+import { useUserStore } from "../../store/useUserStore";
 
 export const MainNavbar = () => {
+  const { user } = useUserStore();
+
   return (
     <div className="flex items-center justify-between">
       <div className="">
@@ -17,9 +20,23 @@ export const MainNavbar = () => {
 
       <div className="flex gap-2">
         <NotificationOverview />
-        <Link to={"/settings"} className="btn btn-square">
-          <Settings size={20} />
-        </Link>
+        <div className="">
+          <div className="dropdown dropdown-bottom dropdown-center">
+            <img
+              tabIndex={0}
+              role="button"
+              src={user?.basicInfo.profileImageUrl}
+              className="btn btn-square rounded-full"
+            />
+            <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2 shadow-sm">
+              <Link to={"/settings"} className="btn">
+                Settings
+              </Link>
+              <hr className="text-accent"></hr>
+              <button className="btn">Sign out</button>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
