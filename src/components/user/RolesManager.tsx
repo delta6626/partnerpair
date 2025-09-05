2b09c5a9ee65835dd95843dd244586401b2a1a54
Delta6626 <hassan911ee@gmail.com>
2025-09-05 22:05:44 +0400

    change copy

--- src/components/user/RolesManager.tsx
+++ src/components/user/RolesManager.tsx
@@ -1,7 +1,7 @@
 export const RolesManager = () => {
   return (
     <div className="mt-4 flex items-center justify-between">
-      <p className="">Roles</p>
+      <p className="">Roles You Play</p>
       <button className="btn btn-primary">Add roles</button>
     </div>
   );
