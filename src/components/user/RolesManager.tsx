b2e1012d85580f513ac5e3f11eb9c2a9adb604ea
Delta6626 <hassan911ee@gmail.com>
2025-09-05 22:03:46 +0400

    label and button

--- src/components/user/RolesManager.tsx
+++ src/components/user/RolesManager.tsx
@@ -1,3 +1,8 @@
 export const RolesManager = () => {
-  return <div className=""></div>;
+  return (
+    <div className="mt-4 flex items-center justify-between">
+      <p className="">Roles</p>
+      <button className="btn btn-primary">Add roles</button>
+    </div>
+  );
 };
