a26d032ffb806d4c84a37affab18f7147749bb97
Delta6626 <hassan911ee@gmail.com>
2025-09-05 22:09:27 +0400

    boilerplate for function

--- src/components/user/RolesManager.tsx
+++ src/components/user/RolesManager.tsx
@@ -4,12 +4,15 @@ import { useTempUserStore } from "../../store/useTempUserStore";
 export const RolesManager = () => {
   const { tempUser } = useTempUserStore();
 
+  const handleAddRolesButtonClick = () => {};
+
   return (
     <div className="mt-4 flex items-center justify-between">
       <p className="">Roles You Play</p>
       <button
         className="btn btn-primary"
         disabled={tempUser?.professionalInfo.roles.length === SETTINGS.MAX_ROLE_COUNT}
+        onClick={handleAddRolesButtonClick}
       >
         Add roles
       </button>
