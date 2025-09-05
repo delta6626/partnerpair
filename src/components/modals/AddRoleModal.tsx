d627b4b138f3090c3a2bc5acce4450faf3046c3f
Delta6626 <hassan911ee@gmail.com>
2025-09-05 22:15:04 +0400

    edit modal

--- src/components/modals/AddRoleModal.tsx
+++ src/components/modals/AddRoleModal.tsx
@@ -1,3 +1,12 @@
+import { MODALS } from "../../constants/MODALS";
+
 export const AddRoleModal = () => {
-  return <div className=""></div>;
+  return (
+    <dialog id={MODALS.ADD_ROLE_MODAL.ID} className="modal">
+      <div className="modal-box bg-base-300">
+        <h1 className="text-lg font-medium">{MODALS.ADD_ROLE_MODAL.TITLE}</h1>
+        <p className="text-accent">{MODALS.ADD_SKILL_MODAL.DESCRIPTION}</p>
+      </div>
+    </dialog>
+  );
 };
