6b6ad515da4651448ca0c52774e6f3f0f31cc678
Delta6626 <hassan911ee@gmail.com>
2025-09-05 22:08:11 +0400

    enforce max limit

--- src/components/user/RolesManager.tsx
+++ src/components/user/RolesManager.tsx
@@ -1,8 +1,18 @@
+import { SETTINGS } from "../../constants/SETTINGS";
+import { useTempUserStore } from "../../store/useTempUserStore";
+
 export const RolesManager = () => {
+  const { tempUser } = useTempUserStore();
+
   return (
     <div className="mt-4 flex items-center justify-between">
       <p className="">Roles You Play</p>
-      <button className="btn btn-primary">Add roles</button>
+      <button
+        className="btn btn-primary"
+        disabled={tempUser?.professionalInfo.roles.length === SETTINGS.MAX_ROLE_COUNT}
+      >
+        Add roles
+      </button>
     </div>
   );
 };
