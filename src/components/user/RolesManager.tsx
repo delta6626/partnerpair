2759d831fa17c5e051359abfff91a0b1ca19ca7b
Delta6626 <hassan911ee@gmail.com>
2025-09-05 22:17:09 +0400

    add modal to component

--- src/components/user/RolesManager.tsx
+++ src/components/user/RolesManager.tsx
@@ -1,21 +1,30 @@
+import { MODALS } from "../../constants/MODALS";
 import { SETTINGS } from "../../constants/SETTINGS";
 import { useTempUserStore } from "../../store/useTempUserStore";
+import { AddSkillModal } from "../modals/AddSkillModal";
 
 export const RolesManager = () => {
   const { tempUser } = useTempUserStore();
 
-  const handleAddRolesButtonClick = () => {};
+  const handleAddRolesButtonClick = () => {
+    const modal = document.getElementById(MODALS.ADD_ROLE_MODAL.ID) as HTMLDialogElement;
+    modal.showModal();
+  };
 
   return (
-    <div className="mt-4 flex items-center justify-between">
-      <p className="">Roles You Play</p>
-      <button
-        className="btn btn-primary"
-        disabled={tempUser?.professionalInfo.roles.length === SETTINGS.MAX_ROLE_COUNT}
-        onClick={handleAddRolesButtonClick}
-      >
-        Add roles
-      </button>
-    </div>
+    <>
+      <AddSkillModal />
+
+      <div className="mt-4 flex items-center justify-between">
+        <p className="">Roles You Play</p>
+        <button
+          className="btn btn-primary"
+          disabled={tempUser?.professionalInfo.roles.length === SETTINGS.MAX_ROLE_COUNT}
+          onClick={handleAddRolesButtonClick}
+        >
+          Add roles
+        </button>
+      </div>
+    </>
   );
 };
