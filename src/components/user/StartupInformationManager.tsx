c05ba649d081b24200dd75c1ceb7dba306769a4c
Delta6626 <hassan911ee@gmail.com>
2025-09-08 14:14:13 +0400

    add copy

--- src/components/user/StartupInformationManager.tsx
+++ src/components/user/StartupInformationManager.tsx
@@ -1,3 +1,7 @@
 export const StartupInformationManager = () => {
-  return <div className=""></div>;
+  return (
+    <div className="mt-4 flex items-center">
+      <p className="">Your Startup Background</p>
+    </div>
+  );
 };
