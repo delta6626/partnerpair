5851d0596f5ed563ef985e5f4a0dfd15e1ea4d9e
Delta6626 <hassan911ee@gmail.com>
2025-09-08 14:31:28 +0400

    add radio

--- src/components/user/StartupInformationManager.tsx
+++ src/components/user/StartupInformationManager.tsx
@@ -6,8 +6,22 @@ export const StartupInformationManager = () => {
       </div>
 
       <div className="mt-4">
-        <div className="">
+        <div className="flex items-center justify-between">
           <p>Do you have a startup?</p>
+          <div className="flex gap-4">
+            <div className="">
+              <label htmlFor="radio1" className="mr-2">
+                Yes
+              </label>
+              <input type="radio" name="radio1" className="radio radio-primary" />
+            </div>
+            <div className="">
+              <label htmlFor="radio2" className="mr-2">
+                No
+              </label>
+              <input type="radio" name="radio2" className="radio radio-primary" />
+            </div>
+          </div>
         </div>
       </div>
     </>
