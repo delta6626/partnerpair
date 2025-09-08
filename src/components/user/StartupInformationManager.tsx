50808ccd8c25f8b93f8e7babd0c252efa20390bf
Delta6626 <hassan911ee@gmail.com>
2025-09-08 14:25:28 +0400

    question

--- src/components/user/StartupInformationManager.tsx
+++ src/components/user/StartupInformationManager.tsx
@@ -1,7 +1,15 @@
 export const StartupInformationManager = () => {
   return (
-    <div className="mt-4 flex items-center">
-      <p className="">Your Startup Background</p>
-    </div>
+    <>
+      <div className="mt-4 flex items-center">
+        <p className="">Your Startup Background</p>
+      </div>
+
+      <div className="mt-4">
+        <div className="">
+          <p>Do you have a startup?</p>
+        </div>
+      </div>
+    </>
   );
 };
