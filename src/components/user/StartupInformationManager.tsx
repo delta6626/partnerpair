66d04140f4c598967d926712e1a02ae9d081445f
Delta6626 <hassan911ee@gmail.com>
2025-09-08 15:23:10 +0400

    add more radios

--- src/components/user/StartupInformationManager.tsx
+++ src/components/user/StartupInformationManager.tsx
@@ -14,6 +14,10 @@ export const StartupInformationManager = () => {
     setTempUser({ ...tempUser, professionalInfo: { ...tempUser.professionalInfo, hasStartup: false } });
   };
 
+  const handleUserHasIdea = () => {};
+
+  const handleUserDoesNotHaveIdea = () => {};
+
   return (
     <div className="max-w-200 border-1 border-accent rounded-3xl p-4">
       <div className="flex items-center gap-2">
@@ -41,6 +45,30 @@ export const StartupInformationManager = () => {
                 className="radio radio-primary"
                 checked={tempUser.professionalInfo.hasStartup === false}
                 onChange={handleUserDoesNotHaveStartup}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-2 flex items-center justify-between">
+          <p>Do you have an idea for a startup?</p>
+          <div className="flex gap-4 ">
+            <div className="flex gap-2">
+              <p>Yes</p>
+              <input
+                type="radio"
+                className="radio radio-primary"
+                checked={tempUser?.professionalInfo.hasStartup === true ? true : false}
+                onChange={handleUserHasIdea}
+              />
+            </div>
+            <div className="flex gap-2">
+              <p>No</p>
+              <input
+                type="radio"
+                className="radio radio-primary"
+                checked={tempUser.professionalInfo.hasStartup === false}
+                onChange={handleUserDoesNotHaveIdea}
               />
             </div>
           </div>
