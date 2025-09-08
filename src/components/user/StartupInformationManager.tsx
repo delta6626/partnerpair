85bb6d1d666f48f3a18e3ee2cfbb7b7833f6d759
Delta6626 <hassan911ee@gmail.com>
2025-09-08 14:34:47 +0400

    add icon

--- src/components/user/StartupInformationManager.tsx
+++ src/components/user/StartupInformationManager.tsx
@@ -1,7 +1,10 @@
+import { Rocket } from "lucide-react";
+
 export const StartupInformationManager = () => {
   return (
     <div className="max-w-200 border-1 border-accent rounded-3xl p-4">
       <div className="flex items-center gap-2">
+        <Rocket />
         <h1 className="text-lg">Statup Information</h1>
       </div>
     </div>
