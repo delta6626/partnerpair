7947ee0d33b4cd36a850b2fb457ae174a554a57f
Delta6626 <hassan911ee@gmail.com>
2025-09-10 15:27:38 +0400

    add icon

--- src/components/user/SocialLinksManager.tsx
+++ src/components/user/SocialLinksManager.tsx
@@ -1,7 +1,10 @@
+import { Globe } from "lucide-react";
+
 export const SocialLinksManager = () => {
   return (
     <div className="max-w-200 border-1 border-accent rounded-3xl p-4">
       <div className="flex items-center gap-2">
+        <Globe />
         <h1 className="text-lg">Social Profiles</h1>
       </div>
     </div>
