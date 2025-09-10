249a1b8ee9e04a4c21290cbc9e937c035de7ad8c
Delta6626 <hassan911ee@gmail.com>
2025-09-10 15:25:53 +0400

    title

--- src/components/user/SocialLinksManager.tsx
+++ src/components/user/SocialLinksManager.tsx
@@ -1,3 +1,9 @@
 export const SocialLinksManager = () => {
-  return <div className=""></div>;
+  return (
+    <div className="max-w-200 border-1 border-accent rounded-3xl p-4">
+      <div className="flex items-center gap-2">
+        <h1 className="text-lg">Social Profiles</h1>
+      </div>
+    </div>
+  );
 };
