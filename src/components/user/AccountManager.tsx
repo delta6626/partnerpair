f25536f0708f79bd5827aabdfb80ebefdf110f47
Delta6626 <hassan911ee@gmail.com>
2025-09-11 15:29:22 +0400

    add theme changer

--- src/components/user/AccountManager.tsx
+++ src/components/user/AccountManager.tsx
@@ -1,11 +1,29 @@
 import { User2 } from "lucide-react";
+import { useTheme } from "../../hooks/useTheme";
+import type { ChangeEvent } from "react";
+import type { AppTheme } from "../../types/AppTheme";
 
 export const AccountManager = () => {
+  const { theme, setTheme } = useTheme();
+
+  const handleThemeChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setTheme(e.target.value as AppTheme);
+  };
+
   return (
     <div className="max-w-200 border-1 border-accent rounded-3xl p-4">
       <div className="flex items-center gap-2">
         <User2 />
         <h1>Account</h1>
+      </div>
+
+      <div className="mt-4 flex items-center justify-between">
+        <p>App theme</p>
+        <select className="select" value={theme} onChange={handleThemeChange}>
+          <option value="light">Light</option>
+          <option value="dark">Dark</option>
+          <option value="system">System</option>
+        </select>
       </div>
     </div>
   );
