95eb0f493b5f8f136c72b9569e088758271e7ab4
Delta6626 <hassan911ee@gmail.com>
2025-09-11 15:22:29 +0400

    create heading

--- src/components/user/AccountManager.tsx
+++ src/components/user/AccountManager.tsx
@@ -1,3 +1,12 @@
+import { User2 } from "lucide-react";
+
 export const AccountManager = () => {
-  return <div className=""></div>;
+  return (
+    <div className="max-w-200 border-1 border-accent rounded-3xl p-4">
+      <div className="flex items-center gap-2">
+        <User2 />
+        <h1>Account</h1>
+      </div>
+    </div>
+  );
 };
