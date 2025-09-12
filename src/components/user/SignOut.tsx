175e7a2f461fd7ef091ac31f99fd2fbab234f36d
Delta6626 <hassan911ee@gmail.com>
2025-09-12 18:05:13 +0400

    add icons

--- src/components/user/SignOut.tsx
+++ src/components/user/SignOut.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import Loader from "../Loader";
+import { LogOut } from "lucide-react";
+
 export const SignOut = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+
   const handleSignOut = () => {
     return;
   };
 
-  return <button className="btn" onClick={handleSignOut}></button>;
+  return (
+    <button className="btn" onClick={handleSignOut}>
+      {loading ? <Loader /> : <LogOut size={20} />}
+      Sign out
+    </button>
+  );
 };
