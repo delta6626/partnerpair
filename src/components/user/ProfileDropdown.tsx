5796b4f3e77a8334a0de11016b020a33b06a516a
Delta6626 <hassan911ee@gmail.com>
2025-09-12 18:07:24 +0400

    remove sign out button from profile drop down

--- src/components/user/ProfileDropdown.tsx
+++ src/components/user/ProfileDropdown.tsx
@@ -1,27 +1,9 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useUserStore } from "../../store/useUserStore";
-import { LogOut, Settings } from "lucide-react";
-import { signOut } from "../../sevices/authentication/authServices";
-import { SIGNUP } from "../../constants/SIGNUP";
-import { useState } from "react";
-import Loader from "../Loader";
+import { Settings } from "lucide-react";
 
 export const ProfileDropdown = () => {
-  const navigate = useNavigate();
   const { user } = useUserStore();
-  const [loading, setLoading] = useState(false);
-
-  const handleSignOut = async () => {
-    setLoading(true);
-    const status = await signOut();
-    setLoading(false);
-
-    if (status === SIGNUP.SIGNOUT_SUCCESS) {
-      navigate("/login");
-    } else {
-      return; // Handle error case.
-    }
-  };
 
   return (
     <div className="dropdown dropdown-bottom dropdown-center">
@@ -32,10 +14,6 @@ export const ProfileDropdown = () => {
           Settings
         </Link>
         <hr className="text-accent"></hr>
-        <button className="btn" onClick={handleSignOut}>
-          {loading ? <Loader /> : <LogOut size={20} />}
-          Sign out
-        </button>
       </ul>
     </div>
   );
