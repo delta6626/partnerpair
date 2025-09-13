e10e0f6d917de91901790d3e752ba240d24051c6
Delta6626 <hassan911ee@gmail.com>
2025-09-13 23:44:22 +0400

    add update function

--- src/sevices/userProfile/userProfileServices.ts
+++ src/sevices/userProfile/userProfileServices.ts
@@ -1,8 +1,9 @@
+import type { User } from "firebase/auth";
 import { SIGNUP } from "../../constants/SIGNUP";
 import { getUserId } from "../authentication/authServices";
 import { handleFirebaseError } from "../authentication/firebaseErrorHandler";
 import { firestore } from "../firebaseConfig";
-import { doc, updateDoc } from "firebase/firestore";
+import { doc, setDoc, updateDoc } from "firebase/firestore";
 
 export const setVerificationStatus = async (status: boolean) => {
   const userId = await getUserId();
@@ -13,6 +14,17 @@ export const setVerificationStatus = async (status: boolean) => {
   try {
     await updateDoc(userDocRef, { "basicInfo.verified": status });
     return true;
+  } catch (error: any) {
+    return handleFirebaseError(error);
+  }
+};
+
+export const updateUserProfile = async (updatedUserProfile: User) => {
+  const userId = await getUserId();
+  const userDocRef = doc(firestore, "users", userId);
+
+  try {
+    await setDoc(userDocRef, updatedUserProfile);
   } catch (error: any) {
     return handleFirebaseError(error);
   }
