78af43b4cb869e0604153bd143fa292fe3d6f8aa
Delta6626 <hassan911ee@gmail.com>
2025-09-18 22:36:21 +0400

    add title

--- src/components/user/MatchingPreferenceManager.tsx
+++ src/components/user/MatchingPreferenceManager.tsx
@@ -1,3 +1,9 @@
 const MatchingPreferenceManager = () => {
-  return <div className=""></div>;
+  return (
+    <div className="max-w-200 border-1 border-accent rounded-3xl p-4">
+      <div className="flex items-center gap-2">
+        <h1 className="text-lg">Matching Preferences</h1>
+      </div>
+    </div>
+  );
 };
