478256676667e3ae046962fe6a9773a96a897b51
Delta6626 <hassan911ee@gmail.com>
2025-09-18 22:37:11 +0400

    add icon

--- src/components/user/MatchingPreferenceManager.tsx
+++ src/components/user/MatchingPreferenceManager.tsx
@@ -1,7 +1,10 @@
+import { Handshake } from "lucide-react";
+
 const MatchingPreferenceManager = () => {
   return (
     <div className="max-w-200 border-1 border-accent rounded-3xl p-4">
       <div className="flex items-center gap-2">
+        <Handshake />
         <h1 className="text-lg">Matching Preferences</h1>
       </div>
     </div>
