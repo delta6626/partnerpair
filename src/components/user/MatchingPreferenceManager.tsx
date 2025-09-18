84d8d2a2cb28a6c70f5c2adb1ad56097fbbaa667
Delta6626 <hassan911ee@gmail.com>
2025-09-18 22:37:22 +0400

    add export

--- src/components/user/MatchingPreferenceManager.tsx
+++ src/components/user/MatchingPreferenceManager.tsx
@@ -1,6 +1,6 @@
 import { Handshake } from "lucide-react";
 
-const MatchingPreferenceManager = () => {
+export const MatchingPreferenceManager = () => {
   return (
     <div className="max-w-200 border-1 border-accent rounded-3xl p-4">
       <div className="flex items-center gap-2">
