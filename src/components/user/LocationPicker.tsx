184f99095c0ed28ba23617273c53ad860aaf3e73
Delta6626 <hassan911ee@gmail.com>
2025-09-19 22:08:38 +0400

    add placeholder

--- src/components/user/LocationPicker.tsx
+++ src/components/user/LocationPicker.tsx
@@ -3,5 +3,10 @@ import { loadCityNames } from "../../utils/loadCityNames";
 const cities = await loadCityNames();
 
 export const LocationPicker = ({ forCurrentUser }: { forCurrentUser: boolean }) => {
-  return <div className=""></div>;
+  return (
+    <div className="mt-4 flex items-center justify-between">
+      <p className="">{forCurrentUser ? "Your Location" : "Preferred Cofounder Locations"}</p>
+      <input type="text" className="input" placeholder="Search a city" />
+    </div>
+  );
 };
