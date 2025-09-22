3bfec48d69e8f04de70cb121e2fbec509560d745
Delta6626 <hassan911ee@gmail.com>
2025-09-22 21:54:45 +0400

    add commitment levels

--- src/components/user/CommitmentLevelManager.tsx
+++ src/components/user/CommitmentLevelManager.tsx
@@ -1,3 +1,14 @@
+import type { UserCommitmentLevel } from "../../types/UserCommitmentLevel";
+
 export const CommitmentLevelManager = () => {
+  const possibleCommitmentLevels: UserCommitmentLevel[] = [
+    "10 to 20 hours per week",
+    "20 to 30 hours per week",
+    "30 to 40 hours per week",
+    "40 to 50 hours per week",
+    "50 to 60 hours per week",
+    "More than 60 hours per week",
+  ];
+
   return <div className=""></div>;
 };
