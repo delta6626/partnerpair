8d2c62f0c61f2eafee17b27e00f408f9cb677be6
Delta6626 <hassan911ee@gmail.com>
2025-09-22 21:56:37 +0400

    remove unused styles

--- src/components/user/RolesManager.tsx
+++ src/components/user/RolesManager.tsx
@@ -17,8 +17,8 @@ export const RolesManager = ({ forCurrentUser }: { forCurrentUser: boolean }) =>
 
   return (
     <>
-      <div className="mt-4 flex items-center justify-between">
-        <p className="">{forCurrentUser ? "Roles You Play" : "Preferred Cofounder Roles"}</p>
+      <div className="mt-4">
+        <p>{forCurrentUser ? "Roles You Play" : "Preferred Cofounder Roles"}</p>
       </div>
 
       <div className="mt-2 flex flex-wrap gap-2">
