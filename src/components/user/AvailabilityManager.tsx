c90da1259dee7954a148cec7450219db2062be9f
Delta6626 <hassan911ee@gmail.com>
2025-09-23 17:39:28 +0400

    add possible options

--- src/components/user/AvailabilityManager.tsx
+++ src/components/user/AvailabilityManager.tsx
@@ -1,4 +1,14 @@
+import type { UserAvailability } from "../../types/UserAvailability";
+
 export const AvailabilityManager = () => {
+  const possibleAvailabilities: UserAvailability[] = [
+    "Available immediately",
+    "Available within a week",
+    "Available within 2 weeks",
+    "Available within a month",
+    "Available after a month",
+  ];
+
   return (
     <div className="mt-4 w-full flex items-center justify-between">
       <p>When would you like cofounders to be available?</p>
