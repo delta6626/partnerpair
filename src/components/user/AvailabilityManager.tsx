3fb958f177af3c3779fedbcb95d0ecfe78b3ec6e
Delta6626 <hassan911ee@gmail.com>
2025-09-23 17:40:57 +0400

    create select

--- src/components/user/AvailabilityManager.tsx
+++ src/components/user/AvailabilityManager.tsx
@@ -12,6 +12,7 @@ export const AvailabilityManager = () => {
   return (
     <div className="mt-4 w-full flex items-center justify-between">
       <p>When would you like cofounders to be available?</p>
+      <select className="select"></select>
     </div>
   );
 };
