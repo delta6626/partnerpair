cd6ab5388f9db3c7ced7c5ed81ef5fc1dbddca9c
Delta6626 <hassan911ee@gmail.com>
2025-09-23 17:44:30 +0400

    render options

--- src/components/user/AvailabilityManager.tsx
+++ src/components/user/AvailabilityManager.tsx
@@ -15,7 +15,19 @@ export const AvailabilityManager = () => {
   return (
     <div className="mt-4 w-full flex items-center justify-between">
       <p>When would you like cofounders to be available?</p>
-      <select className="select" value={tempUser?.matchingPreferences.availability as string}></select>
+      <select className="select" value={tempUser?.matchingPreferences.availability as string}>
+        <option value={""} disabled>
+          Select an option
+        </option>
+
+        {possibleAvailabilities.map((availability, id) => {
+          return (
+            <option key={id} value={availability as string}>
+              {availability}
+            </option>
+          );
+        })}
+      </select>
     </div>
   );
 };
