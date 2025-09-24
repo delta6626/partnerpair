8a87febd9f6b64528eda4f6c91f51dc58a58f1af
Delta6626 <hassan911ee@gmail.com>
2025-09-24 17:47:11 +0400

    refactor

--- src/components/user/AvailabilityManager.tsx
+++ src/components/user/AvailabilityManager.tsx
@@ -23,7 +23,7 @@ export const AvailabilityManager = ({ forCurrentUser }: { forCurrentUser: boolea
 
   return (
     <div className="mt-4 w-full flex items-center justify-between">
-      <p>When would you like cofounders to be available?</p>
+      <p>{forCurrentUser ? "Your Availability" : "When would you like cofounders to be available?"}</p>
       <select
         className="select"
         value={tempUser?.matchingPreferences.availability ?? ""}
