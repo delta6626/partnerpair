54527b6e15ab80ccbdfc6a46f4d73ba53c745f1d
Delta6626 <hassan911ee@gmail.com>
2025-09-25 23:03:34 +0400

    render stages

--- src/components/user/CompanyStagesManager.tsx
+++ src/components/user/CompanyStagesManager.tsx
@@ -1,6 +1,9 @@
+import { useTempUserStore } from "../../store/useTempUserStore";
 import type { UserPreferredCompanyStage } from "../../types/UserPreferredCompanyStage";
 
 export const CompanyStagesManager = () => {
+  const { tempUser, setTempUser } = useTempUserStore();
+
   const possibleStages: UserPreferredCompanyStage[] = [
     "Idea",
     "Building MVP",
@@ -10,8 +13,24 @@ export const CompanyStagesManager = () => {
   ];
 
   return (
-    <div className="w-full">
-      <p></p>
+    <div className="w-full mt-4">
+      <p>Preferred Company Stages</p>
+
+      <div className="">
+        {tempUser?.matchingPreferences.preferredCompanyStage.length != 0 ? (
+          tempUser?.matchingPreferences.preferredCompanyStage.map((companyStage) => {
+            return (
+              <button className="btn inline-flex items-center px-4 py-3 rounded-full border-1 border-accent select-none font-medium text-sm gap-2">
+                {companyStage}
+              </button>
+            );
+          })
+        ) : (
+          <div className="w-full text-center text-accent mt-2">
+            <p>Choose your preferred startup stages.</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
