f55a11bbe6b7918389112710dd0a3ab2b1bea8d3
Delta6626 <hassan911ee@gmail.com>
2025-09-28 18:07:09 +0400

    add placeholder

--- src/components/user/ProfileManager.tsx
+++ src/components/user/ProfileManager.tsx
@@ -103,6 +103,7 @@ export const ProfileManager = () => {
         <div className="w-full">
           <p className="mt-4">Phone</p>
           <PhoneInput
+            placeHolder="Phone number"
             className="mt-2 input w-full"
             value={tempUser.basicInfo.phone as PhoneNumber}
             onChange={(value) => {
