01bbe114043807a0db0195dcdd55d1f8aae0765e
Delta6626 <hassan911ee@gmail.com>
2025-10-01 23:20:11 +0400

    add icon

--- src/components/user/ProfileStatusMessage.tsx
+++ src/components/user/ProfileStatusMessage.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useInitializeUser } from "../../hooks/useInitializeUser";
+import { X } from "lucide-react";
 
 export const ProfileStatusMessage = () => {
   const { user } = useInitializeUser();
@@ -8,8 +9,9 @@ export const ProfileStatusMessage = () => {
   return (
     <div className="">
       {!user?.basicInfo.profileCompleted && visible ? (
-        <div className="bg-error rounded-md p-4 text-error-content">
+        <div className="flex items-center justify-between bg-error rounded-md p-4 text-error-content">
           <p>Complete your profile</p>
+          <X size={20} />
         </div>
       ) : (
         ""
