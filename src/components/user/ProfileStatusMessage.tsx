17c7ee4905cbd866dd9cad4704d8941fc0a9089f
Delta6626 <hassan911ee@gmail.com>
2025-10-01 23:17:35 +0400

    add state

--- src/components/user/ProfileStatusMessage.tsx
+++ src/components/user/ProfileStatusMessage.tsx
@@ -1,16 +1,18 @@
+import { useState } from "react";
 import { useInitializeUser } from "../../hooks/useInitializeUser";
 
 export const ProfileStatusMessage = () => {
   const { user } = useInitializeUser();
+  const [visible, setVisible] = useState(true);
 
   return (
     <div className="">
-      {user?.basicInfo.profileCompleted ? (
-        ""
-      ) : (
+      {!user?.basicInfo.profileCompleted && visible ? (
         <div className="bg-error rounded-md p-4 text-error-content">
           <p>Complete your profile</p>
         </div>
+      ) : (
+        ""
       )}
     </div>
   );
