decb05d4932c9126c81da6f7bb67a3173aae3fdf
Delta6626 <hassan911ee@gmail.com>
2025-10-01 22:57:11 +0400

    add icon

--- src/components/navigation/MainNavbar.tsx
+++ src/components/navigation/MainNavbar.tsx
@@ -2,8 +2,12 @@ import { Logo } from "../branding/Logo";
 import { NotificationOverview } from "../notifications/NotificationOverview";
 import { MainNavbarNavigationItems } from "./MainNavbarNavigationItems";
 import { ProfileDropdown } from "../user/ProfileDropdown";
+import { useInitializeUser } from "../../hooks/useInitializeUser";
+import { AlertCircle } from "lucide-react";
 
 export const MainNavbar = () => {
+  const { user } = useInitializeUser();
+
   return (
     <div className="flex items-center justify-between">
       <div className="">
@@ -16,8 +20,13 @@ export const MainNavbar = () => {
 
       <div className="flex gap-2">
         <NotificationOverview />
-        <div className="">
+        <div className="flex flex-row items-end">
           <ProfileDropdown />
+          {user?.basicInfo.profileCompleted ? (
+            ""
+          ) : (
+            <AlertCircle size={20} className="bg-error rounded-full text-white" />
+          )}
         </div>
       </div>
     </div>
