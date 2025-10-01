631c744ce5c83f13d259dad227647998a66f0612
Delta6626 <hassan911ee@gmail.com>
2025-10-01 23:07:15 +0400

    remove

--- src/components/navigation/MainNavbar.tsx
+++ src/components/navigation/MainNavbar.tsx
@@ -2,12 +2,8 @@ import { Logo } from "../branding/Logo";
 import { NotificationOverview } from "../notifications/NotificationOverview";
 import { MainNavbarNavigationItems } from "./MainNavbarNavigationItems";
 import { ProfileDropdown } from "../user/ProfileDropdown";
-import { useInitializeUser } from "../../hooks/useInitializeUser";
-import { Info } from "lucide-react";
 
 export const MainNavbar = () => {
-  const { user } = useInitializeUser();
-
   return (
     <div className="flex items-center justify-between">
       <div className="">
@@ -20,10 +16,7 @@ export const MainNavbar = () => {
 
       <div className="flex gap-2">
         <NotificationOverview />
-        <div className="flex flex-row items-end">
-          <ProfileDropdown />
-          {user?.basicInfo.profileCompleted ? "" : <Info size={20} className="bg-error rounded-full text-white" />}
-        </div>
+        <ProfileDropdown />
       </div>
     </div>
   );
