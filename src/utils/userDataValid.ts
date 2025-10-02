7e5a223b5dc5f262c3f2fd95ebcc90d0bd5347bf
Delta6626 <hassan911ee@gmail.com>
2025-10-02 21:29:22 +0400

    age validity

--- src/utils/userDataValid.ts
+++ src/utils/userDataValid.ts
@@ -1,10 +1,12 @@
 import { SIGNUP } from "../constants/SIGNUP";
 import type { User } from "../types/User";
+import { isValidAge } from "./isValidAge";
 import { isValidEmail } from "./isValidEmail";
 import { isValidLength } from "./isValidLength";
 
 export const userDataValid = (user: User) => {
   if (!isValidLength(user.basicInfo.firstName, SIGNUP.MINIMUM_FIRST_NAME_LENGTH)) return false;
   if (!isValidLength(user.basicInfo.lastName, SIGNUP.MINIMUM_LAST_NAME_LENGTH)) return false;
+  if (!isValidAge(user.basicInfo.dateOfBirth, SIGNUP.MINIMUM_AGE, SIGNUP.MAXIMUM_AGE)) return false;
   if (!isValidEmail(user.basicInfo.email)) return false;
 };
