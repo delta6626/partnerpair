0c6e39b9c3c6b305fd40d40c69a68aa4dbff092f
Delta6626 <hassan911ee@gmail.com>
2025-10-02 21:26:43 +0400

    fix bug

--- src/utils/userDataValid.ts
+++ src/utils/userDataValid.ts
@@ -6,5 +6,5 @@ import { isValidLength } from "./isValidLength";
 export const userDataValid = (user: User) => {
   if (!isValidLength(user.basicInfo.firstName, SIGNUP.MINIMUM_FIRST_NAME_LENGTH)) return false;
   if (!isValidLength(user.basicInfo.lastName, SIGNUP.MINIMUM_LAST_NAME_LENGTH)) return false;
-  if (isValidEmail(user.basicInfo.email)) return false;
+  if (!isValidEmail(user.basicInfo.email)) return false;
 };
