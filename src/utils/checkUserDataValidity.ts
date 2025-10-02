1e4b6501cdf6ee837ca11ba4360ceb4929bad9bb
Delta6626 <hassan911ee@gmail.com>
2025-10-02 21:23:32 +0400

    check email validity

--- src/utils/checkUserDataValidity.ts
+++ src/utils/checkUserDataValidity.ts
@@ -1,8 +1,10 @@
 import { SIGNUP } from "../constants/SIGNUP";
 import type { User } from "../types/User";
+import { isValidEmail } from "./isValidEmail";
 import { isValidLength } from "./isValidLength";
 
 export const checkUserDataValidity = (user: User) => {
   if (!isValidLength(user.basicInfo.firstName, SIGNUP.MINIMUM_FIRST_NAME_LENGTH)) return false;
   if (!isValidLength(user.basicInfo.lastName, SIGNUP.MINIMUM_LAST_NAME_LENGTH)) return false;
+  if (isValidEmail(user.basicInfo.email)) return false;
 };
