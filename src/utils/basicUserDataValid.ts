e5f4b869439a163f00d0fd3cff409de3a8a7d6b1
Delta6626 <hassan911ee@gmail.com>
2025-10-02 21:33:27 +0400

    rename

--- src/utils/basicUserDataValid.ts
+++ src/utils/basicUserDataValid.ts
@@ -4,7 +4,7 @@ import { isValidAge } from "./isValidAge";
 import { isValidEmail } from "./isValidEmail";
 import { isValidLength } from "./isValidLength";
 
-export const userDataValid = (user: User) => {
+export const basicUserDataValid = (user: User) => {
   if (!isValidLength(user.basicInfo.firstName, SIGNUP.MINIMUM_FIRST_NAME_LENGTH)) return false;
   if (!isValidLength(user.basicInfo.lastName, SIGNUP.MINIMUM_LAST_NAME_LENGTH)) return false;
   if (!isValidAge(user.basicInfo.dateOfBirth, SIGNUP.MINIMUM_AGE, SIGNUP.MAXIMUM_AGE)) return false;
