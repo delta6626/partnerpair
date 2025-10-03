b2a2a4fed1a0ea82e14ea8f1a5727398357f3d8c
Delta6626 <hassan911ee@gmail.com>
2025-10-03 20:37:53 +0400

    add logic

--- src/utils/profileComplete.ts
+++ src/utils/profileComplete.ts
@@ -4,4 +4,8 @@ export const profileComplete = (user: User) => {
   if (user.basicInfo.phone === "") return false;
   if (user.professionalInfo.headline === "") return false;
   if (user.professionalInfo.bio === "") return false;
+  if (user.professionalInfo.skills.length === 0) return false;
+  if (user.professionalInfo.roles.length === 0) return false;
+  if (user.professionalInfo.commitmentLevel === null) return false;
+  if (user.professionalInfo.availability === null) return false;
 };
