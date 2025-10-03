6ba9dee04ba4b1ed24704dcfae992ccc3c7975b2
Delta6626 <hassan911ee@gmail.com>
2025-10-03 20:35:13 +0400

    add logic

--- src/utils/profileComplete.ts
+++ src/utils/profileComplete.ts
@@ -1,3 +1,7 @@
 import type { User } from "../types/User";
 
-export const profileComplete = (user: User) => {};
+export const profileComplete = (user: User) => {
+  if (user.basicInfo.phone === "") return false;
+  if (user.professionalInfo.headline === "") return false;
+  if (user.professionalInfo.bio === "") return false;
+};
