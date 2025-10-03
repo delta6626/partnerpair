a6ff026c4261a81c42ca7c2a57aad53cfb690cf4
Delta6626 <hassan911ee@gmail.com>
2025-10-03 20:56:01 +0400

    add logic

--- src/utils/profileComplete.ts
+++ src/utils/profileComplete.ts
@@ -8,4 +8,14 @@ export const profileComplete = (user: User) => {
   if (user.professionalInfo.roles.length === 0) return false;
   if (user.professionalInfo.commitmentLevel === null) return false;
   if (user.professionalInfo.availability === null) return false;
+  if (
+    user.professionalInfo.hasStartup === null &&
+    user.professionalInfo.hasStartupIdea === null &&
+    user.professionalInfo.wantsToCofound === null
+  )
+    return false;
+
+  if (user.professionalInfo.hasStartup || user.professionalInfo.hasStartupIdea) {
+    if (user.professionalInfo.startupDescription === "" || user.professionalInfo.startupStage === null) return false;
+  }
 };
