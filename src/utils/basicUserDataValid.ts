ff0f7456645d2b7ac7d7caf889aefd5df79a56f0
Delta6626 <hassan911ee@gmail.com>
2025-10-05 23:34:10 +0400

    test links

--- src/utils/basicUserDataValid.ts
+++ src/utils/basicUserDataValid.ts
@@ -3,12 +3,17 @@ import type { User } from "../types/User";
 import { isValidAge } from "./isValidAge";
 import { isValidEmail } from "./isValidEmail";
 import { isValidLength } from "./isValidLength";
+import { isValidProfileLink } from "./isValidProfileLink";
 
 export const basicUserDataValid = (user: User) => {
   if (!isValidLength(user.basicInfo.firstName, SIGNUP.MINIMUM_FIRST_NAME_LENGTH)) return false;
   if (!isValidLength(user.basicInfo.lastName, SIGNUP.MINIMUM_LAST_NAME_LENGTH)) return false;
   if (!isValidAge(user.basicInfo.dateOfBirth, SIGNUP.MINIMUM_AGE, SIGNUP.MAXIMUM_AGE)) return false;
   if (!isValidEmail(user.basicInfo.email)) return false;
+  if (!isValidProfileLink("linkedin", user.socialLinks.linkedin)) return false;
+  if (!isValidProfileLink("twitter", user.socialLinks.twitter)) return false;
+  if (!isValidProfileLink("github", user.socialLinks.github)) return false;
+  if (!isValidProfileLink("website", user.socialLinks.website)) return false;
 
   return true;
 };
