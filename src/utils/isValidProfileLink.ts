a2c5e453be87c7e7e31c3e2f8176f777a43a6748
Delta6626 <hassan911ee@gmail.com>
2025-10-05 23:40:20 +0400

    fix bug

--- src/utils/isValidProfileLink.ts
+++ src/utils/isValidProfileLink.ts
@@ -3,12 +3,12 @@ import type { SocialProfile } from "../types/SocialProfile";
 export const isValidProfileLink = (type: SocialProfile, profileLink: string) => {
   const patterns: Record<SocialProfile, RegExp> = {
     linkedin: /^https:\/\/(www\.)?linkedin\.com\/in\/[A-Za-z0-9_-]+\/?$/,
-    twitter: /^https:\/\/github\.com\/[A-Za-z0-9_-]+\/?$/,
-    github: /^https:\/\/x\.com\/[A-Za-z0-9_-]+\/?$/,
-    website: /^https:\/\/[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)+([\/?#].*)?$/, // Any valid domain
+    twitter: /^https:\/\/x\.com\/[A-Za-z0-9_-]+\/?$/,
+    github: /^https:\/\/github\.com\/[A-Za-z0-9_-]+\/?$/,
+    website: /^https:\/\/[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)+([\/?#].*)?$/,
   };
   const pattern: RegExp = patterns[type];
 
-  if (profileLink === "") return true;
+  if (profileLink === "" || profileLink === null) return true;
   return pattern.test(profileLink.trim());
 };
