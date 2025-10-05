b29bd89cfbb790a05fc4da554ac6e3b715dd6da2
Delta6626 <hassan911ee@gmail.com>
2025-10-05 23:43:12 +0400

    allow http and twitter.com domain

--- src/utils/isValidProfileLink.ts
+++ src/utils/isValidProfileLink.ts
@@ -2,11 +2,12 @@ import type { SocialProfile } from "../types/SocialProfile";
 
 export const isValidProfileLink = (type: SocialProfile, profileLink: string) => {
   const patterns: Record<SocialProfile, RegExp> = {
-    linkedin: /^https:\/\/(www\.)?linkedin\.com\/in\/[A-Za-z0-9_-]+\/?$/,
-    twitter: /^https:\/\/x\.com\/[A-Za-z0-9_-]+\/?$/,
-    github: /^https:\/\/github\.com\/[A-Za-z0-9_-]+\/?$/,
-    website: /^https:\/\/[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)+([\/?#].*)?$/,
+    linkedin: /^https?:\/\/(www\.)?linkedin\.com\/in\/[A-Za-z0-9_-]+\/?$/,
+    twitter: /^https?:\/\/(x|twitter)\.com\/[A-Za-z0-9_-]+\/?$/,
+    github: /^https?:\/\/github\.com\/[A-Za-z0-9_-]+\/?$/,
+    website: /^https?:\/\/[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)+([\/?#].*)?$/,
   };
+
   const pattern: RegExp = patterns[type];
 
   if (profileLink === "" || profileLink === null) return true;
