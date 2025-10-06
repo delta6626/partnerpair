d32603d23c47a30ce34086e11cab4dd7e0defefa
Delta6626 <hassan911ee@gmail.com>
2025-10-06 23:20:01 +0400

    remove age validation

--- src/utils/basicUserDataValid.ts
+++ src/utils/basicUserDataValid.ts
@@ -1,6 +1,5 @@
 import { SIGNUP } from "../constants/SIGNUP";
 import type { User } from "../types/User";
-import { isValidAge } from "./isValidAge";
 import { isValidEmail } from "./isValidEmail";
 import { isValidLength } from "./isValidLength";
 import { isValidProfileLink } from "./isValidProfileLink";
@@ -8,7 +7,6 @@ import { isValidProfileLink } from "./isValidProfileLink";
 export const basicUserDataValid = (user: User) => {
   if (!isValidLength(user.basicInfo.firstName, SIGNUP.MINIMUM_FIRST_NAME_LENGTH)) return false;
   if (!isValidLength(user.basicInfo.lastName, SIGNUP.MINIMUM_LAST_NAME_LENGTH)) return false;
-  if (!isValidAge(user.basicInfo.dateOfBirth, SIGNUP.MINIMUM_AGE, SIGNUP.MAXIMUM_AGE, true)) return false;
   if (!isValidEmail(user.basicInfo.email)) return false;
   if (!isValidProfileLink("linkedin", user.socialLinks.linkedin)) return false;
   if (!isValidProfileLink("twitter", user.socialLinks.twitter)) return false;
