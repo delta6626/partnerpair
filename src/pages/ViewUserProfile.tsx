0cdb81e24450eb004a1be641051b0c9aca1ee683
Delta6626 <hassan911ee@gmail.com>
2025-10-07 19:23:31 +0400

    capture url param and use theme

--- src/pages/ViewUserProfile.tsx
+++ src/pages/ViewUserProfile.tsx
@@ -1,3 +1,17 @@
+import { useParams } from "react-router-dom";
+import { MainNavbar } from "../components/navigation/MainNavbar";
+import { useTheme } from "../hooks/useTheme";
+
 export const ViewUserProfile = () => {
-  return <div className=""></div>;
+  useTheme();
+  const { id } = useParams();
+
+  return (
+    <div className="w-full h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+      <div className="py-4">
+        <MainNavbar />
+      </div>
+      <div className=""></div>
+    </div>
+  );
 };
