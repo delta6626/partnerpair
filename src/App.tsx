e5c4a3ea0f591432139bd64e983a58623b03982a
Delta6626 <hassan911ee@gmail.com>
2025-10-07 16:44:56 +0400

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { Browse } from "./pages/Browse";
 import { Messages } from "./pages/Messages";
 import { Forum } from "./pages/Forum";
 import { Settings } from "./pages/Settings";
+import { ViewUserProfile } from "./pages/ViewUserProfile";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/verify" element={<Verify />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/browse" element={<Browse />}></Route>
+        <Route path="/user" element={<ViewUserProfile />}></Route>
         <Route path="/messages" element={<Messages />}></Route>
         <Route path="/forum" element={<Forum />}></Route>
         <Route path="/blogs" element={<Blogs />}></Route>
