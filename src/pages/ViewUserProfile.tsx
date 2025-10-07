ee6833d4600aba4c68bd2153dac7ed8ea82ca246
Delta6626 <hassan911ee@gmail.com>
2025-10-07 19:24:59 +0400

    add loader

--- src/pages/ViewUserProfile.tsx
+++ src/pages/ViewUserProfile.tsx
@@ -1,17 +1,28 @@
 import { useParams } from "react-router-dom";
 import { MainNavbar } from "../components/navigation/MainNavbar";
 import { useTheme } from "../hooks/useTheme";
+import { useInitializeUser } from "../hooks/useInitializeUser";
+import Loader from "../components/Loader";
 
 export const ViewUserProfile = () => {
   useTheme();
+
   const { id } = useParams();
+  const { loading } = useInitializeUser();
 
   return (
-    <div className="w-full h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
-      <div className="py-4">
-        <MainNavbar />
-      </div>
-      <div className=""></div>
+    <div className="">
+      {loading ? (
+        <div className="w-full h-[100vh] bg-base-300 flex items-center justify-center">
+          <Loader />
+        </div>
+      ) : (
+        <div className="w-full h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+          <div className="py-4">
+            <MainNavbar />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
