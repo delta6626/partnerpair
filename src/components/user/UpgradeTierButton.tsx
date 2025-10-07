6f335b6b723b1afc27dbc46388c6bb81885e8af7
Delta6626 <hassan911ee@gmail.com>
2025-10-07 19:41:43 +0400

    add icon + text

--- src/components/user/UpgradeTierButton.tsx
+++ src/components/user/UpgradeTierButton.tsx
@@ -1,9 +1,10 @@
+import { CircleStar } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export const UpgradeTierButton = () => {
   return (
     <Link to={""} className="btn btn-primary">
-      Get Pro
+      <CircleStar /> Get Pro
     </Link>
   );
 };
