9b7ad9c676c3313330bffdc0b0303e0f4cb94037
Delta6626 <hassan911ee@gmail.com>
2025-10-12 21:22:50 +0400

    add component style

--- src/components/user/GenericChip.tsx
+++ src/components/user/GenericChip.tsx
@@ -1,1 +1,7 @@
-export const GenericChip = () => {};
+export const GenericChip = ({ chipText }: { chipText: string }) => {
+  return (
+    <div className="btn inline-flex items-center px-4 py-3 rounded-full border-1 border-accent select-none font-medium text-sm gap-2">
+      {chipText}
+    </div>
+  );
+};
