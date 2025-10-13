444348899ed1f6065da52cb894e4608f35f15683
Delta6626 <hassan911ee@gmail.com>
2025-10-13 23:28:43 +0400

    render list items

--- src/components/ProfileViewer/GenericChipCollection.tsx
+++ src/components/ProfileViewer/GenericChipCollection.tsx
@@ -1,3 +1,13 @@
+import { GenericChip } from "./GenericChip";
+
 export const GenericChipCollection = ({ listItems }: { listItems: string[] }) => {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      {listItems.length != 0
+        ? listItems.map((item) => {
+            return <GenericChip key={item} chipText={item} />;
+          })
+        : ""}
+    </div>
+  );
 };
