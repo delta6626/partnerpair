a745d50e746763a99386c8a848182a28723ece41
Delta6626 <hassan911ee@gmail.com>
2025-10-13 23:29:32 +0400

    render param

--- src/components/ProfileViewer/GenericChipCollection.tsx
+++ src/components/ProfileViewer/GenericChipCollection.tsx
@@ -3,11 +3,13 @@ import { GenericChip } from "./GenericChip";
 export const GenericChipCollection = ({ listItems, fallbackText }: { listItems: string[]; fallbackText: string }) => {
   return (
     <div className="">
-      {listItems.length != 0
-        ? listItems.map((item) => {
-            return <GenericChip key={item} chipText={item} />;
-          })
-        : ""}
+      {listItems.length != 0 ? (
+        listItems.map((item) => {
+          return <GenericChip key={item} chipText={item} />;
+        })
+      ) : (
+        <p className="w-full text-center text-accent">{fallbackText}</p>
+      )}
     </div>
   );
 };
