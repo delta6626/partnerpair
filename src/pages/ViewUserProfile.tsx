53d71ce25956c8f876053d888a60bd265e447cad
delta6626 <hassan911ee@gmail.com>
2025-10-16 10:56:06 +0400

    add min height

--- src/pages/ViewUserProfile.tsx
+++ src/pages/ViewUserProfile.tsx
@@ -22,7 +22,7 @@ export const ViewUserProfile = () => {
           <Loader />
         </div>
       ) : (
-        <div className="w-full h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+        <div className="w-full min-h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
           <div className="py-4">
             <MainNavbar />
           </div>
