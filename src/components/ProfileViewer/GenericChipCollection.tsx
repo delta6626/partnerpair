70efe64115c80746b57436df2a89408ca7852b95
delta6626 <hassan911ee@gmail.com>
2025-10-16 11:17:10 +0400

    edit generic chip collection

--- src/components/ProfileViewer/GenericChipCollection.tsx
+++ src/components/ProfileViewer/GenericChipCollection.tsx
@@ -1,11 +1,20 @@
+import type { MouseEventHandler } from "react";
 import { GenericChip } from "./GenericChip";
 
-export const GenericChipCollection = ({ listItems, fallbackText }: { listItems: string[]; fallbackText: string }) => {
+export const GenericChipCollection = ({
+  listItems,
+  fallbackText,
+  onClick,
+}: {
+  listItems: string[];
+  fallbackText: string;
+  onClick?: MouseEventHandler<HTMLDivElement>;
+}) => {
   return (
     <div className="w-full flex flex-wrap gap-2">
       {listItems.length != 0 ? (
         listItems.map((item) => {
-          return <GenericChip key={item} chipText={item} />;
+          return <GenericChip key={item} chipText={item} onClick={onClick} />;
         })
       ) : (
         <p className="w-full text-center text-accent">{fallbackText}</p>
