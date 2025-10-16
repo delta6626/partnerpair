c648e5ef50d9184f034ddff8a1665f06e0253923
delta6626 <hassan911ee@gmail.com>
2025-10-16 11:16:02 +0400

    edit generic chip

--- src/components/ProfileViewer/GenericChip.tsx
+++ src/components/ProfileViewer/GenericChip.tsx
@@ -1,4 +1,12 @@
-export const GenericChip = ({ chipText }: { chipText: string }) => {
+import type { MouseEventHandler } from "react";
+
+export const GenericChip = ({
+  chipText,
+  onClick,
+}: {
+  chipText: string;
+  onClick?: MouseEventHandler<HTMLDivElement>;
+}) => {
   return (
     <div className="btn inline-flex items-center px-4 py-3 rounded-full border-1 border-accent select-none font-medium text-sm gap-2">
       {chipText}
