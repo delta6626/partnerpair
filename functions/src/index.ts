19ef3dd9ba9242a66e26ed1557dad911dee36158
Delta6626 <hassan911ee@gmail.com>
2025-10-20 23:55:37 +0400

    edit function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,5 +2,5 @@ import { onCall } from "firebase-functions/v2/https";
 import { UserTier } from "../../shared/types/UserTier";
 
 export const getUserTier = onCall((request) => {
-  const userId: string = request.data.userId;
+  const userId: string | undefined = request.auth?.uid;
 });
