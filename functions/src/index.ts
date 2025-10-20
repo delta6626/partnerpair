7e1a521b7b392f0495e7337cf1748635803b5cc9
Delta6626 <hassan911ee@gmail.com>
2025-10-20 23:13:43 +0400

    edit functios

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,5 @@
 import { setGlobalOptions } from "firebase-functions";
+import { UserTier } from "../../shared/types/UserTier";
+import { onCall } from "firebase-functions/https";
 
 setGlobalOptions({ maxInstances: 10 });
