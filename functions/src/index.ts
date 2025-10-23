215cee16c8a0b5577c84e4f92e614d1d857a5dd1
delta6626 <hassan911ee@gmail.com>
2025-10-23 10:17:47 +0400

    modify getUserFunction

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -18,16 +18,12 @@ const fetchUserTier = async (userId: string): Promise<UserTier> => {
 export const getUserTier = onCall(async (request) => {
   const userId = request.auth?.uid;
 
-  if (!userId) {
-    throw new HttpsError("unauthenticated", "User must be logged in to access this function.");
-  }
+  if (!userId) throw new HttpsError("unauthenticated", "User must be logged in to access this function.");
 
   try {
-    const userDoc = await db.collection("users").doc(userId).get();
-    const userTier = userDoc.data()?.basicInfo.tier;
-    return userTier as UserTier;
+    const userTier = await fetchUserTier(userId);
+    return userTier;
   } catch (error: unknown) {
-    console.error("Error fetching user tier:", error);
     throw new HttpsError("internal", "Failed to fetch user tier.");
   }
 });
