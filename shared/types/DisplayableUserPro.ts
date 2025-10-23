2e2ad6a31b361f596d7be0c0975c80c039826844
delta6626 <hassan911ee@gmail.com>
2025-10-23 11:34:56 +0400

    create type

--- shared/types/DisplayableUserPro.ts
+++ shared/types/DisplayableUserPro.ts
@@ -1,1 +1,11 @@
-export interface DisplayableUserPro {}
+import type { User } from "./User";
+
+export interface DisplayableUserPro {
+  basicInfo: Omit<
+    User["basicInfo"],
+    "email" | "verified" | "authenticationMethod" | "profileCompleted" | "profileViews" | "contactList"
+  >;
+  professionalInfo: User["professionalInfo"];
+  matchingPreferences: User["matchingPreferences"];
+  socialLinks: User["socialLinks"];
+}
