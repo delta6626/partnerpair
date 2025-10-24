081f6130ff12eb93357a2846241d9dd25a6285c1
Delta6626 <hassan911ee@gmail.com>
2025-10-24 15:17:16 +0400

    refactor

--- src/components/ProfileViewer/RenderProData.tsx
+++ src/components/ProfileViewer/RenderProData.tsx
@@ -123,6 +123,20 @@ export const RenderProData = ({ visitedUserData }: { visitedUserData: Displayabl
           </div>
         )}
       </div>
+
+      {visitedUserData.professionalInfo.wantsToCofound ? (
+        <div className="mt-4">
+          <h1 className="text-lg font-medium">My Preferred Startup Stages</h1>
+          <div className="mt-2">
+            <GenericChipCollection
+              listItems={visitedUserData.matchingPreferences.preferredCompanyStage as string[]}
+              fallbackText={visitedUserData.basicInfo.firstName + " has not added any preferred stages yet."}
+            />
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
