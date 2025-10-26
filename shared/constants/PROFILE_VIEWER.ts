6c2b1109543187965085452240e6e6002812f132
Delta6626 <hassan911ee@gmail.com>
2025-10-26 17:47:38 +0400

    remove constants

--- shared/constants/PROFILE_VIEWER.ts
+++ shared/constants/PROFILE_VIEWER.ts
@@ -1,6 +1,4 @@
 export const PROFILE_VIEWER = {
   PRO_ONLY: "Pro Only",
   PRO_ONLY_DESCRIPTIVE: "Accessible to Pro Members Only",
-  ADD_CONTACT: "Add Contact",
-  REMOVE_CONTACT: "Remove Contact",
 };
