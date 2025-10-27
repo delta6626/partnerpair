fd941d887990b4146cf66465e1efe8a5308d0fc2
Delta6626 <hassan911ee@gmail.com>
2025-10-27 13:06:59 +0400

    ui

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -5,7 +5,7 @@ import { useTheme } from "../hooks/useTheme";
 
 export const Dashboard = () => {
   useTheme();
-  const { loading } = useInitializeUser();
+  const { user, loading } = useInitializeUser();
 
   return (
     <div className="">
@@ -17,6 +17,11 @@ export const Dashboard = () => {
         <div className="w-full h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
           <div className="py-4">
             <MainNavbar />
+          </div>
+
+          <div className="py-8">
+            <h1 className="text-3xl font-bold">Hello, {user?.basicInfo.firstName}</h1>
+            <h1 className="text-xl text-accent">Here's what's happening with your cofounder search.</h1>
           </div>
         </div>
       )}
