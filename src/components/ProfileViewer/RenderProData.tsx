7140cd3fcd731363df280a77778b1dfcc7e0e7ff
Delta6626 <hassan911ee@gmail.com>
2025-10-29 00:32:25 +0400

    refactor

--- src/components/ProfileViewer/RenderProData.tsx
+++ src/components/ProfileViewer/RenderProData.tsx
@@ -42,12 +42,14 @@ export const RenderProData = ({ visitedUserData }: { visitedUserData: Displayabl
                     </div>
                   )}
 
-                  <div className="flex items-center gap-2">
-                    <div className="tooltip tooltip-top" data-tip="Commitment Level">
-                      <Clock size={20} className="text-accent cursor-pointer" />
+                  {visitedUserData.professionalInfo.commitmentLevel && (
+                    <div className="flex items-center gap-2">
+                      <div className="tooltip tooltip-top" data-tip="Commitment Level">
+                        <Clock size={20} className="text-accent cursor-pointer" />
+                      </div>
+                      <span>{visitedUserData.professionalInfo.commitmentLevel}</span>
                     </div>
-                    <span>{visitedUserData.professionalInfo.commitmentLevel}</span>
-                  </div>
+                  )}
                 </div>
 
                 <div className="flex flex-col gap-2">
