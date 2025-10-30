3f7819471e0514ea771e0e650a03b99c788169bf
Delta6626 <hassan911ee@gmail.com>
2025-10-30 16:56:52 +0400

    add logic

--- shared/utils/getProfileViewCountWithinTimePeriod.ts
+++ shared/utils/getProfileViewCountWithinTimePeriod.ts
@@ -1,3 +1,19 @@
 import type { DocumentData } from "firebase/firestore";
+import type { ViewerMetaData } from "../types/ViewerMetaData";
 
-export const getProfileViewCountWithinTimePeriod = (documentArray: DocumentData[], timePeriod: number) => {};
+export const getProfileViewCountWithinTimePeriod = (documentArray: DocumentData[], timePeriod: number) => {
+  const currentDate = new Date();
+  let viewCount = 0;
+
+  documentArray.forEach((document) => {
+    const viewerData: ViewerMetaData = document.data() as ViewerMetaData;
+    const viewDate = viewerData.viewedAt.toDate();
+
+    const diffInMs = currentDate.getTime() - viewDate.getTime();
+    const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+
+    if (diffInDays <= timePeriod) viewCount++;
+  });
+
+  return viewCount;
+};
