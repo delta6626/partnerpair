a9c1d09fabff7992eca4ea52b60094169dbffabe
Delta6626 <hassan911ee@gmail.com>
2025-10-31 17:29:54 +0400

    integrate react query

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,29 @@ import { Messages } from "./pages/Messages";
 import { Forum } from "./pages/Forum";
 import { Settings } from "./pages/Settings";
 import { ViewUserProfile } from "./pages/ViewUserProfile";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/verify" element={<Verify />}></Route>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
-        <Route path="/browse" element={<Browse />}></Route>
-        <Route path="/user/:id" element={<ViewUserProfile />}></Route>
-        <Route path="/messages" element={<Messages />}></Route>
-        <Route path="/forum" element={<Forum />}></Route>
-        <Route path="/blogs" element={<Blogs />}></Route>
-        <Route path="/settings" element={<Settings />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/verify" element={<Verify />}></Route>
+          <Route path="/dashboard" element={<Dashboard />}></Route>
+          <Route path="/browse" element={<Browse />}></Route>
+          <Route path="/user/:id" element={<ViewUserProfile />}></Route>
+          <Route path="/messages" element={<Messages />}></Route>
+          <Route path="/forum" element={<Forum />}></Route>
+          <Route path="/blogs" element={<Blogs />}></Route>
+          <Route path="/settings" element={<Settings />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
