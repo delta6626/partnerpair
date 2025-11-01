9c88ec1712180499b6a7395658a0b4f614e35ee8
delta6626 <hassan911ee@gmail.com>
2025-11-01 10:30:35 +0400

    integrate component

--- src/components/DashboardHeader.tsx
+++ src/components/DashboardHeader.tsx
@@ -1,5 +1,6 @@
 import type { User } from "../../shared/types/User";
 import { ProfileStatusMessage } from "./user/ProfileStatusMessage";
+import { ProfileViewCount } from "./user/ProfileViewCount";
 
 export const DashboardHeader = ({ user }: { user: User | null }) => {
   return (
@@ -11,6 +12,7 @@ export const DashboardHeader = ({ user }: { user: User | null }) => {
         <h1 className="text-3xl font-bold">Hello, {user?.basicInfo.firstName}</h1>
         <h1 className="text-xl text-accent">Here's what's happening with your cofounder search.</h1>
       </div>
+      <ProfileViewCount />
     </div>
   );
 };
