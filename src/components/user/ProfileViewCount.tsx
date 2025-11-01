394a41581abe488115ee8b17ede89ad329a7162f
delta6626 <hassan911ee@gmail.com>
2025-11-01 10:33:24 +0400

    refactor

--- src/components/user/ProfileViewCount.tsx
+++ src/components/user/ProfileViewCount.tsx
@@ -1,3 +1,19 @@
+import { functions } from "../../services/firebaseConfig";
+import { httpsCallable } from "firebase/functions";
+import { useQuery } from "@tanstack/react-query";
+import { QUERY_KEYS } from "../../../shared/constants/QUERY_KEYS";
+import { STALE_TIME } from "../../../shared/constants/STALE_TIME";
+
 export const ProfileViewCount = () => {
+  const getProfileViewCount = httpsCallable(functions, "getProfileViewCount");
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: [QUERY_KEYS.PROFILE_VIEW_COUNT],
+    queryFn: async () => {
+      const response = await getProfileViewCount();
+      return response.data as number | number[];
+    },
+    staleTime: STALE_TIME.MINUTE_FIVE,
+  });
+
   return <div className=""></div>;
 };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,27 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
 import { DashboardHeader } from "../components/DashboardHeader";
 import { Loader } from "../components/Loader";
 import { MainNavbar } from "../components/navigation/MainNavbar";
 import { useInitializeUser } from "../hooks/useInitializeUser";
 import { useTheme } from "../hooks/useTheme";
-import { functions } from "../services/firebaseConfig";
-import { httpsCallable } from "firebase/functions";
-import { QUERY_KEYS } from "../../shared/constants/QUERY_KEYS";
-import { STALE_TIME } from "../../shared/constants/STALE_TIME";
 
 export const Dashboard = () => {
   useTheme();
   const { user, loading } = useInitializeUser();
-
-  const getProfileViewCount = httpsCallable(functions, "getProfileViewCount");
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: [QUERY_KEYS.PROFILE_VIEW_COUNT],
-    queryFn: async () => {
-      const response = await getProfileViewCount();
-      return response.data as number | number[];
-    },
-    staleTime: STALE_TIME.MINUTE_FIVE,
-  });
 
   return (
     <div className="">
@@ -34,7 +19,6 @@ export const Dashboard = () => {
           <div className="py-4">
             <MainNavbar />
           </div>
-
           <DashboardHeader user={user} />
         </div>
       )}
