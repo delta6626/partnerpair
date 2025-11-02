99f3ca449a7901bd1a597e58b3de4b5f8ba1c67e
Delta6626 <hassan911ee@gmail.com>
2025-11-02 21:31:26 +0400

    add constants

---
+++ shared/constants/DASHBOARD.ts
@@ -0,0 +1,5 @@
+export const DASHBOARD = {
+  HEADER_SUB_TEXT: "Here's what's happening with your co-founder search.",
+  PROFILE_COUNTER_USER_BASIC: "Upgrade to see more insights",
+  PROFILE_COUNTER_USER_PRO: "Click to see more",
+};
