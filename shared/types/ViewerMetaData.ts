3310d3ae5d32deba5de9b08e7cc1ef3d06f0c594
delta6626 <hassan911ee@gmail.com>
2025-11-03 10:28:02 +0400

    update schema

--- shared/types/ViewerMetaData.ts
+++ shared/types/ViewerMetaData.ts
@@ -3,4 +3,6 @@ import type { Timestamp } from "firebase-admin/firestore";
 export interface ViewerMetaData {
   viewerId: string;
   viewedAt: Timestamp;
+  viewerName: string;
+  viewerPhotoURL: string;
 }
