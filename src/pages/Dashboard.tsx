21a6d1adc54af83b59cebb8eabf187edd951412e
delta6626 <hassan911ee@gmail.com>
2025-11-04 10:42:52 +0400

    fix style

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -11,11 +11,11 @@ export const Dashboard = () => {
   return (
     <div className="">
       {loading ? (
-        <div className="w-full h-[100vh] bg-base-300 flex items-center justify-center">
+        <div className="w-full min-h-[100vh] bg-base-300 flex items-center justify-center">
           <Loader />
         </div>
       ) : (
-        <div className="w-full h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+        <div className="w-full min-h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
           <div className="py-4">
             <MainNavbar />
           </div>
