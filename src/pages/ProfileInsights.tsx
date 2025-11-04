784314a5808aed67fa54fa263cc17b014406a65a
delta6626 <hassan911ee@gmail.com>
2025-11-04 10:38:46 +0400

    boilerplate

--- src/pages/ProfileInsights.tsx
+++ src/pages/ProfileInsights.tsx
@@ -1,3 +1,25 @@
+import { Loader } from "../components/Loader";
+import { MainNavbar } from "../components/navigation/MainNavbar";
+import { useInitializeUser } from "../hooks/useInitializeUser";
+import { useTheme } from "../hooks/useTheme";
+
 export const ProfileInsights = () => {
-  return <div className=""></div>;
+  useTheme();
+  const { loading } = useInitializeUser();
+
+  return (
+    <div className="">
+      {loading ? (
+        <div className="w-full h-[100vh] bg-base-300 flex items-center justify-center">
+          <Loader />
+        </div>
+      ) : (
+        <div className="w-full h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+          <div className="py-4">
+            <MainNavbar />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
