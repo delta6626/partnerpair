604a895f5fe6ab89c573e792fa9e3a63b7848104
delta6626 <hassan911ee@gmail.com>
2025-11-06 12:21:06 +0400

    add logic

--- src/components/user/VisitedUserCollection.tsx
+++ src/components/user/VisitedUserCollection.tsx
@@ -8,5 +8,43 @@ export const VisitedUserCollection = ({
   visitedUsers: ViewerMetaData[];
   timePeriod: ProfileInsightsTimePeriod;
 }) => {
+  const now = Date.now();
+  const day = 24 * 60 * 60 * 1000;
+
+  let filteredVisitedUsers: ViewerMetaData[] = [];
+
+  switch (timePeriod) {
+    case "last24Hours": {
+      const startTime = now - 24 * 60 * 60 * 1000;
+      filteredVisitedUsers = visitedUsers.filter(
+        (v) => v.viewedAt.toMillis() >= startTime && v.viewedAt.toMillis() <= now
+      );
+      break;
+    }
+    case "last7Days": {
+      const startTime = now - 7 * day;
+      filteredVisitedUsers = visitedUsers.filter(
+        (v) => v.viewedAt.toMillis() >= startTime && v.viewedAt.toMillis() <= now
+      );
+      break;
+    }
+    case "last30Days": {
+      const startTime = now - 30 * day;
+      filteredVisitedUsers = visitedUsers.filter(
+        (v) => v.viewedAt.toMillis() >= startTime && v.viewedAt.toMillis() <= now
+      );
+      break;
+    }
+    case "last90Days": {
+      const startTime = now - 90 * day;
+      filteredVisitedUsers = visitedUsers.filter(
+        (v) => v.viewedAt.toMillis() >= startTime && v.viewedAt.toMillis() <= now
+      );
+      break;
+    }
+    default:
+      filteredVisitedUsers = visitedUsers;
+  }
+
   return <div className=""></div>;
 };
