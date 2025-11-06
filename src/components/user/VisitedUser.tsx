4c288b74063e7776da05fecd6b436cc0d40060e6
delta6626 <hassan911ee@gmail.com>
2025-11-06 17:11:23 +0400

    add style

--- src/components/user/VisitedUser.tsx
+++ src/components/user/VisitedUser.tsx
@@ -2,7 +2,7 @@ import type { ViewerMetaData } from "../../../shared/types/ViewerMetaData";
 
 export const VisitedUser = ({ viewerData }: { viewerData: ViewerMetaData }) => {
   return (
-    <div className="justify-between">
+    <div className="p-4 border border-accent rounded-2xl">
       <div className="flex items-center gap-4">
         <img className="w-15 h-15 rounded-full" src={viewerData.viewerProfileImageURL}></img>
         <h1 className="">{viewerData.viewerFirstName + " " + viewerData.viewerLastName}</h1>
