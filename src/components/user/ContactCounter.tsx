545c4357c6a73d4561bc30888aef207b7ecf8c1e
delta6626 <hassan911ee@gmail.com>
2025-11-09 11:06:11 +0400

    add loader

--- src/components/user/ContactCounter.tsx
+++ src/components/user/ContactCounter.tsx
@@ -1,3 +1,18 @@
+import { useInitializeUser } from "../../hooks/useInitializeUser";
+import { Loader } from "../Loader";
+
 export const ContactCounter = () => {
-  return <div className="p-4 rounded-3xl bg-base-200 min-w-75 w-fit flex flex-col gap-2"></div>;
+  const { user, loading } = useInitializeUser();
+
+  return (
+    <div className="p-4 rounded-3xl bg-base-200 min-w-75 w-fit flex flex-col gap-2">
+      {loading ? (
+        <div>
+          <Loader />
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
 };
