7085fc6dbd2c7b1745066d0db20264dce4c96d1c
delta6626 <hassan911ee@gmail.com>
2025-11-10 11:28:02 +0400

    add logic

--- src/components/user/AddContact.tsx
+++ src/components/user/AddContact.tsx
@@ -1,10 +1,28 @@
+import type { User } from "../../../shared/types/User";
 import { useInitializeUser } from "../../hooks/useInitializeUser";
+import { updateUserProfile } from "../../services/userProfile/userProfileServices";
+import { useUserStore } from "../../store/useUserStore";
 
 export const AddContact = ({ contactId }: { contactId: string }) => {
   const { user } = useInitializeUser();
+  const { setUser } = useUserStore();
   const userIsAContact = user?.basicInfo.contactList.includes(contactId);
 
-  const addContact = () => {};
+  const addContact = async () => {
+    const updatedUser: User = {
+      basicInfo: { ...user!.basicInfo, contactList: [...user!.basicInfo.contactList, contactId] },
+      professionalInfo: { ...user!.professionalInfo },
+      matchingPreferences: { ...user!.matchingPreferences },
+      socialLinks: { ...user!.socialLinks },
+    };
+
+    const userUpdated = await updateUserProfile(updatedUser);
+    if (typeof userUpdated === "string") {
+      return; // TO DO: handle error case;
+    }
+
+    setUser(user!);
+  };
 
   const removeContact = () => {};
 
