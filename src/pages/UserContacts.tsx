7e2c084c3a9a71672fae6c40444105a6857f7587
delta6626 <hassan911ee@gmail.com>
2025-11-12 11:13:54 +0400

    simplify logic

--- src/pages/UserContacts.tsx
+++ src/pages/UserContacts.tsx
@@ -5,19 +5,19 @@ import { useInitializeUser } from "../hooks/useInitializeUser";
 export const UserContacts = () => {
   const { user, loading } = useInitializeUser();
 
+  if (loading) {
+    return (
+      <div className="w-full min-h-[100vh] bg-base-300 flex items-center justify-center">
+        <Loader />
+      </div>
+    );
+  }
+
   return (
-    <div className="">
-      {loading ? (
-        <div className="w-full min-h-[100vh] bg-base-300 flex items-center justify-center">
-          <Loader />
-        </div>
-      ) : (
-        <div className="w-full flex flex-col min-h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
-          <div className="py-4">
-            <MainNavbar />
-          </div>
-        </div>
-      )}
+    <div className="w-full flex flex-col min-h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+      <div className="py-4">
+        <MainNavbar />
+      </div>
     </div>
   );
 };
