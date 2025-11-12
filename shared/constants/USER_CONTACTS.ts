c99c1c77b287f4d6e3cfc8ea91952a4cb5a99c35
delta6626 <hassan911ee@gmail.com>
2025-11-12 11:24:56 +0400

    add title and subtitle

---
+++ shared/constants/USER_CONTACTS.ts
@@ -0,0 +1,3 @@
+export const USER_CONTACTS = {
+  SUBTITLE_TEXT: "View and manage your contacts",
+};
--- src/pages/UserContacts.tsx
+++ src/pages/UserContacts.tsx
@@ -1,3 +1,4 @@
+import { USER_CONTACTS } from "../../shared/constants/USER_CONTACTS";
 import { Loader } from "../components/Loader";
 import { MainNavbar } from "../components/navigation/MainNavbar";
 import { useInitializeUser } from "../hooks/useInitializeUser";
@@ -17,6 +18,13 @@ export const UserContacts = () => {
     <div className="w-full flex flex-col min-h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
       <div className="py-4">
         <MainNavbar />
+      </div>
+
+      <div className="py-10 flex items-center justify-center">
+        <div className="w-full max-w-200">
+          <h1 className="font-bold text-3xl">Contacts</h1>
+          <p className="text-accent">{USER_CONTACTS.SUBTITLE_TEXT}</p>
+        </div>
       </div>
     </div>
   );
