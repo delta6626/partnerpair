76a92b3a53a10a3c06b562978353053156e02456
Delta6626 <hassan911ee@gmail.com>
2025-11-14 18:57:22 +0400

    create component ui

--- src/components/user/ContactHolder.tsx
+++ src/components/user/ContactHolder.tsx
@@ -1,5 +1,22 @@
 import type { Contact } from "../../../shared/types/Contact";
+import { AddContact } from "./AddContact";
 
 export const ContactHolder = ({ contactDetails }: { contactDetails: Contact }) => {
-  return <div className=""></div>;
+  return (
+    <div className="flex items-center justify-between p-4 border border-accent rounded-3xl">
+      <div className="flex items-center gap-4">
+        <img
+          className="w-15 h-15 rounded-full"
+          src={contactDetails.contactProfileImageURL}
+          alt={`${contactDetails.contactFirstName} ${contactDetails.contactLastName}`}
+        />
+        <h1 className="flex gap-2">{contactDetails.contactFirstName + " " + contactDetails.contactLastName}</h1>
+      </div>
+
+      <div className="flex gap-2">
+        <AddContact contactId={contactDetails.contactId} />
+        <button className="btn btn-primary">Message</button>
+      </div>
+    </div>
+  );
 };
