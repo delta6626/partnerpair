af905f2085035a11000012e11298427933a87326
delta6626 <hassan911ee@gmail.com>
2025-11-15 12:32:10 +0400

    add ui

--- src/components/ProfileViewer/SuggestedProfiles.tsx
+++ src/components/ProfileViewer/SuggestedProfiles.tsx
@@ -1,3 +1,14 @@
+import { Link } from "react-router-dom";
+
 export const SuggestedProfiles = () => {
-  return <div className=""></div>;
+  return (
+    <div className="w-full py-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Suggested Profiles</h1>
+        <Link to="/browse" className="btn btn-primary">
+          Browse all profiles
+        </Link>
+      </div>
+    </div>
+  );
 };
