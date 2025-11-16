9b33e462038b06b8a74047dc7896d6d9cece6ebf
delta6626 <hassan911ee@gmail.com>
2025-11-16 10:52:14 +0400

    add logic

--- shared/utils/compatibilityScore.ts
+++ shared/utils/compatibilityScore.ts
@@ -1,3 +1,9 @@
 import type { User } from "../types/User";
+import { jaccardIndex } from "./jaccardIndex";
 
-export const compatibilityScore = (a: User, b: User) => {};
+export const compatibilityScore = (a: User, b: User) => {
+  let score = 0;
+
+  const skillSimilarity = jaccardIndex(a.matchingPreferences.lookingForSkills, b.professionalInfo.skills);
+  const roleSimilarity = jaccardIndex(a.matchingPreferences.lookingForRoles, b.professionalInfo.roles);
+};
