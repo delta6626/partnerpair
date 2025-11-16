fef1beee8b06ae97e81172b524e2c2c23a3a5191
delta6626 <hassan911ee@gmail.com>
2025-11-16 10:34:16 +0400

    create jaccard index utility function

--- shared/utils/jaccardIndex.ts
+++ shared/utils/jaccardIndex.ts
@@ -1,1 +1,10 @@
-export const jaccardIndex = (a: string[], b: string[]) => {};
+export const jaccardIndex = (a: string[], b: string[]) => {
+  if (!a || !b) return 0;
+  const setA = new Set(a);
+  const setB = new Set(b);
+  const intersectionLength = [...setA].filter((element) => [...setB].includes(element)).length;
+  const unionLength = new Set([...setA, ...setB]).size;
+
+  if (unionLength === 0) return 0;
+  return intersectionLength / unionLength;
+};
