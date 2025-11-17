8265197772e0e8f0a42aed4d8f77d3737fd5ee55
delta6626 <hassan911ee@gmail.com>
2025-11-17 11:22:24 +0400

    add logic

--- shared/utils/compatibilityScore.ts
+++ shared/utils/compatibilityScore.ts
@@ -5,21 +5,32 @@ import { jaccardIndex } from "./jaccardIndex";
 export const compatibilityScore = (a: User, b: User) => {
   let score = 0;
 
+  // Individual role and skill based matching
+
   const skillMatchForUserA = jaccardIndex(a.matchingPreferences.lookingForSkills, b.professionalInfo.skills);
   const roleMatchForUserA = jaccardIndex(a.matchingPreferences.lookingForRoles, b.professionalInfo.roles);
+  const skillMatchForUserB = jaccardIndex(b.matchingPreferences.lookingForSkills, a.professionalInfo.skills);
+  const roleMatchForUserB = jaccardIndex(b.matchingPreferences.lookingForRoles, a.professionalInfo.roles);
+
+  // Individual commitment and availability based matching
+
   const commitmentLevelMatchForUserA = enumEqual(
     a.matchingPreferences.commitmentLevel,
     b.professionalInfo.commitmentLevel
   );
-  const availabilityMatchForUserA = enumEqual(a.matchingPreferences.availability, b.professionalInfo.availability);
-
-  const skillMatchForUserB = jaccardIndex(b.matchingPreferences.lookingForSkills, a.professionalInfo.skills);
-  const roleMatchForUserB = jaccardIndex(b.matchingPreferences.lookingForRoles, a.professionalInfo.roles);
   const commitmentLevelMatchForUserB = enumEqual(
-    a.professionalInfo.commitmentLevel,
-    b.matchingPreferences.commitmentLevel
+    b.matchingPreferences.commitmentLevel,
+    a.professionalInfo.commitmentLevel
   );
-  const availabilityMatchForUserB = enumEqual(a.professionalInfo.availability, b.matchingPreferences.commitmentLevel);
+  const availabilityMatchForUserA = enumEqual(a.matchingPreferences.availability, b.professionalInfo.availability);
+  const availabilityMatchForUserB = enumEqual(b.matchingPreferences.availability, a.professionalInfo.availability);
+
+  // Mutual commitment and availability based matching
+
+  const commitmentLevelMutualMatch = enumEqual(a.professionalInfo.commitmentLevel, b.professionalInfo.commitmentLevel);
+  const AvailabilityMutualMatch = enumEqual(a.professionalInfo.availability, b.professionalInfo.availability);
+
+  // Check if a user owns a startup and the other user wants to join the startup
 
   const canBePartners =
     (a.professionalInfo.hasStartup && b.professionalInfo.wantsToCofound) ||
