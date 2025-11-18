117d3ae0a8ca06b00ca380c5f70161e2037e05d5
Delta6626 <hassan911ee@gmail.com>
2025-11-18 23:21:45 +0400

    add query

--- src/components/ProfileViewer/SuggestedProfiles.tsx
+++ src/components/ProfileViewer/SuggestedProfiles.tsx
@@ -1,9 +1,18 @@
 import { httpsCallable } from "firebase/functions";
 import { Link } from "react-router-dom";
 import { functions } from "../../services/firebaseConfig";
+import { useQuery } from "@tanstack/react-query";
+import { QUERY_KEYS } from "../../../shared/constants/QUERY_KEYS";
 
 export const SuggestedProfiles = () => {
   const getSuggestedProfiles = httpsCallable(functions, "getSuggestedProfiles");
+  const { data, isLoading, isError } = useQuery({
+    queryKey: [QUERY_KEYS.SUGGESTED_PROFILES],
+    queryFn: async () => {
+      const response = await getSuggestedProfiles();
+      return response.data;
+    },
+  });
 
   return (
     <div className="w-full py-4">
