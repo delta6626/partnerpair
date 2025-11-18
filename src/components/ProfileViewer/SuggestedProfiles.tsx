dc00634dbe07c4ee805d7d2233a71ce30969077f
Delta6626 <hassan911ee@gmail.com>
2025-11-18 23:27:18 +0400

    add stale time

--- src/components/ProfileViewer/SuggestedProfiles.tsx
+++ src/components/ProfileViewer/SuggestedProfiles.tsx
@@ -4,6 +4,7 @@ import { functions } from "../../services/firebaseConfig";
 import { useQuery } from "@tanstack/react-query";
 import { QUERY_KEYS } from "../../../shared/constants/QUERY_KEYS";
 import type { SuggestedProfile } from "../../../shared/types/SuggestedProfile";
+import { STALE_TIME } from "../../../shared/constants/STALE_TIME";
 
 export const SuggestedProfiles = () => {
   const getSuggestedProfiles = httpsCallable(functions, "getSuggestedProfiles");
@@ -13,6 +14,7 @@ export const SuggestedProfiles = () => {
       const response = await getSuggestedProfiles();
       return response.data as SuggestedProfile[];
     },
+    staleTime: STALE_TIME.MINUTE_FIVE,
   });
 
   return (
