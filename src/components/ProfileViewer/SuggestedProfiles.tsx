1bf192c5c3adb40b91c912ba89b251bb6915bd3f
Delta6626 <hassan911ee@gmail.com>
2025-11-18 23:35:23 +0400

    edit style

--- src/components/ProfileViewer/SuggestedProfiles.tsx
+++ src/components/ProfileViewer/SuggestedProfiles.tsx
@@ -18,13 +18,15 @@ export const SuggestedProfiles = () => {
   });
 
   return (
-    <div className="w-full py-4">
+    <div className="w-full flex flex-col grow">
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold">Suggested Profiles</h1>
         <Link to="/browse" className="btn btn-primary">
           Browse all profiles
         </Link>
       </div>
+
+      <div className="w-full grow py-8"></div>
     </div>
   );
 };
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -16,7 +16,7 @@ export const Dashboard = () => {
           <Loader />
         </div>
       ) : (
-        <div className="w-full min-h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+        <div className="w-full flex flex-col grow min-h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
           <div className="py-4">
             <MainNavbar />
           </div>
