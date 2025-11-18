a1cbb369f0c3cf924f71436b6c378c92643c9c5f
Delta6626 <hassan911ee@gmail.com>
2025-11-18 23:26:31 +0400

    add type safety

--- src/components/ProfileViewer/SuggestedProfiles.tsx
+++ src/components/ProfileViewer/SuggestedProfiles.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { functions } from "../../services/firebaseConfig";
 import { useQuery } from "@tanstack/react-query";
 import { QUERY_KEYS } from "../../../shared/constants/QUERY_KEYS";
+import type { SuggestedProfile } from "../../../shared/types/SuggestedProfile";
 
 export const SuggestedProfiles = () => {
   const getSuggestedProfiles = httpsCallable(functions, "getSuggestedProfiles");
@@ -10,7 +11,7 @@ export const SuggestedProfiles = () => {
     queryKey: [QUERY_KEYS.SUGGESTED_PROFILES],
     queryFn: async () => {
       const response = await getSuggestedProfiles();
-      return response.data;
+      return response.data as SuggestedProfile[];
     },
   });
 
