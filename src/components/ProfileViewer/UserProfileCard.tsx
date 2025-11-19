3a705cd045582a75cf09d4dd3d3a73b77c048bc3
delta6626 <hassan911ee@gmail.com>
2025-11-19 10:28:15 +0400

    render profile image

--- src/components/ProfileViewer/UserProfileCard.tsx
+++ src/components/ProfileViewer/UserProfileCard.tsx
@@ -1,5 +1,11 @@
 import type { SuggestedProfile } from "../../../shared/types/SuggestedProfile";
 
 export const UserProfileCard = ({ userData }: { userData: SuggestedProfile }) => {
-  return <div className=""></div>;
+  return (
+    <div className="p-4">
+      <div className="">
+        <img src={userData.user.basicInfo.profileImageUrl} className="w-15 h-15 rounded-full" />
+      </div>
+    </div>
+  );
 };
