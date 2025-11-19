5d83029194a444107184778ffd63626e82d2e8a4
delta6626 <hassan911ee@gmail.com>
2025-11-19 10:51:31 +0400

    fix layout with grid

--- src/components/ProfileViewer/UserProfileCard.tsx
+++ src/components/ProfileViewer/UserProfileCard.tsx
@@ -2,7 +2,7 @@ import type { SuggestedProfile } from "../../../shared/types/SuggestedProfile";
 
 export const UserProfileCard = ({ userData }: { userData: SuggestedProfile }) => {
   return (
-    <div className="p-4 bg-base-200 min-w-100 rounded-3xl">
+    <div className="p-4 bg-base-200 min-w-105 rounded-3xl">
       <div className="flex items-center gap-4">
         <img src={userData.user.basicInfo.profileImageUrl} className="w-15 h-15 rounded-full" />
         <div className="">
