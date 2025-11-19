492d348741f43438f055300e77a44184d4e33e5b
delta6626 <hassan911ee@gmail.com>
2025-11-19 10:29:54 +0400

    render name

--- src/components/ProfileViewer/UserProfileCard.tsx
+++ src/components/ProfileViewer/UserProfileCard.tsx
@@ -3,8 +3,9 @@ import type { SuggestedProfile } from "../../../shared/types/SuggestedProfile";
 export const UserProfileCard = ({ userData }: { userData: SuggestedProfile }) => {
   return (
     <div className="p-4">
-      <div className="">
+      <div className="flex items-center gap-4">
         <img src={userData.user.basicInfo.profileImageUrl} className="w-15 h-15 rounded-full" />
+        <h1>{userData.user.basicInfo.firstName + " " + userData.user.basicInfo.lastName}</h1>
       </div>
     </div>
   );
