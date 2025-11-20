1b8701799086b30c2767cb7dca82f23482158959
Delta6626 <hassan911ee@gmail.com>
2025-11-20 21:02:17 +0400

    edit layout

--- src/components/ProfileViewer/UserProfileCard.tsx
+++ src/components/ProfileViewer/UserProfileCard.tsx
@@ -5,9 +5,12 @@ export const UserProfileCard = ({ userData }: { userData: SuggestedProfile }) =>
     <div className="p-4 bg-base-200 min-w-105 rounded-3xl">
       <div className="flex items-center gap-4">
         <img src={userData.profileImageURL} className="w-15 h-15 rounded-full" />
-        <div className="">
-          <h1>{userData.firstName + " " + userData.lastName}</h1>
-          <p className="text-accent w-full max-w-70 truncate">{userData.headline}</p>
+        <div className="w-full flex items-center justify-between">
+          <div className="">
+            <h1>{userData.firstName + " " + userData.lastName}</h1>
+            <p className="text-accent w-full max-w-70 truncate">{userData.headline}</p>
+          </div>
+          <p>Score label</p>
         </div>
       </div>
     </div>
