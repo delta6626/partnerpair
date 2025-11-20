aa49734ef1222e54547ad3d46fc0612f597c2b83
Delta6626 <hassan911ee@gmail.com>
2025-11-20 21:16:08 +0400

    add pro badge

--- src/components/ProfileViewer/UserProfileCard.tsx
+++ src/components/ProfileViewer/UserProfileCard.tsx
@@ -1,3 +1,4 @@
+import { CircleStar } from "lucide-react";
 import type { SuggestedProfile } from "../../../shared/types/SuggestedProfile";
 
 export const UserProfileCard = ({ userData }: { userData: SuggestedProfile }) => {
@@ -5,12 +6,16 @@ export const UserProfileCard = ({ userData }: { userData: SuggestedProfile }) =>
     <div className="p-4 bg-base-200 min-w-105 rounded-3xl">
       <div className="flex items-center gap-4">
         <img src={userData.profileImageURL} className="w-15 h-15 rounded-full" />
-        <div className="w-full flex items-center justify-between">
-          <div className="">
+        <div className="">
+          <div className="flex items-center gap-2">
             <h1>{userData.firstName + " " + userData.lastName}</h1>
-            <p className="text-accent w-full max-w-70 truncate">{userData.headline}</p>
+            {userData.tier === "Pro" && (
+              <div className="tooltip tooltip-top tooltip-primary" data-tip={"Pro user"}>
+                <CircleStar className="text-primary" size={20} />
+              </div>
+            )}
           </div>
-          <p>Score label</p>
+          <p className="text-accent w-full max-w-70 truncate">{userData.headline}</p>
         </div>
       </div>
     </div>
