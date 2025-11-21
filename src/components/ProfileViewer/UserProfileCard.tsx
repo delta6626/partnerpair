e4de3e6f0309f07459678d31d43631ba2201f831
Delta6626 <hassan911ee@gmail.com>
2025-11-21 22:40:00 +0400

    edit layout

--- src/components/ProfileViewer/UserProfileCard.tsx
+++ src/components/ProfileViewer/UserProfileCard.tsx
@@ -1,6 +1,5 @@
 import { CircleStar } from "lucide-react";
 import type { SuggestedProfile } from "../../../shared/types/SuggestedProfile";
-import { GenericChipCollection } from "./GenericChipCollection";
 import { AddContact } from "../user/AddContact";
 
 export const UserProfileCard = ({ userData }: { userData: SuggestedProfile }) => {
@@ -8,16 +7,22 @@ export const UserProfileCard = ({ userData }: { userData: SuggestedProfile }) =>
     <div className="p-4 border border-accent min-w-105 rounded-3xl flex flex-col justify-between">
       <div className="flex items-center gap-4">
         <img src={userData.profileImageURL} className="w-15 h-15 rounded-full" />
-        <div className="">
-          <div className="flex items-center gap-2">
-            <h1>{userData.firstName + " " + userData.lastName}</h1>
-            {userData.tier === "Pro" && (
-              <div className="tooltip tooltip-top tooltip-primary" data-tip={"Pro user"}>
-                <CircleStar className="text-primary" size={20} />
-              </div>
-            )}
+        <div className="w-full flex items-center justify-between">
+          <div className="">
+            <div className="flex items-center gap-2">
+              <h1>{userData.firstName + " " + userData.lastName}</h1>
+              {userData.tier === "Pro" && (
+                <div className="tooltip tooltip-top tooltip-primary" data-tip={"Pro user"}>
+                  <CircleStar className="text-primary" size={20} />
+                </div>
+              )}
+            </div>
+            <p className="text-accent w-full max-w-70 truncate">{userData.headline}</p>
           </div>
-          <p className="text-accent w-full max-w-70 truncate">{userData.headline}</p>
+
+          <div className="">
+            <AddContact contactId={userData.id} />
+          </div>
         </div>
       </div>
     </div>
