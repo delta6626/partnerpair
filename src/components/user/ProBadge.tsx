5590551bff6a9d6350ab8e8e468f27a4d0aa36ac
delta6626 <hassan911ee@gmail.com>
2025-11-24 11:37:34 +0400

    change cursor style

--- src/components/user/ProBadge.tsx
+++ src/components/user/ProBadge.tsx
@@ -2,7 +2,7 @@ import { CircleStar } from "lucide-react";
 
 export const ProBadge = () => {
   return (
-    <div className="px-2 flex items-center gap-1 rounded-lg bg-primary/20">
+    <div className="px-2 flex items-center gap-1 rounded-lg bg-primary/20 cursor-default">
       <CircleStar className="text-primary" size={15} />
       <p className="text-primary">Pro</p>
     </div>
