e12b635692e8ca3fe4f4726b40458748f30fdf97
delta6626 <hassan911ee@gmail.com>
2025-11-25 11:01:38 +0400

    add icon

--- src/components/BrowseHeader.tsx
+++ src/components/BrowseHeader.tsx
@@ -1,3 +1,4 @@
+import { Search } from "lucide-react";
 import { BROWSE } from "../../shared/constants/BROWSE";
 import { ProfileStatusMessage } from "./user/ProfileStatusMessage";
 
@@ -14,8 +15,9 @@ export const BrowseHeader = () => {
           <p className="text-accent">{BROWSE.HEADER_SUB_TEXT}</p>
         </div>
 
-        <div>
-          <input className="input min-w-120" placeholder={BROWSE.SEARCH_PLACEHOLDER}></input>
+        <div className="relative">
+          <Search size={20} className="text-accent absolute left-4 top-1/2 -translate-y-1/2 z-10" />
+          <input className="input pl-12 min-w-120" placeholder={BROWSE.SEARCH_PLACEHOLDER}></input>
         </div>
 
         <div className="">Filters</div>
