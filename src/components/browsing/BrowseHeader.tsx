3b36e76e872dc3c64df12eda409edb91d6ec16ef
delta6626 <hassan911ee@gmail.com>
2025-11-27 10:17:53 +0400

    refactor

--- src/components/browsing/BrowseHeader.tsx
+++ src/components/browsing/BrowseHeader.tsx
@@ -1,6 +1,6 @@
-import { Search } from "lucide-react";
 import { BROWSE } from "../../../shared/constants/BROWSE";
 import { ProfileStatusMessage } from "../user/ProfileStatusMessage";
+import { SearchUser } from "./SearchUser";
 
 export const BrowseHeader = () => {
   return (
@@ -15,12 +15,7 @@ export const BrowseHeader = () => {
           <p className="text-accent">{BROWSE.HEADER_SUB_TEXT}</p>
         </div>
 
-        <div className="relative">
-          <Search size={20} className="text-accent absolute left-4 top-1/2 -translate-y-1/2 z-10" />
-          <input className="input pl-12 min-w-120" placeholder={BROWSE.SEARCH_PLACEHOLDER}></input>
-        </div>
-
-        <div className="">Filters</div>
+        <SearchUser />
       </div>
     </div>
   );
