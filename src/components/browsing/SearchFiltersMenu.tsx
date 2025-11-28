76f7c068a174543001f3d1d4559b5b70b569aefc
Delta6626 <hassan911ee@gmail.com>
2025-11-28 14:24:51 +0400

    docs

--- src/components/browsing/SearchFiltersMenu.tsx
+++ src/components/browsing/SearchFiltersMenu.tsx
@@ -1,4 +1,25 @@
 export const SearchFiltersMenu = () => {
+  /*
+  Relevant properties to be included for filtering.
+
+  basicInfo.location
+  basicInfo.profileCompleted
+  
+  professionalInfo.skills
+  professonalInfo.roles
+  professionalInfo.commitmentLevel
+  professionalInfo.availability
+  professionalInfo.hasStartup
+  professionalInfo.startupStage
+  professonalInfo.wantsToCofound
+
+  matchingPreferences.lookingForSkills
+  matchingPreferences.lookingForRoles
+  matchingPreferences.commitmentLevel
+  matchingPreferences.availability
+  matchingPreferences.preferredCompanyStage
+  */
+
   return (
     <div className="min-w-full bg-base-200 rounded-3xl p-4">
       <div className="">
