2ba5d48f790b32536a0591560258be7e12706a31
Delta6626 <hassan911ee@gmail.com>
2025-11-30 02:12:43 +0400

    add search input

--- src/components/browsing/SkillsFilterDropdown.tsx
+++ src/components/browsing/SkillsFilterDropdown.tsx
@@ -8,7 +8,12 @@ export const SkillsFilterDropdown = () => {
         <ChevronDown size={20} />
       </button>
 
-      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2"></ul>
+      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2 p-4">
+        <form className="flex gap-2">
+          <input className="input w-70" type="text" placeholder={"Search skills"} />
+          <button className="btn btn-primary">Add</button>
+        </form>
+      </ul>
     </div>
   );
 };
