81302baac393a5a2f61c9d143ac7d7a949ebb85f
delta6626 <hassan911ee@gmail.com>
2025-11-29 10:34:31 +0400

    create dropdown

--- src/components/browsing/SearchFiltersMenu.tsx
+++ src/components/browsing/SearchFiltersMenu.tsx
@@ -21,9 +21,13 @@ export const SearchFiltersMenu = () => {
   */
 
   return (
-    <div className="min-w-full bg-base-200 rounded-3xl p-4">
-      <div className="">
-        <h1 className="font-bold">Filters</h1>
+    <div className="min-w-full bg-blue-50">
+      <div className="dropdown dropdown-bottom">
+        <button tabIndex={0} role="button" className="btn">
+          Skills
+        </button>
+
+        <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2"></ul>
       </div>
     </div>
   );
