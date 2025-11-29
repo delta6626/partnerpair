40d01000ed19fad9009bcc54549dd07692c9bcc3
Delta6626 <hassan911ee@gmail.com>
2025-11-30 02:16:15 +0400

    add boilerplate

--- src/components/browsing/SkillsFilterDropdown.tsx
+++ src/components/browsing/SkillsFilterDropdown.tsx
@@ -1,7 +1,12 @@
 import { ChevronDown } from "lucide-react";
 import { BROWSE } from "../../../shared/constants/BROWSE";
+import type { FormEvent } from "react";
 
 export const SkillsFilterDropdown = () => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="dropdown dropdown-bottom">
       <button tabIndex={0} role="button" className="btn">
@@ -10,9 +15,11 @@ export const SkillsFilterDropdown = () => {
       </button>
 
       <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2 p-4">
-        <form className="flex gap-2">
+        <form className="flex gap-2" onSubmit={handleSubmit}>
           <input className="input w-70" type="text" placeholder={BROWSE.SKILL_SEARCH_PLACEHOLDER} />
-          <button className="btn btn-primary">Add</button>
+          <button type="submit" className="btn btn-primary">
+            Add
+          </button>
         </form>
       </ul>
     </div>
