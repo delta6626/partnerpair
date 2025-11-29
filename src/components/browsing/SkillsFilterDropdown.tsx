41b76eddea8ac5674652abe216b8b3ecaf637ebb
Delta6626 <hassan911ee@gmail.com>
2025-11-30 02:28:10 +0400

    render skills

--- src/components/browsing/SkillsFilterDropdown.tsx
+++ src/components/browsing/SkillsFilterDropdown.tsx
@@ -1,8 +1,14 @@
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, XIcon } from "lucide-react";
 import { BROWSE } from "../../../shared/constants/BROWSE";
 import type { FormEvent } from "react";
+import { useSearchParams } from "react-router-dom";
+import { GenericChip } from "../ProfileViewer/GenericChip";
 
 export const SkillsFilterDropdown = () => {
+  const [searchParams, getSearchParams] = useSearchParams();
+
+  const skills = searchParams.get("skills")?.split(",") ?? [];
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   };
@@ -15,6 +21,18 @@ export const SkillsFilterDropdown = () => {
       </button>
 
       <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2 p-4">
+        <div className="">
+          {skills.length > 0
+            ? skills.map((skill) => {
+                return (
+                  <GenericChip chipText={skill.toLocaleUpperCase()} fallbackText="">
+                    <XIcon size={20} />
+                  </GenericChip>
+                );
+              })
+            : ""}
+        </div>
+
         <form className="flex gap-2" onSubmit={handleSubmit}>
           <input className="input w-70" type="text" placeholder={BROWSE.SKILL_SEARCH_PLACEHOLDER} />
           <button type="submit" className="btn btn-primary">
