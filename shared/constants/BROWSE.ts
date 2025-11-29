628e65a369dc8c2cbed531c3d99f38d9489de7d1
Delta6626 <hassan911ee@gmail.com>
2025-11-30 02:14:26 +0400

    refactor

--- shared/constants/BROWSE.ts
+++ shared/constants/BROWSE.ts
@@ -1,3 +1,4 @@
 export const BROWSE = {
   HEADER_SUB_TEXT: "Discover potential cofounders.",
+  SKILL_SEARCH_PLACEHOLDER: "Enter skills to filter by",
 };
--- src/components/browsing/SkillsFilterDropdown.tsx
+++ src/components/browsing/SkillsFilterDropdown.tsx
@@ -1,4 +1,5 @@
 import { ChevronDown } from "lucide-react";
+import { BROWSE } from "../../../shared/constants/BROWSE";
 
 export const SkillsFilterDropdown = () => {
   return (
@@ -10,7 +11,7 @@ export const SkillsFilterDropdown = () => {
 
       <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2 p-4">
         <form className="flex gap-2">
-          <input className="input w-70" type="text" placeholder={"Search skills"} />
+          <input className="input w-70" type="text" placeholder={BROWSE.SKILL_SEARCH_PLACEHOLDER} />
           <button className="btn btn-primary">Add</button>
         </form>
       </ul>
