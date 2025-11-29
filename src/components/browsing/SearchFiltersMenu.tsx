a1b995ef2eb4c15b72934a775ea87f2f8a5f5636
delta6626 <hassan911ee@gmail.com>
2025-11-29 10:36:49 +0400

    refactor

--- src/components/browsing/SearchFiltersMenu.tsx
+++ src/components/browsing/SearchFiltersMenu.tsx
@@ -1,3 +1,5 @@
+import { SkillsFilterDropdown } from "./SkillsFilterDropdown";
+
 export const SearchFiltersMenu = () => {
   /*
   Relevant properties to be included for filtering.
@@ -22,13 +24,7 @@ export const SearchFiltersMenu = () => {
 
   return (
     <div className="min-w-full bg-blue-50">
-      <div className="dropdown dropdown-bottom">
-        <button tabIndex={0} role="button" className="btn">
-          Skills
-        </button>
-
-        <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2"></ul>
-      </div>
+      <SkillsFilterDropdown />
     </div>
   );
 };
