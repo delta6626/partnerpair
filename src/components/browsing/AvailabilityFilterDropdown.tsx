ca0bb2aef23d4fd164c031314b186c4378ac46ba
Delta6626 <hassan911ee@gmail.com>
2025-12-03 15:39:03 +0400

    fix stupid mistake

--- src/components/browsing/AvailabilityFilterDropdown.tsx
+++ src/components/browsing/AvailabilityFilterDropdown.tsx
@@ -1,6 +1,16 @@
 import { ChevronDown } from "lucide-react";
+import type { UserAvailability } from "../../../shared/types/UserAvailability";
+import { GenericChip } from "../ProfileViewer/GenericChip";
 
 export const AvailabilityFilterDropdown = () => {
+  const validAvailabilities: UserAvailability[] = [
+    "Available immediately",
+    "Available within a week",
+    "Available within 2 weeks",
+    "Available within a month",
+    "Available after a month",
+  ];
+
   return (
     <div className="dropdown dropdown-bottom">
       <button tabIndex={0} role="button" className="btn">
@@ -8,7 +18,13 @@ export const AvailabilityFilterDropdown = () => {
         <ChevronDown size={20} />
       </button>
 
-      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2"></ul>
+      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2">
+        <div className="">
+          {validAvailabilities.map((availability) => {
+            return <GenericChip key={`option-${availability}`} chipText={availability as string} fallbackText="" />;
+          })}
+        </div>
+      </ul>
     </div>
   );
 };
--- src/components/browsing/LookingForAvailabilityFilterDropdown.tsx
+++ src/components/browsing/LookingForAvailabilityFilterDropdown.tsx
@@ -1,16 +1,6 @@
 import { ChevronDown } from "lucide-react";
-import type { UserAvailability } from "../../../shared/types/UserAvailability";
-import { GenericChip } from "../ProfileViewer/GenericChip";
 
 export const LookingForAvailabilityFilterDropdown = () => {
-  const validAvailabilities: UserAvailability[] = [
-    "Available immediately",
-    "Available within a week",
-    "Available within 2 weeks",
-    "Available within a month",
-    "Available after a month",
-  ];
-
   return (
     <div className="dropdown dropdown-bottom">
       <button tabIndex={0} role="button" className="btn">
@@ -18,13 +8,7 @@ export const LookingForAvailabilityFilterDropdown = () => {
         <ChevronDown size={20} />
       </button>
 
-      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2">
-        <div className="">
-          {validAvailabilities.map((availability) => {
-            return <GenericChip key={`option-${availability}`} chipText={availability as string} fallbackText="" />;
-          })}
-        </div>
-      </ul>
+      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2"></ul>
     </div>
   );
 };
