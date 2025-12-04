3a99a1d13c854b2850c11eeee877af8d248888ba
Delta6626 <hassan911ee@gmail.com>
2025-12-04 23:34:47 +0400

    add check condition

--- src/components/browsing/StartupFilterDropdown.tsx
+++ src/components/browsing/StartupFilterDropdown.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export const StartupFilterDropdown = () => {
   const [showStartupOwners, setShowStartupOwners] = useState<boolean | null>(null);
-  const [showStartupSeekes, setShowStartupSeekers] = useState<boolean | null>(null);
+  const [showStartupSeekers, setShowStartupSeekers] = useState<boolean | null>(null);
 
   return (
     <div className="dropdown dropdown-bottom">
@@ -16,11 +16,11 @@ export const StartupFilterDropdown = () => {
         <div className="flex flex-col gap-2">
           <div className="flex items-center justify-between">
             <h1>Show users with startups</h1>
-            <input type="radio" className="radio" />
+            <input type="radio" className="radio" checked={showStartupOwners ? showStartupOwners : false} />
           </div>
           <div className="flex items-center justify-between">
             <h1>Show users who want to join a startup</h1>
-            <input type="radio" className="radio" />
+            <input type="radio" className="radio" checked={showStartupSeekers ? showStartupSeekers : false} />
           </div>
         </div>
       </ul>
