eddbb50e7697d1b2b4311d47582b08dd4c9dbf60
Delta6626 <hassan911ee@gmail.com>
2025-12-04 22:48:25 +0400

    add option

--- src/components/browsing/StartupFilterDropdown.tsx
+++ src/components/browsing/StartupFilterDropdown.tsx
@@ -8,7 +8,14 @@ export const StartupFilterDropdown = () => {
         <ChevronDown size={20} />
       </button>
 
-      <ul tabIndex={0} className="dropdown-content menu bg-base-200 w-100 rounded-box z-1 mt-2 p-4"></ul>
+      <ul tabIndex={0} className="dropdown-content menu bg-base-200 w-100 rounded-box z-1 mt-2 p-4">
+        <div className="">
+          <div className="flex items-center justify-between">
+            <h1>Show users with startups</h1>
+            <input type="radio" className="radio" />
+          </div>
+        </div>
+      </ul>
     </div>
   );
 };
