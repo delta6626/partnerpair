e28558f9b46c39d06db66224fc92682b6b3ad1b0
Delta6626 <hassan911ee@gmail.com>
2025-12-06 01:45:24 +0400

    add boilerplate

--- src/components/browsing/LookingForSkillsFilterDropdown.tsx
+++ src/components/browsing/LookingForSkillsFilterDropdown.tsx
@@ -1,7 +1,15 @@
 import { ChevronDown } from "lucide-react";
 import { BROWSE } from "../../../shared/constants/BROWSE";
+import type { FormEvent } from "react";
+import { useSearchParams } from "react-router-dom";
 
 export const LookingForSkillsFilterDropDown = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="dropdown dropdown-bottom">
       <button tabIndex={0} role="button" className="btn">
@@ -10,7 +18,7 @@ export const LookingForSkillsFilterDropDown = () => {
       </button>
 
       <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2 p-4">
-        <form className="flex gap-2">
+        <form className="flex gap-2" onSubmit={handleSubmit}>
           <input className="input w-100" type="text" placeholder={BROWSE.LOOKING_FOR_SKILL_SEARCH_PLACEHOLDER} />
           <button type="submit" className="btn btn-primary">
             Add
