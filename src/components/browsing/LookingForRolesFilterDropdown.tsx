765bb54c5b95c27e147ce117e322cdb4964ce7da
Delta6626 <hassan911ee@gmail.com>
2025-12-06 02:02:30 +0400

    boilerplate

--- src/components/browsing/LookingForRolesFilterDropdown.tsx
+++ src/components/browsing/LookingForRolesFilterDropdown.tsx
@@ -8,7 +8,12 @@ export const LookingForRolesFilterDropdown = () => {
         <ChevronDown size={20} />
       </button>
 
-      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2"></ul>
+      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-200 mt-2 p-4">
+        <div className="">
+          <h1 className="text-accent">Options</h1>
+          <div className="flex flex-wrap gap-2 mt-2"></div>
+        </div>
+      </ul>
     </div>
   );
 };
