8b333a16c2c3273ab2dcaef757dba07b33ecb48f
Delta6626 <hassan911ee@gmail.com>
2025-12-06 01:39:13 +0400

    add form

--- src/components/browsing/LookingForSkillsFilterDropdown.tsx
+++ src/components/browsing/LookingForSkillsFilterDropdown.tsx
@@ -1,4 +1,5 @@
 import { ChevronDown } from "lucide-react";
+import { BROWSE } from "../../../shared/constants/BROWSE";
 
 export const LookingForSkillsFilterDropDown = () => {
   return (
@@ -8,7 +9,14 @@ export const LookingForSkillsFilterDropDown = () => {
         <ChevronDown size={20} />
       </button>
 
-      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2"></ul>
+      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2 p-4">
+        <form className="flex gap-2">
+          <input className="input w-100" type="text" placeholder={BROWSE.SKILL_SEARCH_PLACEHOLDER} />
+          <button type="submit" className="btn btn-primary">
+            Add
+          </button>
+        </form>
+      </ul>
     </div>
   );
 };
