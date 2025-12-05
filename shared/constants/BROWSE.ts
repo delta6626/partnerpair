e0ca9d5b957ac044b49b4f35ef756d71713e6245
Delta6626 <hassan911ee@gmail.com>
2025-12-06 01:40:29 +0400

    change text

--- shared/constants/BROWSE.ts
+++ shared/constants/BROWSE.ts
@@ -1,4 +1,5 @@
 export const BROWSE = {
   HEADER_SUB_TEXT: "Discover potential cofounders.",
   SKILL_SEARCH_PLACEHOLDER: "Enter skills to filter by",
+  LOOKING_FOR_SKILL_SEARCH_PLACEHOLDER: "Filter by skills others are seeking",
 };
