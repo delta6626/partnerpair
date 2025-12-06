53da055d85b5ac7d0f8e72d809556385782a5541
Delta6626 <hassan911ee@gmail.com>
2025-12-07 01:20:53 +0400

    set up variables

--- src/components/browsing/LookingForCommitmentFilterDropdown.tsx
+++ src/components/browsing/LookingForCommitmentFilterDropdown.tsx
@@ -1,7 +1,10 @@
 import { ChevronDown } from "lucide-react";
 import type { UserCommitmentLevel } from "../../../shared/types/UserCommitmentLevel";
+import { useSearchParams } from "react-router-dom";
 
 export const LookingForCommitmentFilterDropdown = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const validCommitmentLevels: UserCommitmentLevel[] = [
     "10 to 20 hours per week",
     "20 to 30 hours per week",
@@ -10,6 +13,11 @@ export const LookingForCommitmentFilterDropdown = () => {
     "50 to 60 hours per week",
     "More than 60 hours per week",
   ];
+
+  const commitmentLevels = searchParams.get("lookingForCommitments")?.split(",") ?? [];
+  const validParameterCommitmentLevels = commitmentLevels.filter((level: string) =>
+    validCommitmentLevels.includes(level as UserCommitmentLevel)
+  );
 
   return (
     <div className="dropdown dropdown-bottom dropdown-end">
