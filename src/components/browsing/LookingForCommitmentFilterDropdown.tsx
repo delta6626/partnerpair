a5174e07a21f06bc3631eeee7d0df6e60b464d81
Delta6626 <hassan911ee@gmail.com>
2025-12-07 01:18:17 +0400

    edit style

--- src/components/browsing/LookingForCommitmentFilterDropdown.tsx
+++ src/components/browsing/LookingForCommitmentFilterDropdown.tsx
@@ -2,13 +2,13 @@ import { ChevronDown } from "lucide-react";
 
 export const LookingForCommitmentFilterDropdown = () => {
   return (
-    <div className="dropdown dropdown-bottom">
+    <div className="dropdown dropdown-bottom dropdown-end">
       <button tabIndex={0} role="button" className="btn">
         Commitment they Seek
         <ChevronDown size={20} />
       </button>
 
-      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2"></ul>
+      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-170 mt-2 p-4"></ul>
     </div>
   );
 };
