80f74a202480e06225e91ab5dbe64b9838d08ffd
Delta6626 <hassan911ee@gmail.com>
2025-12-07 01:11:25 +0400

    render roles + add limit

--- src/components/browsing/LookingForRolesFilterDropdown.tsx
+++ src/components/browsing/LookingForRolesFilterDropdown.tsx
@@ -1,7 +1,8 @@
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, XIcon } from "lucide-react";
 import type { UserRole } from "../../../shared/types/UserRole";
 import { GenericChip } from "../ProfileViewer/GenericChip";
 import { useSearchParams } from "react-router-dom";
+import { SETTINGS } from "../../../shared/constants/SETTINGS";
 
 export const LookingForRolesFilterDropdown = () => {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -49,6 +50,7 @@ export const LookingForRolesFilterDropdown = () => {
 
   const handleRoleAddition = (role: string) => {
     if (validParameterRoles.includes(role)) return;
+    if (validParameterRoles.length >= SETTINGS.MAX_ROLE_COUNT) return;
 
     const trimmedRole = role.trim();
     if (!trimmedRole) return;
@@ -65,6 +67,21 @@ export const LookingForRolesFilterDropdown = () => {
       </button>
 
       <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-200 mt-2 p-4">
+        <div>
+          {validParameterRoles.length > 0 && <h1 className="text-accent">Selected</h1>}
+          {validParameterRoles.length > 0 && (
+            <div className="flex flex-wrap gap-2 mt-2 mb-4">
+              {validParameterRoles.map((role) => {
+                return (
+                  <GenericChip key={`selected-${role}`} chipText={role} fallbackText="">
+                    <XIcon size={20} className="hover:text-error focus:text-error ease-in-out duration-200" />
+                  </GenericChip>
+                );
+              })}
+            </div>
+          )}
+        </div>
+
         <div className="">
           <h1 className="text-accent">Options</h1>
           <div className="flex flex-wrap gap-2 mt-2">
