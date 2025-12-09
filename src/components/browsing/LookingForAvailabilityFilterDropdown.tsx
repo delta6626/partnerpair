1501a084763ec3008888b2740468e29083047e30
Delta6626 <hassan911ee@gmail.com>
2025-12-09 19:25:55 +0400

    edit style

--- src/components/browsing/LookingForAvailabilityFilterDropdown.tsx
+++ src/components/browsing/LookingForAvailabilityFilterDropdown.tsx
@@ -8,7 +8,7 @@ export const LookingForAvailabilityFilterDropdown = () => {
         <ChevronDown size={20} />
       </button>
 
-      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2"></ul>
+      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box w-160 z-1 mt-2 p-4"></ul>
     </div>
   );
 };
