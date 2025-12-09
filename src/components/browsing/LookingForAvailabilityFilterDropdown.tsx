6995b92032c1e3c816e7dd08b44342b89ef8e596
Delta6626 <hassan911ee@gmail.com>
2025-12-09 19:31:02 +0400

    render valid options

--- src/components/browsing/LookingForAvailabilityFilterDropdown.tsx
+++ src/components/browsing/LookingForAvailabilityFilterDropdown.tsx
@@ -2,6 +2,7 @@ import { ChevronDown } from "lucide-react";
 import type { UserAvailability } from "../../../shared/types/UserAvailability";
 import { useSearchParams } from "react-router-dom";
 import { BROWSE } from "../../../shared/constants/BROWSE";
+import { GenericChip } from "../ProfileViewer/GenericChip";
 
 export const LookingForAvailabilityFilterDropdown = () => {
   const validAvailabilities: UserAvailability[] = [
@@ -25,7 +26,16 @@ export const LookingForAvailabilityFilterDropdown = () => {
         <ChevronDown size={20} />
       </button>
 
-      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box w-160 z-1 mt-2 p-4"></ul>
+      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box w-160 z-1 mt-2 p-4">
+        <div className="">
+          <h1 className="text-accent">Options</h1>
+          <div className="flex flex-wrap gap-2 mt-2">
+            {validAvailabilities.map((availability) => {
+              return <GenericChip key={`option-${availability}`} chipText={availability as string} />;
+            })}
+          </div>
+        </div>
+      </ul>
     </div>
   );
 };
