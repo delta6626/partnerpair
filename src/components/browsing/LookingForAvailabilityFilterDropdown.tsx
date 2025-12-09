51a4516a6c57dca33059f9c84fd6f856ce176ce1
Delta6626 <hassan911ee@gmail.com>
2025-12-09 19:26:41 +0400

    add valid options

--- src/components/browsing/LookingForAvailabilityFilterDropdown.tsx
+++ src/components/browsing/LookingForAvailabilityFilterDropdown.tsx
@@ -1,6 +1,15 @@
 import { ChevronDown } from "lucide-react";
+import type { UserAvailability } from "../../../shared/types/UserAvailability";
 
 export const LookingForAvailabilityFilterDropdown = () => {
+  const validAvailabilities: UserAvailability[] = [
+    "Available immediately",
+    "Available within a week",
+    "Available within 2 weeks",
+    "Available within a month",
+    "Available after a month",
+  ];
+
   return (
     <div className="dropdown dropdown-bottom">
       <button tabIndex={0} role="button" className="btn">
