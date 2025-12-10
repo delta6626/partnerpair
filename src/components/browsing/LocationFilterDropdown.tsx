40f4e0ee041732d3a5ad157b6e5ad7c0ec7d02b9
Delta6626 <hassan911ee@gmail.com>
2025-12-10 20:23:11 +0400

    render country list

--- src/components/browsing/LocationFilterDropdown.tsx
+++ src/components/browsing/LocationFilterDropdown.tsx
@@ -9,7 +9,22 @@ export const LocationFilterDropdown = () => {
         <ChevronDown size={20} />
       </button>
 
-      <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2"></ul>
+      <ul
+        tabIndex={0}
+        className="dropdown-content menu bg-base-200 rounded-box z-10 w-80 mt-2 max-h-80 grid grid-cols-1 overflow-y-auto"
+      >
+        {defaultCountries.map((c) => {
+          const parsed = parseCountry(c);
+          return (
+            <li key={parsed.iso2}>
+              <button className="flex items-center px-4 py-3">
+                <FlagImage iso2={parsed.iso2} style={{ width: "20px" }} />
+                <p>{parsed.name}</p>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
