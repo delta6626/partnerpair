c72d073e3c6072106a375f52823dc1ad72dc7200
Delta6626 <hassan911ee@gmail.com>
2025-12-11 20:05:27 +0400

    add search button

--- src/components/browsing/SearchUser.tsx
+++ src/components/browsing/SearchUser.tsx
@@ -4,10 +4,12 @@ export const SearchUser = () => {
   const { isOpen, setIsOpen } = useFilterMenuStore();
 
   return (
-    <>
+    <div className="flex gap-2">
       <button className="btn" onClick={() => setIsOpen(!isOpen)}>
         {isOpen ? "Close Filters" : "Open Filters"}
       </button>
-    </>
+
+      <button className="btn btn-primary">Search</button>
+    </div>
   );
 };
