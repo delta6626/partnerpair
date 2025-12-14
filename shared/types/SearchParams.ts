b0bb037996758091c532daa15392d711081348db
Delta6626 <hassan911ee@gmail.com>
2025-12-14 15:14:42 +0400

    update type

--- shared/types/SearchParams.ts
+++ shared/types/SearchParams.ts
@@ -4,12 +4,12 @@ import type { UserPreferredCompanyStage } from "./UserPreferredCompanyStage";
 import type { UserRole } from "./UserRole";
 
 export interface SearchParams {
-  location: string;
+  location: string | "";
   skills: string[];
   roles: UserRole[];
   commitmentLevels: UserCommitmentLevel[];
   availabilities: UserAvailability[];
-  profileType: "startupOwner" | "startupSeeker";
+  profileType: "startupOwner" | "startupSeeker" | "";
   preferredStartupStages: UserPreferredCompanyStage[];
   skillsSought: string[];
   rolesSought: UserRole[];
