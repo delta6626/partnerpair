1a35668a803896fd56fab967a3fa46742b29f430
Delta6626 <hassan911ee@gmail.com>
2025-12-21 17:29:27 +0400

    boilerplate

--- src/components/browsing/NameFilterInput.tsx
+++ src/components/browsing/NameFilterInput.tsx
@@ -1,5 +1,18 @@
+import { useSearchParams } from "react-router-dom";
 import { BROWSE } from "../../../shared/constants/BROWSE";
+import type { ChangeEvent } from "react";
 
 export const NameFilterInput = () => {
-  return <input type="text" className="input bg-base-200 border-none" placeholder={BROWSE.NAME_SEARCH_PLACEHOLDER} />;
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleNameParamChange = (e: ChangeEvent<HTMLInputElement>) => {};
+
+  return (
+    <input
+      type="text"
+      className="input bg-base-200 border-none"
+      placeholder={BROWSE.NAME_SEARCH_PLACEHOLDER}
+      onChange={handleNameParamChange}
+    />
+  );
 };
