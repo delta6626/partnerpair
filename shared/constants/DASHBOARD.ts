2c8941ad52ff9380b7952b0dfb4c7c02c4058b62
Delta6626 <hassan911ee@gmail.com>
2025-12-21 23:31:27 +0400

    change copy

--- shared/constants/DASHBOARD.ts
+++ shared/constants/DASHBOARD.ts
@@ -1,7 +1,7 @@
 export const DASHBOARD = {
   HEADER_SUB_TEXT: "Here's what's happening with your cofounder search.",
-  PROFILE_COUNTER_USER_BASIC: "Upgrade to see more insights",
-  PROFILE_COUNTER_USER_PRO: "Click to see more",
+  PROFILE_COUNTER_USER_BASIC: "Upgrade for more insights",
+  PROFILE_COUNTER_USER_PRO: "View details",
   CONTACT_COUNTER_NO_CONTACTS: "Browse and add contacts",
   CONTACT_COUNTER_VIEW_SAVED: "View saved contacts",
   SUGGESTED_PROFILE_LOADING_ERROR: "An unexpected error occured.",
