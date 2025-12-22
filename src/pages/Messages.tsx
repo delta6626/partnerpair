cc33d464c1f29c914a14a09dfbf2f4533448eaf9
Delta6626 <hassan911ee@gmail.com>
2025-12-22 19:01:59 +0400

    add message

--- src/pages/Messages.tsx
+++ src/pages/Messages.tsx
@@ -2,6 +2,7 @@ import { Loader } from "lucide-react";
 import { MainNavbar } from "../components/navigation/MainNavbar";
 import { useInitializeUser } from "../hooks/useInitializeUser";
 import { useTheme } from "../hooks/useTheme";
+import { ProfileStatusMessage } from "../components/user/ProfileStatusMessage";
 
 export const Messages = () => {
   useTheme();
@@ -17,6 +18,12 @@ export const Messages = () => {
         <div className="w-full flex flex-col grow min-h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
           <div className="py-4">
             <MainNavbar />
+          </div>
+
+          <div className="py-8">
+            <div className="mb-4">
+              <ProfileStatusMessage />
+            </div>
           </div>
         </div>
       )}
