fd96e4834e1cf329db2be317035db66990369359
Delta6626 <hassan911ee@gmail.com>
2025-12-24 11:39:44 +0400

    import backend function

--- src/components/messaging/MessageUser.tsx
+++ src/components/messaging/MessageUser.tsx
@@ -1,3 +1,8 @@
+import { httpsCallable } from "firebase/functions";
+import { functions } from "../../services/firebaseConfig";
+
 export const MessageUser = () => {
+  const initiateChat = httpsCallable(functions, "initiateChat");
+
   return <button className="btn btn-primary">Message</button>;
 };
