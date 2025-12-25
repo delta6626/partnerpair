1d42bf426fa0369b0f3a0bcc9b57c48f149315f9
Delta6626 <hassan911ee@gmail.com>
2025-12-25 12:46:31 +0400

    fetch chats

--- src/components/messaging/ChatInbox.tsx
+++ src/components/messaging/ChatInbox.tsx
@@ -1,8 +1,56 @@
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { firestore } from "../../services/firebaseConfig";
+import { useState, useEffect } from "react";
+import type { ChatMetaData } from "../../../shared/types/ChatMetaData";
+import { useQuery } from "@tanstack/react-query";
+import { QUERY_KEYS } from "../../../shared/constants/QUERY_KEYS";
+import { getUserId } from "../../services/authentication/authServices";
+import { Loader } from "../Loader";
+
 export const ChatInbox = () => {
+  const [chats, setChats] = useState<ChatMetaData[]>([]);
+  const {
+    data: userId,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: [QUERY_KEYS.USER_ID],
+    queryFn: getUserId,
+  });
+
+  useEffect(() => {
+    if (!userId) return;
+
+    const chatsQuery = query(collection(firestore, "chats"), where("participants", "array-contains", userId));
+
+    const unsubscribe = onSnapshot(chatsQuery, (snapshot) => {
+      const updatedChats = snapshot.docs.map(
+        (doc) =>
+          ({
+            id: doc.id,
+            ...doc.data(),
+          } as ChatMetaData)
+      );
+      setChats(updatedChats);
+    });
+
+    return () => unsubscribe();
+  }, [userId]);
+
+  console.log(chats);
+
   return (
     <div className="flex min-w-100 flex-col gap-4">
       <input className="input w-full bg-base-200 border-none" type="text" placeholder={"Search chats"} />
-      <div className="flex flex-1 bg-base-200 rounded-3xl"></div>
+      <div className="flex flex-1 bg-base-200 rounded-3xl">
+        {isLoading ? (
+          <div className="flex flex-1 items-center justify-center">
+            <Loader />
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 };
