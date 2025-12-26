a2aabbc1832a1a66bd504bbd0b47f976d778ac7e
Delta6626 <hassan911ee@gmail.com>
2025-12-27 00:00:36 +0400

    add wrapper

--- src/components/messaging/ChatCard.tsx
+++ src/components/messaging/ChatCard.tsx
@@ -5,17 +5,19 @@ export const ChatCard = ({ chat, currentUserId }: { chat: ChatMetaData; currentU
   const otherParticipantId = chat.participants.find((id) => id !== currentUserId)!;
 
   return (
-    <div className="p-4 border-1 border-accent rounded-3xl w-full flex gap-4">
-      <img
-        className="rounded-3xl"
-        src={chat.participantProfileImageUrls[otherParticipantId]}
-        alt="Profile"
-        width={50}
-      />
+    <div className="p-4 border-1 border-accent rounded-3xl w-full flex justify-between">
+      <div className="flex gap-4">
+        <img
+          className="rounded-3xl"
+          src={chat.participantProfileImageUrls[otherParticipantId]}
+          alt="Profile"
+          width={50}
+        />
 
-      <div className="flex flex-col justify-center items-start">
-        <h1 className="font-medium">{chat.participantNames[otherParticipantId]}</h1>
-        <p className="text-accent">{chat.lastMessage ? chat.lastMessage : MESSAGES.NO_MESSAGES}</p>
+        <div className="flex flex-col justify-center items-start">
+          <h1 className="font-medium">{chat.participantNames[otherParticipantId]}</h1>
+          <p className="text-accent">{chat.lastMessage ? chat.lastMessage : MESSAGES.NO_MESSAGES}</p>
+        </div>
       </div>
     </div>
   );
