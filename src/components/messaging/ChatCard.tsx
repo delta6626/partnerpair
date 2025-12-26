e7776f3cab8eab8175a794b74ffc085c15d3f69a
Delta6626 <hassan911ee@gmail.com>
2025-12-26 23:50:53 +0400

    add ui elements to chat card

--- src/components/messaging/ChatCard.tsx
+++ src/components/messaging/ChatCard.tsx
@@ -1,16 +1,21 @@
 import type { ChatMetaData } from "../../../shared/types/ChatMetaData";
 
 export const ChatCard = ({ chat, currentUserId }: { chat: ChatMetaData; currentUserId: string }) => {
-  const otherParticipantId = chat.participants.find((id) => id !== currentUserId);
+  const otherParticipantId = chat.participants.find((id) => id !== currentUserId)!;
 
   return (
-    <div className="p-2 border-1 border-accent rounded-3xl w-full">
+    <div className="p-2 border-1 border-accent rounded-3xl w-full flex gap-4">
       <img
         className="rounded-3xl"
-        src={chat.participantProfileImageUrls[otherParticipantId!]}
+        src={chat.participantProfileImageUrls[otherParticipantId]}
         alt="Profile"
         width={50}
       />
+
+      <div className="flex flex-col justify-center items-start">
+        <h1 className="">{chat.participantNames[otherParticipantId]}</h1>
+        <p className="text-accent">{chat.lastMessage ? chat.lastMessage : "No messages yet. Say hi!"}</p>
+      </div>
     </div>
   );
 };
