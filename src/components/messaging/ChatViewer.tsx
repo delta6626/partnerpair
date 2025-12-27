b18cc1b5ecb4774dc28a2bd477efdb02d3a989c3
Delta6626 <hassan911ee@gmail.com>
2025-12-28 01:35:26 +0400

    use constants

--- src/components/messaging/ChatViewer.tsx
+++ src/components/messaging/ChatViewer.tsx
@@ -1,3 +1,4 @@
+import { MESSAGES } from "../../../shared/constants/MESSAGES";
 import { useSelectedChatStore } from "../../store/useSelectedChatStore";
 
 export const ChatViewer = () => {
@@ -5,7 +6,13 @@ export const ChatViewer = () => {
 
   return (
     <div className="w-full flex flex-1 bg-base-200 rounded-3xl">
-      {!selectedChatId && <div className="flex flex-1 items-center justify-center text-accent"></div>}
+      {!selectedChatId && (
+        <div className="flex flex-col flex-1 items-center justify-center text-accent">
+          <p>{MESSAGES.NO_CHAT_OPENED}</p>
+          <br />
+          <p>{MESSAGES.START_MESSAGING}</p>
+        </div>
+      )}
     </div>
   );
 };
