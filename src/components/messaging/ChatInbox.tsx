ae8729ac3e523b1c510dbbd7acb6ec5a277514e8
Delta6626 <hassan911ee@gmail.com>
2025-12-28 21:00:51 +0400

    hide scrollbar

--- src/components/messaging/ChatInbox.tsx
+++ src/components/messaging/ChatInbox.tsx
@@ -115,7 +115,7 @@ export const ChatInbox = () => {
         )}
 
         {filteredChats.length > 0 && (
-          <div className="p-4 w-full flex flex-col gap-2 overflow-y-auto scrollbar-thin">
+          <div className="p-4 w-full flex flex-col gap-2 overflow-y-auto scrollbar-none">
             {filteredChats.map((chat) => {
               return <ChatCard chat={chat} currentUserId={userId ?? ""} />;
             })}
