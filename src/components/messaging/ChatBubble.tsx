fe5954a99c085a9efd2cf9b8287a87f94cad494f
Delta6626 <hassan911ee@gmail.com>
2025-12-31 12:28:53 +0400

    add styles

--- src/components/messaging/ChatBubble.tsx
+++ src/components/messaging/ChatBubble.tsx
@@ -15,7 +15,7 @@ export const ChatBubble = ({
       className={
         isSentByCurrentUser
           ? "max-w-[50%] bg-primary px-4 py-2 rounded-t-3xl rounded-bl-3xl rounded-br-md self-end"
-          : "bg-base-300"
+          : "max-w-[50%] bg-base-300 px-4 py-2 rounded-t-3xl rounded-bl-3xl rounded-br-md self-start"
       }
     >
       {message.content}
