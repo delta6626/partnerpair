267213f70ecee150c83b78a2aca24c150a9d8a6d
Delta6626 <hassan911ee@gmail.com>
2026-01-04 12:53:22 +0400

    add placeholder for displaying sent time

--- src/components/messaging/ChatBubble.tsx
+++ src/components/messaging/ChatBubble.tsx
@@ -11,14 +11,20 @@ export const ChatBubble = ({
 }) => {
   const isSentByCurrentUser = message.senderId === currentUserId;
   return (
-    <div
-      className={
-        isSentByCurrentUser
-          ? "text-white max-w-[70%] bg-primary px-4 py-2 rounded-t-3xl rounded-bl-3xl rounded-br-md self-end cursor-default"
-          : "max-w-[70%] bg-base-300 px-4 py-2 rounded-t-3xl rounded-bl-md rounded-br-3xl self-start cursor-default"
-      }
-    >
-      {message.content}
-    </div>
+    <>
+      <div
+        className={
+          isSentByCurrentUser
+            ? "text-white max-w-[70%] bg-primary px-4 py-2 rounded-t-3xl rounded-bl-3xl rounded-br-md self-end cursor-default"
+            : "max-w-[70%] bg-base-300 px-4 py-2 rounded-t-3xl rounded-bl-md rounded-br-3xl self-start cursor-default"
+        }
+      >
+        {message.content}
+      </div>
+
+      <p className={isSentByCurrentUser ? "self-end text-accent mt-[-8px]" : "self-start text-accent mt-[-8px]"}>
+        {message.sentAt.seconds}
+      </p>
+    </>
   );
 };
