e6287647cb52e536294657797804f8fc1d06627a
Delta6626 <hassan911ee@gmail.com>
2026-01-06 11:44:45 +0400

    add blink animation

--- src/components/messaging/UnreadMessageCounter.tsx
+++ src/components/messaging/UnreadMessageCounter.tsx
@@ -39,6 +39,11 @@ export const UnreadMessageCounter = () => {
       <div className="flex gap-2 text-accent items-center">
         <Mail size={20} />
         Unread Messages
+        {unreadMessageCount != 0 ? (
+          <div className="w-2 h-2 rounded-full bg-error animate-[blink_1.5s_ease-in-out_infinite] mt-[1.5px]"></div>
+        ) : (
+          ""
+        )}
       </div>
 
       {!isError && (
