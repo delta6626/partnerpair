f420c3702e14e64c7bc3b5688741a97d2d222471
Delta6626 <hassan911ee@gmail.com>
2026-01-09 15:45:43 +0400

    remove unused constant

--- shared/constants/MESSAGES.ts
+++ shared/constants/MESSAGES.ts
@@ -7,5 +7,4 @@ export const MESSAGES = {
   NO_CHAT_OPENED: "No chat opened yet.",
   START_MESSAGING: "Select a chat to start messaging.",
   WRITE_MESSAGE: "Type your message here",
-  SEND: "Send",
 };
