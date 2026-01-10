90ccd5f8084747332433db40f6ec21bf35a2a283
Delta6626 <hassan911ee@gmail.com>
2026-01-10 22:33:56 +0400

    design 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,23 @@
+import { Link } from "react-router-dom";
 import { Navbar } from "../components/navigation/Navbar";
 
 export const NotFound = () => {
   return (
-    <div className="w-full min-h-[100vh] bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+    <div className="w-full font-inter flex flex-col min-h-[100vh] bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
       <div className="py-4">
         <Navbar></Navbar>
+      </div>
+
+      <div className="flex flex-col items-center justify-center grow w-full">
+        <h1 className="text-9xl font-black">404</h1>
+        <h1 className="text-4xl font-bold mt-4">There's nothing here</h1>
+        <p className="text-accent text-center mt-2">
+          {" "}
+          We couldn’t find the page you requested. The link may be <br /> outdated, or the URL may be incorrect.
+        </p>
+        <Link className="btn btn-primary mt-2" to={"/"}>
+          Go Home
+        </Link>
       </div>
     </div>
   );
