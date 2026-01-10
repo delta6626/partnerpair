f29f9a698569a6c0254f00b289355f4987d5745e
Delta6626 <hassan911ee@gmail.com>
2026-01-10 22:16:05 +0400

    integrate navbar

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,11 @@
+import { Navbar } from "../components/navigation/Navbar";
+
 export const NotFound = () => {
-  return <div></div>;
+  return (
+    <div className="w-full min-h-[100vh] bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+      <div className="py-4">
+        <Navbar></Navbar>
+      </div>
+    </div>
+  );
 };
