2ff95a768045328a8580f5b9b4e15d160dc468a0
Delta6626 <hassan911ee@gmail.com>
2026-01-13 16:44:40 +0400

    extract function to delete all user uploaded photos

--- src/services/userProfile/userProfileServices.ts
+++ src/services/userProfile/userProfileServices.ts
@@ -4,7 +4,7 @@ import { handleFirebaseError } from "../authentication/firebaseErrorHandler";
 import { firestore, storage } from "../firebaseConfig";
 import { collection, doc, getDocs, query, updateDoc, where, writeBatch } from "firebase/firestore";
 import type { User } from "../../../shared/types/User";
-import { deleteObject, getDownloadURL, listAll, ref, uploadBytes } from "firebase/storage";
+import { deleteObject, getDownloadURL, listAll, ref, uploadBytes, type StorageReference } from "firebase/storage";
 
 export const setVerificationStatus = async (status: boolean) => {
   const userId = await getUserId();
@@ -53,6 +53,17 @@ export const updateUserProfile = async (updatedUserProfile: User, chatMetaDataCh
   }
 };
 
+const deleteAllUserPhotos = async (userFolderRef: StorageReference) => {
+  try {
+    const existingFiles = await listAll(userFolderRef);
+    await Promise.all(existingFiles.items.map((fileRef) => deleteObject(fileRef)));
+    return true; // all deletes resolved
+  } catch (error) {
+    console.error(error);
+    return false; // at least one delete failed
+  }
+};
+
 export const uploadUserPhoto = async (photo: File) => {
   const userId = await getUserId();
 
@@ -64,8 +75,8 @@ export const uploadUserPhoto = async (photo: File) => {
     const userFolderRef = ref(storage, `profilePhotos/${userId}/`);
 
     // Delete existing profile photos
-    const existingFiles = await listAll(userFolderRef);
-    await Promise.all(existingFiles.items.map((fileRef) => deleteObject(fileRef)));
+    const photosDeleted = await deleteAllUserPhotos(userFolderRef);
+    if (!photosDeleted) return false; // If photos cannot be deleted, cancel upload operation.
 
     const newPhotoRef = ref(storage, `profilePhotos/${userId}/${photo.name}`);
     const snapshot = await uploadBytes(newPhotoRef, photo);
