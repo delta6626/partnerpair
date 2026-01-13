359180ea8b297bf548ee9db634e4ae2825c9c9ce
Delta6626 <hassan911ee@gmail.com>
2026-01-13 17:34:30 +0400

    remove cross origin attribute

--- src/components/user/ProfileDropdown.tsx
+++ src/components/user/ProfileDropdown.tsx
@@ -13,7 +13,6 @@ export const ProfileDropdown = () => {
         role="button"
         src={user?.basicInfo.profileImageUrl}
         referrerPolicy="no-referrer"
-        crossOrigin="anonymous"
         className="btn btn-square rounded-full"
       />
       <ul tabIndex={0} className="dropdown-content menu bg-base-200 rounded-box z-1 w-fit mt-2 shadow-sm">
