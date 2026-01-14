72f73da59681f38b529ddd46ac9e376bb240b7e7
Delta6626 <hassan911ee@gmail.com>
2026-01-14 17:29:15 +0400

    create generic error modal

---
+++ src/components/modals/GenericErrorModal.tsx
@@ -0,0 +1,31 @@
+import { MODAL_ACTIONS } from "../../../shared/constants/MODAL_ACTIONS";
+
+export const GenericErrorModal = ({
+  modalId,
+  errorTitle,
+  errorText,
+}: {
+  modalId: string;
+  errorTitle: string;
+  errorText: string;
+}) => {
+  const closeModal = () => {
+    const modal = document.getElementById(modalId) as HTMLDialogElement;
+    modal.close();
+  };
+
+  return (
+    <dialog id={modalId} className="modal">
+      <div className="modal-box bg-base-300 border border-base-100">
+        <h1 className="text-lg font-medium">{errorTitle}</h1>
+        <p className="mt-2 text-accent">{errorText}</p>
+
+        <div className="flex justify-end mt-4">
+          <button type="button" className="btn" onClick={closeModal}>
+            {MODAL_ACTIONS.ACTION_OK}
+          </button>
+        </div>
+      </div>
+    </dialog>
+  );
+};
