c795b833eba187064c61b2721dec9a6fe265fc86
Delta6626 <hassan911ee@gmail.com>
2026-01-14 11:58:40 +0400

    blogs page boilerplate

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,3 +1,14 @@
+import { MainNavbar } from "../components/navigation/MainNavbar";
+import { useTheme } from "../hooks/useTheme";
+
 export const Blogs = () => {
-  return <div className="">blog</div>;
+  useTheme();
+
+  return (
+    <div className="w-full flex flex-col grow min-h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+      <div className="py-4">
+        <MainNavbar />
+      </div>
+    </div>
+  );
 };
