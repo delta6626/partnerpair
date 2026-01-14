55aa8068e0af7281c62b780348286cce5749a31f
Delta6626 <hassan911ee@gmail.com>
2026-01-14 12:03:14 +0400

    rewrite boilerplate

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,14 +1,25 @@
+import { Loader } from "../components/Loader";
 import { MainNavbar } from "../components/navigation/MainNavbar";
+import { useInitializeUser } from "../hooks/useInitializeUser";
 import { useTheme } from "../hooks/useTheme";
 
 export const Blogs = () => {
   useTheme();
+  const { loading } = useInitializeUser();
 
   return (
-    <div className="w-full flex flex-col grow min-h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
-      <div className="py-4">
-        <MainNavbar />
-      </div>
+    <div className="">
+      {loading ? (
+        <div className="w-full min-h-[100vh] bg-base-300 flex items-center justify-center">
+          <Loader />
+        </div>
+      ) : (
+        <div className="w-full flex flex-col grow min-h-[100vh] font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+          <div className="py-4">
+            <MainNavbar />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
