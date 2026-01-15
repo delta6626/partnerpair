865e4f79c2cddb8234b4ebdd32d9fddac770742e
Delta6626 <hassan911ee@gmail.com>
2026-01-15 11:58:39 +0400

    add icon in modal

--- src/components/modals/ProfileUpdateSuccessfulModal.tsx
+++ src/components/modals/ProfileUpdateSuccessfulModal.tsx
@@ -1,3 +1,4 @@
+import { CheckCircle2 } from "lucide-react";
 import { MODALS } from "../../../shared/constants/MODALS";
 import { MODAL_ACTIONS } from "../../../shared/constants/MODAL_ACTIONS";
 
@@ -10,7 +11,10 @@ export const ProfileUpdateSuccessfulModal = () => {
   return (
     <dialog id={MODALS.PROFILE_UPDATE_SUCCESS_MODAL.ID} className="modal">
       <div className="modal-box bg-base-300 border border-base-100">
-        <h1 className="text-lg font-medium">{MODALS.PROFILE_UPDATE_SUCCESS_MODAL.TITLE}</h1>
+        <div className="flex items-center gap-2">
+          <CheckCircle2 size={20} className="text-primary" />
+          <h1 className="text-lg font-medium">{MODALS.PROFILE_UPDATE_SUCCESS_MODAL.TITLE}</h1>
+        </div>
         <p className="mt-2 text-accent">{MODALS.PROFILE_UPDATE_SUCCESS_MODAL.DESCRIPTION}</p>
 
         <div className="flex justify-end mt-4">
