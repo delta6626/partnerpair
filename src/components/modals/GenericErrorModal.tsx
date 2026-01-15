fe37291d8fc81f6a3cf9fa04d6d79bf120b4ddbc
Delta6626 <hassan911ee@gmail.com>
2026-01-15 11:57:15 +0400

    change modal appearance

--- src/components/modals/GenericErrorModal.tsx
+++ src/components/modals/GenericErrorModal.tsx
@@ -1,3 +1,4 @@
+import { AlertCircle } from "lucide-react";
 import { MODAL_ACTIONS } from "../../../shared/constants/MODAL_ACTIONS";
 
 export const GenericErrorModal = ({
@@ -17,8 +18,11 @@ export const GenericErrorModal = ({
   return (
     <dialog id={modalId} className="modal">
       <div className="modal-box bg-base-300 border border-base-100">
-        <h1 className="text-lg text-error font-medium">{errorTitle}</h1>
-        <p className="mt-2 text-error/60">{errorText}</p>
+        <div className="flex items-center gap-2">
+          <AlertCircle size={20} className="text-error" />
+          <h1 className="text-lg font-medium">{errorTitle}</h1>
+        </div>
+        <p className="mt-2 text-accent">{errorText}</p>
 
         <div className="flex justify-end mt-4">
           <button type="button" className="btn" onClick={closeModal}>
