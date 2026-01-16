b526b006bd5e2918984638fe01a3e841b1d6d07d
Delta6626 <hassan911ee@gmail.com>
2026-01-16 19:05:31 +0400

    add content

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -6,19 +6,33 @@ import { TwitterIcon } from "../../assets/customIcons/TwitterIcon";
 
 export const Footer = () => {
   return (
-    <div className="w-full font-inter bg-base-200 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28 py-8">
+    <div className="w-full flex justify-between font-inter bg-base-200 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28 py-8">
       <div className="flex flex-col gap-8 max-w-50">
         <Logo />
-        <p className="text-accent">{FOOTER.TAG_LINE}</p>
-        <div className="flex gap-4">
-          <Link to={FOOTER.GITHUB_REPO_LINK}>
-            <GithubIcon />
-          </Link>
+        <div className="flex flex-col gap-8">
+          <p className="text-accent">{FOOTER.TAG_LINE}</p>
+          <div className="flex gap-4">
+            <Link to={FOOTER.GITHUB_REPO_LINK}>
+              <GithubIcon />
+            </Link>
 
-          <Link to={FOOTER.PERSONAL_TWITTER_LINK}>
-            <TwitterIcon />
-          </Link>
+            <Link to={FOOTER.PERSONAL_TWITTER_LINK}>
+              <TwitterIcon />
+            </Link>
+          </div>
         </div>
+      </div>
+
+      <div className="">
+        <h1 className="text-lg font-semibold">Product</h1>
+      </div>
+
+      <div className="">
+        <h1 className="text-lg font-semibold">Legal</h1>
+      </div>
+
+      <div className="">
+        <h1 className="text-lg font-semibold">Contact</h1>
       </div>
     </div>
   );
