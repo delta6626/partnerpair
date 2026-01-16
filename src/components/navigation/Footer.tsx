1e2bed0ba07056d106547f6bf313d1146437e887
Delta6626 <hassan911ee@gmail.com>
2026-01-16 19:41:05 +0400

    fix styles

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -25,31 +25,67 @@ export const Footer = () => {
 
       <div className="flex flex-col gap-8">
         <h1 className="text-lg font-semibold">Product</h1>
-        <div className="text-accent flex flex-col gap-2">
-          <Link to={"/settings"}>Manage Subscription</Link>
-          <Link to={"/#about"}>About</Link>
-          <Link to={"/#pricing"}>Pricing</Link>
-          <Link to={"/#faq"}>FAQ</Link>
-          <Link to={FOOTER.GITHUB_REPO_LINK}>Source code</Link>
+        <div className="text-accent flex flex-col gap-2 font-semibold text-sm">
+          <Link className="hover:text-base-content transition-all ease-in-out duration-200" to={"/settings"}>
+            Manage Subscription
+          </Link>
+          <Link className="hover:text-base-content transition-all ease-in-out duration-200" to={"/#about"}>
+            About
+          </Link>
+          <Link className="hover:text-base-content transition-all ease-in-out duration-200" to={"/#pricing"}>
+            Pricing
+          </Link>
+          <Link className="hover:text-base-content transition-all ease-in-out duration-200" to={"/#faq"}>
+            FAQ
+          </Link>
+          <Link
+            className="hover:text-base-content transition-all ease-in-out duration-200"
+            to={FOOTER.GITHUB_REPO_LINK}
+          >
+            Source code
+          </Link>
         </div>
       </div>
 
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-8 font-semibold text-sm">
         <h1 className="text-lg font-semibold">Legal</h1>
         <div className="text-accent flex flex-col gap-2">
-          <Link to={"/privacy-policy"}>Privacy Policy</Link>
-          <Link to={"/terms-of-service"}>Terms of Service</Link>
-          <Link to={"/disclaimer"}>Community Guidelines</Link>
-          <Link to={"/disclaimer"}>Disclaimer</Link>
+          <Link className="hover:text-base-content transition-all ease-in-out duration-200" to={"/privacy-policy"}>
+            Privacy Policy
+          </Link>
+          <Link className="hover:text-base-content transition-all ease-in-out duration-200" to={"/terms-of-service"}>
+            Terms of Service
+          </Link>
+          <Link className="hover:text-base-content transition-all ease-in-out duration-200" to={"/disclaimer"}>
+            Community Guidelines
+          </Link>
+          <Link className="hover:text-base-content transition-all ease-in-out duration-200" to={"/disclaimer"}>
+            Disclaimer
+          </Link>
         </div>
       </div>
 
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-8 font-semibold text-sm">
         <h1 className="text-lg font-semibold">Contact</h1>
         <div className="text-accent flex flex-col gap-2">
-          <Link to={FOOTER.PERSONAL_GITHUB_LINK}>Developer Github</Link>
-          <Link to={FOOTER.PERSONAL_TWITTER_LINK}>Twitter</Link>
-          <Link to={`mailto:${FOOTER.PERSONAL_MAIL_ADDRESS}`}>Mail</Link>
+          <Link
+            className="hover:text-base-content transition-all ease-in-out duration-200s"
+            to={FOOTER.PERSONAL_GITHUB_LINK}
+          >
+            Developer Github
+          </Link>
+          <Link
+            className="hover:text-base-content transition-all ease-in-out duration-200s"
+            to={FOOTER.PERSONAL_TWITTER_LINK}
+          >
+            Twitter
+          </Link>
+          <Link
+            className="hover:text-base-content transition-all ease-in-out duration-200s"
+            to={`mailto:${FOOTER.PERSONAL_MAIL_ADDRESS}`}
+          >
+            Mail
+          </Link>
         </div>
       </div>
     </div>
