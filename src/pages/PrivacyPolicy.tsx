aef9303c7700f0e7d7a88b770d4896c54687c9d4
Delta6626 <hassan911ee@gmail.com>
2026-01-17 12:16:12 +0400

    improve layout

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -1,3 +1,26 @@
+import { Footer } from "../components/navigation/Footer";
+import { MainNavbar } from "../components/navigation/MainNavbar";
+import { useTheme } from "../hooks/useTheme";
+
 export const PrivacyPolicy = () => {
-  return <div className=""></div>;
+  useTheme();
+
+  return (
+    <>
+      <div className="w-full min-h-[100vh] flex flex-col font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+        <div className="py-4">
+          <MainNavbar />
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="py-8 w-full max-w-200 mt-4">
+            <h1 className="text-3xl font-bold">Privacy Policy</h1>
+            <p className="text-accent">Last Updated: January 17, 2026</p>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
 };
