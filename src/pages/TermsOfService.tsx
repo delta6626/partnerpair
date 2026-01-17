9e12b47fad8afb047cee5ec9b0b5e51588f92e77
delta6626 <hassan911ee@gmail.com>
2026-01-18 00:49:55 +0400

    create basic structure

--- src/pages/TermsOfService.tsx
+++ src/pages/TermsOfService.tsx
@@ -1,3 +1,20 @@
+import { Navbar } from "../components/navigation/Navbar";
+
 export const TermsOfService = () => {
-  return <div className=""></div>;
+  return (
+    <>
+      <div className="w-full min-h-[100vh] flex flex-col font-inter bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+        <div className="py-4">
+          <Navbar />
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className="py-8 w-full max-w-200 mt-4">
+            <h1 className="text-3xl font-bold">Terms of Service</h1>
+            <p className="text-accent">Last Updated: January 18, 2026</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
