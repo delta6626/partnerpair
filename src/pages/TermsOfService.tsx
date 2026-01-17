76bef93c4a51a657d94314f59c7ecc31dabeee2f
delta6626 <hassan911ee@gmail.com>
2026-01-18 01:00:58 +0400

    add content

--- src/pages/TermsOfService.tsx
+++ src/pages/TermsOfService.tsx
@@ -12,6 +12,14 @@ export const TermsOfService = () => {
           <div className="py-8 w-full max-w-200 mt-4">
             <h1 className="text-3xl font-bold">Terms of Service</h1>
             <p className="text-accent">Last Updated: January 18, 2026</p>
+
+            <section className="mt-8">
+              Welcome to PartnerPair (“PartnerPair”, “we”, “us”, or “our”). These Terms of Service (“Terms”) govern your
+              access to and use of PartnerPair, a web-based co-founder matching platform owned and operated by an
+              individual based in the United Arab Emirates. By creating an account or using PartnerPair, you agree to be
+              bound by these Terms.
+              <h1 className="pt-2">If you do not agree to these Terms, do not use PartnerPair.</h1>
+            </section>
           </div>
         </div>
       </div>
