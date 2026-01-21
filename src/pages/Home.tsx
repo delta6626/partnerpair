f3cecfa81ef20d632b8d717ee0079a20e29041f0
Delta6626 <hassan911ee@gmail.com>
2026-01-21 17:25:09 +0400

    create hero for landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,24 @@ import { Navbar } from "../components/navigation/Navbar";
 
 export const Home = () => {
   return (
-    <div className="w-full min-h-[100vh] bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
+    <div className="w-full font-inter min-h-[100vh] bg-base-300 px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28">
       <div className="py-4">
         <Navbar></Navbar>
       </div>
+
+      <section className="py-32 text-center">
+        <h1 className="text-6xl font-semibold">
+          Great Ideas Deserve<br></br>
+          <span className="text-primary">Great Partners</span>
+        </h1>
+
+        <div className="w-full text-center mt-8">
+          <h1 className="text-lg text-accent mx-auto max-w-xl">
+            Discover and connect with co-founders who complement your skills and vision. Whether you’re starting up or
+            joining one, this is where teams begin.
+          </h1>
+        </div>
+      </section>
     </div>
   );
 };
