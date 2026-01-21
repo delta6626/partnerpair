e817b0ee4676333fcd1ef3bb7194fb6030bf2c91
Delta6626 <hassan911ee@gmail.com>
2026-01-21 17:32:12 +0400

    change layout + add buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { ArrowRight } from "lucide-react";
 import { Navbar } from "../components/navigation/Navbar";
 
 export const Home = () => {
@@ -7,17 +8,23 @@ export const Home = () => {
         <Navbar></Navbar>
       </div>
 
-      <section className="py-32 text-center">
+      <section className="py-32">
         <h1 className="text-6xl font-semibold">
           Great Ideas Deserve<br></br>
           <span className="text-primary">Great Partners</span>
         </h1>
 
-        <div className="w-full text-center mt-8">
-          <h1 className="text-lg text-accent mx-auto max-w-xl">
-            Discover and connect with co-founders who complement your skills and vision. Whether you’re starting up or
-            joining one, this is where teams begin.
-          </h1>
+        <h1 className="mt-8 text-lg text-accent max-w-lg">
+          Discover and connect with co-founders who complement your skills and vision. Whether you’re starting up or
+          joining one, this is where teams begin.
+        </h1>
+
+        <div className="mt-4 flex gap-4">
+          <button className="btn btn-primary">
+            Get started <ArrowRight size={20} />
+          </button>
+
+          <button className="btn">Watch Demo</button>
         </div>
       </section>
     </div>
