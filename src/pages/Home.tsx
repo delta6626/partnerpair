76ae6e00ab504c6c8c4a22d3b733a405745292e6
Delta6626 <hassan911ee@gmail.com>
2026-01-24 13:28:48 +0400

    change gap

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -142,7 +142,7 @@ export const Home = () => {
 
       <div className="border-b border-base-100"></div>
 
-      <section className="py-16 flex flex-col gap-8 paddingContainer">
+      <section className="py-16 flex flex-col gap-16 paddingContainer">
         <div className="w-full flex items-center flex-col gap-8">
           <IconText icon={UserCheck} text={"Comprehensive Profiles"} topTag={true} />
 
