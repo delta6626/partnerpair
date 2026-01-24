c6c8d9fb778887cd0f5d228d4483396c7cb04d88
Delta6626 <hassan911ee@gmail.com>
2026-01-24 13:27:52 +0400

    change icon padding

--- src/components/landing/FeatureCard.tsx
+++ src/components/landing/FeatureCard.tsx
@@ -10,12 +10,12 @@ export const FeatureCard = ({
   description: string;
 }) => {
   return (
-    <div className="w-full rounded-3xl flex flex-col gap-2 p-8 border border-base-100">
-      <div className="bg-primary/20 text-primary rounded-xl w-fit px-4 py-2">
+    <div className="w-full h-full rounded-3xl flex flex-col gap-2 p-8 border border-base-100">
+      <div className="bg-primary/20 text-primary rounded-xl w-fit p-2">
         <Icon size={20} />
       </div>
-      <h1 className="text-lg max-w-xs">{title}</h1>
-      <h1 className="text-accent max-w-xs">{description}</h1>
+      <h1 className="text-lg">{title}</h1>
+      <h1 className="text-accent">{description}</h1>
     </div>
   );
 };
