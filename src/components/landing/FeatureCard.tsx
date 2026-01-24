e1138d3a007e7ae0d9a3dc30844395052a228848
Delta6626 <hassan911ee@gmail.com>
2026-01-24 12:43:25 +0400

    create feature card

--- src/components/landing/FeatureCard.tsx
+++ src/components/landing/FeatureCard.tsx
@@ -10,10 +10,12 @@ export const FeatureCard = ({
   description: string;
 }) => {
   return (
-    <div className="">
-      <Icon />
-      {title}
-      {description}
+    <div className="w-fit rounded-3xl flex flex-col gap-2 p-8 border border-base-100">
+      <div className="bg-primary/20 text-primary rounded-xl w-fit px-4 py-2">
+        <Icon size={20} />
+      </div>
+      <h1 className="text-lg max-w-xs">{title}</h1>
+      <h1 className="text-accent max-w-xs">{description}</h1>
     </div>
   );
 };
