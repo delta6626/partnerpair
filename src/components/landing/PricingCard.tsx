cfcb23573a5986a6cfe6dd6bb3e90f3ab9d3afd8
Delta6626 <hassan911ee@gmail.com>
2026-01-26 17:24:04 +0400

    add props

--- src/components/landing/PricingCard.tsx
+++ src/components/landing/PricingCard.tsx
@@ -1,3 +1,15 @@
-export const PricingCard = () => {
+export const PricingCard = ({
+  tierName,
+  tierPrice,
+  tierFeatures,
+  tierLink,
+  isRecommended,
+}: {
+  tierName: string;
+  tierPrice: string;
+  tierFeatures: string[];
+  tierLink: string;
+  isRecommended: boolean;
+}) => {
   return <div className=""></div>;
 };
