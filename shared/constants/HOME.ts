85fd8d713028019d2594e42374232d3d2ee0a0d3
Delta6626 <hassan911ee@gmail.com>
2026-01-27 13:39:13 +0400

    add features

--- shared/constants/HOME.ts
+++ shared/constants/HOME.ts
@@ -1,4 +1,20 @@
 export const HOME = {
-  BASIC_FEATURES: [],
-  PRO_FEATURES: [],
+  BASIC_FEATURES: [
+    "Create your profile",
+    "Search and browse co-founders",
+    "Limited search filters",
+    "Limited access to other profiles",
+    `Start up to 5 chats`,
+    `Save up to 5 contacts`,
+  ],
+  PRO_FEATURES: [
+    "Create your profile",
+    "Search and browse co-founders",
+    "Full access to all search filters",
+    "Full access to other profiles",
+    "See who viewed your profile",
+    "Unlimited chats",
+    "Unlimited saved contacts",
+    "Priority support",
+  ],
 };
