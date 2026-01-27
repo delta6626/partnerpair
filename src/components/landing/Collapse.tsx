4eb7c941a8ae7e138ff27ff31f806381a79bc630
delta6626 <hassan911ee@gmail.com>
2026-01-28 02:06:39 +0400

    create collapse component for FAQ section

---
+++ src/components/landing/Collapse.tsx
@@ -0,0 +1,8 @@
+export const Collapse = ({ question, answer }: { question: string; answer: string }) => {
+  return (
+    <div tabIndex={0} className="collapse collapse-arrow border border-base-100 rounded-3xl">
+      <div className="collapse-title text-lg">{question}</div>
+      <div className="collapse-content text-sm">{answer}</div>
+    </div>
+  );
+};
