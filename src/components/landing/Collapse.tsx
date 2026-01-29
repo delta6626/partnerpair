84ebaedcf79bb24988f1725f1e106cc42baa9325
delta6626 <hassan911ee@gmail.com>
2026-01-29 19:46:41 +0400

    remove text-accent

--- src/components/landing/Collapse.tsx
+++ src/components/landing/Collapse.tsx
@@ -1,7 +1,7 @@
 export const Collapse = ({ question, answer }: { question: string; answer: string }) => {
   return (
     <div tabIndex={0} className="collapse collapse-arrow border border-base-100 rounded-3xl">
-      <div className="collapse-title text-lg text-accent">{question}</div>
+      <div className="collapse-title text-lg">{question}</div>
       <div className="collapse-content">{answer}</div>
     </div>
   );
