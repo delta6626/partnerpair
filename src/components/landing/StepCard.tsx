f42de9e792a99b5871f1ab37c3ecb273b4bb6b9f
Delta6626 <hassan911ee@gmail.com>
2026-01-30 19:52:27 +0400

    change text color

--- src/components/landing/StepCard.tsx
+++ src/components/landing/StepCard.tsx
@@ -9,7 +9,7 @@ export const StepCard = ({
 }) => {
   return (
     <div className="flex flex-col gap-2">
-      <h1 className="text-6xl font-semibold text-accent">{step}</h1>
+      <h1 className="text-6xl font-semibold text-primary">{step}</h1>
       <h1 className="text-2xl">{stepTitle}</h1>
       <h1 className="text-accent max-w-xs">{stepContent}</h1>
     </div>
