a24b0f7bae9419d2b11386575481125259700cb1
Delta6626 <hassan911ee@gmail.com>
2026-02-07 20:27:11 +0400

    create new modal

---
+++ src/components/modals/MaxChatsReachedErrorModal.tsx
@@ -0,0 +1,31 @@
+import { AlertCircle } from "lucide-react";
+import { MODALS } from "../../../shared/constants/MODALS";
+import { MODAL_ACTIONS } from "../../../shared/constants/MODAL_ACTIONS";
+import { UpgradeTierButton } from "../user/UpgradeTierButton";
+
+export const MaxChatsReachedModal = () => {
+  const closeModal = () => {
+    const modal = document.getElementById(MODALS.MAX_CHATS_REACHED_ERROR_MODAL.ID) as HTMLDialogElement;
+    modal.close();
+  };
+
+  return (
+    <dialog id={MODALS.MAX_CHATS_REACHED_ERROR_MODAL.ID} className="modal">
+      <div className="modal-box bg-base-300 border border-base-100">
+        <div className="flex items-center gap-2">
+          <AlertCircle size={20} className="text-error/60" />
+          <h1 className="text-lg font-medium">{MODALS.MAX_CHATS_REACHED_ERROR_MODAL.TITLE}</h1>
+        </div>
+        <p className="mt-2 text-accent">{MODALS.MAX_CHATS_REACHED_ERROR_MODAL.DESCRIPTION}</p>
+
+        <div className="flex justify-end gap-2 mt-4">
+          <button type="button" className="btn" onClick={closeModal}>
+            {MODAL_ACTIONS.ACTION_CANCEL}
+          </button>
+
+          <UpgradeTierButton />
+        </div>
+      </div>
+    </dialog>
+  );
+};
