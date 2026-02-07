ff7fad8d33224cdf960208ab645f2f4c6e27458c
Delta6626 <hassan911ee@gmail.com>
2026-02-07 22:16:10 +0400

    apply min width to button

--- src/components/user/DeleteAccount.tsx
+++ src/components/user/DeleteAccount.tsx
@@ -2,7 +2,7 @@ import { HeartCrack } from "lucide-react";
 
 export const DeleteAccount = ({ className }: { className?: string }) => {
   return (
-    <button className={`btn btn-error w-45 ${className ?? ""}`}>
+    <button className={`btn btn-error min-w-45 ${className ?? ""}`}>
       <HeartCrack size={20} />
       Delete account
     </button>
