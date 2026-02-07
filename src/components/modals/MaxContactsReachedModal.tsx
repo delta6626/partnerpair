bbb304401105ef62500c7c9002d787ad9338270d
Delta6626 <hassan911ee@gmail.com>
2026-02-07 19:24:56 +0400

    change modal appearance

--- src/components/modals/MaxContactsReachedModal.tsx
+++ src/components/modals/MaxContactsReachedModal.tsx
@@ -1,4 +1,4 @@
-import { CheckCircle2 } from "lucide-react";
+import { AlertCircle, CheckCircle2 } from "lucide-react";
 import { MODALS } from "../../../shared/constants/MODALS";
 import { MODAL_ACTIONS } from "../../../shared/constants/MODAL_ACTIONS";
 import { UpgradeTierButton } from "../user/UpgradeTierButton";
@@ -13,14 +13,14 @@ export const MaxContactsReachedModal = () => {
     <dialog id={MODALS.MAX_CONTACTS_REACHED_ERROR_MODAL.ID} className="modal">
       <div className="modal-box bg-base-300 border border-base-100">
         <div className="flex items-center gap-2">
-          <CheckCircle2 size={20} className="text-primary" />
+          <AlertCircle size={20} className="text-error/60" />
           <h1 className="text-lg font-medium">{MODALS.MAX_CONTACTS_REACHED_ERROR_MODAL.TITLE}</h1>
         </div>
         <p className="mt-2 text-accent">{MODALS.MAX_CONTACTS_REACHED_ERROR_MODAL.DESCRIPTION}</p>
 
         <div className="flex justify-end gap-2 mt-4">
           <button type="button" className="btn" onClick={closeModal}>
-            {MODAL_ACTIONS.ACTION_OK}
+            {MODAL_ACTIONS.ACTION_CANCEL}
           </button>
 
           <UpgradeTierButton />
