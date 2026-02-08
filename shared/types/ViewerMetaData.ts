fee7a57e8e5f751b8de547484c28f3ffd7a1e7c6
Delta6626 <hassan911ee@gmail.com>
2026-02-08 20:54:00 +0400

    add comment

--- shared/types/ViewerMetaData.ts
+++ shared/types/ViewerMetaData.ts
@@ -1,4 +1,4 @@
-// @ts-expect-error
+// @ts-expect-error : This type is shared by the server and the client, but firebase-admin/firestore is not accessible from the client.
 import type { Timestamp } from "firebase-admin/firestore";
 
 export interface ViewerMetaData {
