daf043c1bc21c9ef4c4a21b70a0182117f243f16
Delta6626 <hassan911ee@gmail.com>
2026-02-09 23:04:26 +0400

    boilerplate

--- src/pages/Upgrade.tsx
+++ src/pages/Upgrade.tsx
@@ -1,3 +1,25 @@
+import { Loader } from "../components/Loader";
+import { MainNavbar } from "../components/navigation/MainNavbar";
+import { useInitializeUser } from "../hooks/useInitializeUser";
+import { useTheme } from "../hooks/useTheme";
+
 export const Upgrade = () => {
-  return <div className=""></div>;
+  useTheme();
+  const { user, loading } = useInitializeUser();
+
+  if (loading) {
+    return (
+      <div className="w-full min-h-[100vh] bg-base-300 flex items-center justify-center">
+        <Loader />
+      </div>
+    );
+  }
+
+  return (
+    <div className="w-full min-h-[100vh] font-inter bg-base-300 paddingContainer">
+      <div className="py-4">
+        <MainNavbar />
+      </div>
+    </div>
+  );
 };
