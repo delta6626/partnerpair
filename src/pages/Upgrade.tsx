5d12025e05ecd36e9c65ef085de2203b1a1a2c4b
Delta6626 <hassan911ee@gmail.com>
2026-02-10 12:38:16 +0400

    add header and subtitle

--- src/pages/Upgrade.tsx
+++ src/pages/Upgrade.tsx
@@ -1,3 +1,5 @@
+import { Crown } from "lucide-react";
+import { IconText } from "../components/landing/IconText";
 import { Loader } from "../components/Loader";
 import { MainNavbar } from "../components/navigation/MainNavbar";
 import { useInitializeUser } from "../hooks/useInitializeUser";
@@ -16,13 +18,30 @@ export const Upgrade = () => {
   }
 
   if (user?.basicInfo.tier === "Pro") {
-    return <div className=""></div>;
+    return (
+      <div className="w-full min-h-[100vh] font-inter bg-base-300 paddingContainer">
+        <div className="py-4">
+          <MainNavbar />
+        </div>
+      </div>
+    );
   }
 
   return (
     <div className="w-full min-h-[100vh] font-inter bg-base-300 paddingContainer">
       <div className="py-4">
         <MainNavbar />
+      </div>
+
+      <div className="w-full py-8 mt-4 flex flex-col items-center text-center gap-8">
+        <IconText icon={Crown} text={"Upgrade to Pro"} topTag={true} />
+
+        <h1 className="text-4xl font-medium">Upgrade for a Superior Experience</h1>
+
+        <h2 className="text-accent text-lg max-w-lg">
+          PartnerPair Pro unlocks advanced features designed to help you connect with the right co-founder faster and
+          more confidently.
+        </h2>
       </div>
     </div>
   );
