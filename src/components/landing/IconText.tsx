b1699056e6c8069cc1ab969571a215143adfe14e
Delta6626 <hassan911ee@gmail.com>
2026-02-10 12:47:09 +0400

    refactor

--- src/components/landing/IconText.tsx
+++ src/components/landing/IconText.tsx
@@ -1,11 +1,21 @@
 import type { LucideIcon } from "lucide-react";
 
-export const IconText = ({ icon: Icon, text, topTag }: { icon: LucideIcon; text: string; topTag?: boolean }) => {
+export const IconText = ({
+  icon: Icon,
+  text,
+  topTag,
+  iconClassName,
+}: {
+  icon: LucideIcon;
+  text: string;
+  topTag?: boolean;
+  iconClassName?: string;
+}) => {
   const topTagClassName = "bg-primary/20 w-fit px-4 py-2 rounded-3xl text-primary text-sm";
 
   return (
     <h1 className={`flex items-center gap-2 text-accent ${topTag ? topTagClassName : ""}`}>
-      <Icon className="text-primary" size={20} />
+      <Icon className={iconClassName ? iconClassName : "text-primary"} size={20} />
       {text}
     </h1>
   );
