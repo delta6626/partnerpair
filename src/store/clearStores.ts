c760a5ae06c749feaeab0c2281105047fd558fb2
Delta6626 <hassan911ee@gmail.com>
2026-02-14 20:35:01 +0400

    invalidate cache

--- src/store/clearStores.ts
+++ src/store/clearStores.ts
@@ -3,11 +3,13 @@ import { useSelectedChatStore } from "./useSelectedChatStore";
 import { useSelectedChatMetaDataStore } from "./useSelectedChatMetaData";
 import { useProfileStatusVisibleStore } from "./useProfileStatusVisibleStore";
 import { useFilterMenuStore } from "./useFilterMenuStore";
+import { useSelectedMessageStore } from "./useSelectedMessageStore";
 
 export const clearStores = () => {
   useUserStore.getState().resetUser();
   useSelectedChatStore.getState().setSelectedChatId(null);
   useSelectedChatMetaDataStore.getState().setSelectedChatMetaData(null);
+  useSelectedMessageStore.getState().setSelectedMessage(null);
   useProfileStatusVisibleStore.getState().setStatusVisible(false);
   useFilterMenuStore.getState().setIsOpen(false);
 };
