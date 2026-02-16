881eadac1bcff3c78316e91ba3c3abf0aa2593f6
Delta6626 <hassan911ee@gmail.com>
2026-02-16 17:10:48 +0400

    add click event handler

--- src/components/user/DeleteAccount.tsx
+++ src/components/user/DeleteAccount.tsx
@@ -1,8 +1,8 @@
 import { HeartCrack } from "lucide-react";
 
-export const DeleteAccount = ({ className }: { className?: string }) => {
+export const DeleteAccount = ({ className, onClickHandler }: { className?: string; onClickHandler?: () => void }) => {
   return (
-    <button className={`btn btn-error min-w-45 ${className ?? ""}`}>
+    <button className={`btn btn-error min-w-45 ${className ?? ""}`} onClick={onClickHandler}>
       <HeartCrack size={20} />
       Delete account
     </button>
