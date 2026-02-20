b80b85252c00cca76910161b12941548cca26ce7
Delta6626 <hassan911ee@gmail.com>
2026-02-20 14:44:17 +0400

    create new hook

---
+++ src/hooks/useVerificationCheck.ts
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { getAuthenticatedUser } from "../services/authentication/authServices";
+import { useNavigate } from "react-router-dom";
+
+export const useVerificationCheck = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkUser = async () => {
+      const user = await getAuthenticatedUser();
+      if (typeof user === "string") return;
+
+      if (user.emailVerified) {
+        navigate("/dashboard");
+      } else {
+        navigate("/verify");
+      }
+    };
+
+    checkUser();
+  }, []);
+};
