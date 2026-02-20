baa5978ed1a90b7fe5e27ad93957f23c523b7add
Delta6626 <hassan911ee@gmail.com>
2026-02-21 01:03:05 +0400

    change style

--- src/components/landing/DemoUserCollection.tsx
+++ src/components/landing/DemoUserCollection.tsx
@@ -12,7 +12,7 @@ export const DemoUserCollection = ({
   return (
     <div className="flex flex-col gap-2 border border-base-100 p-8 rounded-3xl select-none">
       <div className="flex items-center justify-between mb-2">
-        <h1 className="text-lg">{title}</h1>
+        <h1 className="font-medium text-lg">{title}</h1>
         {sideElement}
       </div>
 
