12038b9d4ddfd17d69b414c4c4888599b66aaf0b
Delta6626 <hassan911ee@gmail.com>
2026-02-23 02:04:41 +0400

    responsive section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -121,8 +121,8 @@ export const Home = ({ redirect }: { redirect: boolean }) => {
 
       <div className="border-b border-base-100"></div>
 
-      <section className="py-16 flex items-center justify-between gap-16 paddingContainer">
-        <div className="max-w-lg flex flex-col gap-8">
+      <section className="py-16 flex flex-col xl:flex-row items-center justify-between gap-16 paddingContainer">
+        <div className="w-full xl:max-w-lg flex flex-col gap-8">
           <IconText icon={Stars} text={"Smart Matching"} topTag={true} />
 
           <h1 className="text-4xl font-medium">Data Driven Co-founder Matching</h1>
