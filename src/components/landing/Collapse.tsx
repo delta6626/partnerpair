3b5fb441cc17dfaa590dd101c0647e072c2dd05b
Delta6626 <hassan911ee@gmail.com>
2026-02-24 22:58:18 +0400

    add border

--- src/components/landing/Collapse.tsx
+++ src/components/landing/Collapse.tsx
@@ -1,6 +1,6 @@
 export const Collapse = ({ question, answer }: { question: string; answer: string }) => {
   return (
-    <div tabIndex={0} className="collapse collapse-arrow bg-base-200 rounded-3xl">
+    <div tabIndex={0} className="collapse collapse-arrow bg-base-200 border border-base-100 rounded-3xl">
       <div className="collapse-title text-lg text-accent">{question}</div>
       <div className="collapse-content">{answer}</div>
     </div>
