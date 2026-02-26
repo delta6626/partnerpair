4700fb2d09744b237b7c8f304451e6608e1caae9
Delta6626 <hassan911ee@gmail.com>
2026-02-26 23:58:13 +0400

    integrate logo

--- src/components/landing/Logo.tsx
+++ src/components/landing/Logo.tsx
@@ -2,8 +2,11 @@ import { Link } from "react-router-dom";
 
 export const Logo = ({ link }: { link?: string }) => {
   return (
-    <Link to={link ?? "/"} className="font-semibold text-2xl">
-      PartnerPair
-    </Link>
+    <div className="flex items-center gap-2">
+      <img src={"logo.svg"} alt="PartnerPair Logo" className="w-8 aspect-square" />
+      <Link to={link ?? "/"} className="font-semibold text-2xl">
+        PartnerPair
+      </Link>
+    </div>
   );
 };
