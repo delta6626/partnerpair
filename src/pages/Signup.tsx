16b9ddbbed578b600c625c0d5f795b46f44481ae
Delta6626 <hassan911ee@gmail.com>
2026-02-27 14:32:08 +0400

    link to terms of service, privacy policy and login page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -3,6 +3,7 @@ import { Navbar } from "../components/navigation/Navbar";
 import { SIGNUP } from "../../shared/constants/SIGNUP";
 import { useVerificationCheck } from "../hooks/useVerificationCheck";
 import { useTheme } from "../hooks/useTheme";
+import { Link } from "react-router-dom";
 
 export const Signup = () => {
   useTheme();
@@ -18,8 +19,24 @@ export const Signup = () => {
         <div className="w-full max-w-lg">
           <div className="w-full flex flex-col items-center">
             <h1 className="font-semibold text-3xl md:text-4xl text-center">{SIGNUP.GREETING_TEXT}</h1>
-
             <SignupForm />
+            <p className="mt-2">
+              By signing up, you agree to our{" "}
+              <Link className="text-primary" to={"/terms-of-service"}>
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link className="text-primary" to={"/privacy-policy"}>
+                Privacy Policy.
+              </Link>
+            </p>
+
+            <p className="mt-2">
+              Already have an account?{" "}
+              <Link className="text-primary" to={"/login"}>
+                Log In.
+              </Link>
+            </p>
           </div>
         </div>
       </div>
