4383bb53370ec847e191cca45d5c396b865409a8
Delta6626 <hassan911ee@gmail.com>
2026-02-27 14:31:11 +0400

    change text

--- shared/constants/SIGNUP.ts
+++ shared/constants/SIGNUP.ts
@@ -4,7 +4,6 @@ export const SIGNUP = {
   GREETING_TEXT: "Hi, let's get you started.",
   SIGNUP_BUTTON_TEXT: "Continue",
   SIGNUP_WITH_GOOGLE_BUTTON_TEXT: "Continue with Google",
-  TOS_TEXT: "By signing up, you agree to our Terms of Service and Privacy Policy.",
   MAXIMUM_FIRST_NAME_LENGTH: 50,
   MAXIMUM_LAST_NAME_LENGTH: 50,
   MAXIMUM_EMAIL_LENGTH: 254,
