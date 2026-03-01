ea37c49e3faac6be024ddd8fb4738fe6eb262a60
Delta6626 <hassan911ee@gmail.com>
2026-03-01 23:58:14 +0400

    add ui

--- src/components/modals/PasswordResetModal.tsx
+++ src/components/modals/PasswordResetModal.tsx
@@ -1,12 +1,27 @@
 import { MailIcon } from "lucide-react";
 import { MODAL_ACTIONS } from "../../../shared/constants/MODAL_ACTIONS";
 import { MODALS } from "../../../shared/constants/MODALS";
+import { useEffect, useState, type ChangeEvent } from "react";
+import { isValidEmail } from "../../../shared/utils/isValidEmail";
 
 export const PassswordResetModal = () => {
+  const [email, setEmail] = useState<string>("");
+  const [emailValid, setEmailValid] = useState<boolean>(false);
+
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
   const closeModal = () => {
     const modal = document.getElementById(MODALS.PASSWORD_RESET_MODAL.ID) as HTMLDialogElement;
     modal.close();
   };
+
+  useEffect(() => {
+    if (isValidEmail(email)) {
+      setEmailValid(true);
+    }
+  }, [email]);
 
   return (
     <dialog id={MODALS.PASSWORD_RESET_MODAL.ID} className="modal">
@@ -15,11 +30,23 @@ export const PassswordResetModal = () => {
           <MailIcon size={20} className="text-primary" />
           <h1 className="text-lg font-medium">{MODALS.PASSWORD_RESET_MODAL.TITLE}</h1>
         </div>
-        <p className="mt-2 text-accent">{MODALS.PASSWORD_RESET_MODAL.DESCRIPTION}</p>
 
-        <div className="flex justify-end mt-4">
-          <button type="button" className="btn" onClick={closeModal}>
-            {MODAL_ACTIONS.ACTION_OK}
+        <p className="mt-2 text-accent">{MODALS.PASSWORD_RESET_MODAL.DESCRIPTION}</p>
+        <input
+          type="email"
+          className="input w-full mt-4"
+          placeholder={"Enter your email"}
+          value={email}
+          onChange={handleEmailChange}
+        ></input>
+
+        <div className="flex gap-2 justify-end mt-2">
+          <button type="button" className="btn flex-grow" onClick={closeModal}>
+            {MODAL_ACTIONS.ACTION_CANCEL}
+          </button>
+
+          <button type="button" className="btn btn-primary flex-grow">
+            {MODAL_ACTIONS.ACTION_SEND}
           </button>
         </div>
       </div>
