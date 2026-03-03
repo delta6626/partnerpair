9232614c3e2837468cfadaa0221edfc1b59db928
Delta6626 <hassan911ee@gmail.com>
2026-03-03 22:24:05 +0400

    fix logo not rendering bug

--- src/components/landing/Logo.tsx
+++ src/components/landing/Logo.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import logo from "../../../public/logo.svg";
 
 export const Logo = ({ link }: { link?: string }) => {
   return (
     <div className="flex items-center gap-2">
-      <img src={"logo.svg"} alt="PartnerPair Logo" className="w-8 aspect-square" />
+      <img src={logo} alt="PartnerPair Logo" className="w-8 aspect-square" />
       <Link to={link ?? "/"} className="font-semibold text-2xl">
         PartnerPair
       </Link>
