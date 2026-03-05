6cbdbee424e3fbb0e415f5917191c567b6ff5c00
Delta6626 <hassan911ee@gmail.com>
2026-03-05 16:44:01 +0400

    hide links

--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -12,7 +12,7 @@ export const Verify = () => {
   return (
     <div className="font-inter w-full min-h-[100vh] bg-base-300 paddingContainer flex flex-col">
       <div className="py-4">
-        <Navbar />
+        <Navbar hideHomePageSpecificLinks={true} />
       </div>
 
       {loading ? (
